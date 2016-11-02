openbmc/phosphor-objmgr

libmapper: add config.h
Add a fake config.h pending autotools switchover.

Change-Id: I794d1d411de32e5be1ef66b6dfd32cc809734290
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

libmapper/config.h
@@ -1,0 +1,3 @@
+#define BUSNAME "org.openbmc.ObjectMapper"
+#define INTERFACE "org.openbmc.ObjectMapper"
+#define PATH "/org/openbmc/ObjectMapper"

edtanous/openbmc

Change-Id: I777d7b4399107db058329ab29c9299d68cfbd042

phosphor-objmgr/libmapper/config.h
@@ -1,0 +1,3 @@
+#define MAPPER_BUSNAME "org.openbmc.ObjectMapper"
+#define MAPPER_INTERFACE "org.openbmc.ObjectMapper"
+#define MAPPER_PATH "/org/openbmc/ObjectMapper"