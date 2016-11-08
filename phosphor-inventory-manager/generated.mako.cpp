openbmc/phosphor-inventory-manager

Switch to generated interface factory methods
Automatically populate the association of interface with an
appropriate sdbusplus server side interface binding.

Change-Id: Ie46d91cf2e6726b033789be6228c10ca14a76220
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

generated.mako.cpp
@@ -1,6 +1,9 @@
 // This file was auto generated.  Do not edit.
 
 #include "manager.hpp"
+% for i in interfaces:
+#include <${'/'.join(i.split('.') + ['server.hpp'])}>
+% endfor
 
 namespace phosphor
 {
@@ -8,6 +11,16 @@ namespace inventory
 {
 namespace manager
 {
+
+const Manager::Makers Manager::_makers{
+% for i in interfaces:
+    {
+        "${i}",
+        details::interface::holder::Make<
+            sdbusplus::server::${'::'.join(i.split('.'))}>::make,
+    },
+% endfor
+};
 
 const Manager::Events Manager::_events{
 % for e in events:

edtanous/openbmc

phosphor-inventory-manager/generated.mako.cpp
@@ -3,6 +3,9 @@
 // This file was auto generated.  Do not edit.
 
 #include "manager.hpp"
+% for i in interfaces:
+#include <${'/'.join(i.split('.') + ['server.hpp'])}>
+% endfor
 
 namespace phosphor
 {
@@ -10,6 +13,16 @@ namespace inventory
 {
 namespace manager
 {
+
+const Manager::Makers Manager::_makers{
+% for i in interfaces:
+    {
+        "${i}",
+        details::interface::holder::Holder<
+            sdbusplus::server::${'::'.join(i.split('.'))}>::make,
+    },
+% endfor
+};
 
 const Manager::Events Manager::_events{
 % for e in events: