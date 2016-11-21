openbmc/phosphor-objmgr

Rename mapper DBus names to match xyz
Use the domain we actually own, per DBus naming best practices.

Change-Id: I2a982670b28428c0c345aa654e3ebb6d9bf7aac1
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

libmapper/config.h
@@ -1,3 +1,3 @@
-#define BUSNAME "org.openbmc.ObjectMapper"
-#define INTERFACE "org.openbmc.ObjectMapper"
-#define PATH "/org/openbmc/ObjectMapper"
+#define BUSNAME "xyz.openbmc_project.ObjectMapper"
+#define INTERFACE "xyz.openbmc_project.ObjectMapper"
+#define PATH "/xyz/openbmc_project/ObjectMapper"

edtanous/openbmc

Change-Id: I69e85d91bcf30bfab5c1d6157f399683b91dda35

phosphor-objmgr/libmapper/config.h
@@ -1,3 +1,3 @@
-#define MAPPER_BUSNAME "org.openbmc.ObjectMapper"
-#define MAPPER_INTERFACE "org.openbmc.ObjectMapper"
-#define MAPPER_PATH "/org/openbmc/ObjectMapper"
+#define MAPPER_BUSNAME "xyz.openbmc_project.ObjectMapper"
+#define MAPPER_INTERFACE "xyz.openbmc_project.ObjectMapper"
+#define MAPPER_PATH "/xyz/openbmc_project/ObjectMapper"