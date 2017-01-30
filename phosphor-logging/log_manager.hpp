openbmc/phosphor-logging

log_manager: Cleanup main function
The design direction changed after the commit that added the
log_manager main function, so updating it to the correct usage.

Change-Id: I88459fb65ba6b8272f0f9f85b55f9f837487ffea
Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

log_manager.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <sdbusplus/server.hpp>
+#include <sdbusplus/bus.hpp>
 #include "xyz/openbmc_project/Logging/Internal/Manager/server.hpp"
 
 namespace phosphor
@@ -23,28 +23,22 @@ using ManagerIface =
  *  @details A concrete implementation for the
  *  xyz.openbmc_project.Logging.Internal.Manager DBus API.
  */
-class Manager final :
-    public details::ServerObject<details::ManagerIface>
+class Manager : public details::ServerObject<details::ManagerIface>
 {
     public:
         Manager() = delete;
         Manager(const Manager&) = delete;
         Manager& operator=(const Manager&) = delete;
         Manager(Manager&&) = default;
-        Manager& operator=(Manager&&) = default;
-        ~Manager() = default;
+        Manager& operator=(Manager&&) = delete;
+        virtual ~Manager() = default;
 
-        /** @brief Constructor for the Log Manager object
-         *  @param[in] bus - DBus bus to attach to.
-         *  @param[in] busname - Name of DBus bus to own.
-         *  @param[in] obj - Object path to attach to.
+        /** @brief Constructor to put object onto bus at a dbus path.
+         *  @param[in] bus - Bus to attach to.
+         *  @param[in] path - Path to attach at.
          */
-        Manager(sdbusplus::bus::bus&& bus,
-                    const char* busname,
-                    const char* obj);
-
-        /** @brief Start processing DBus messages. */
-        void run() noexcept;
+        Manager(sdbusplus::bus::bus& bus, const char* objPath) :
+                details::ServerObject<details::ManagerIface>(bus, objPath) {};
 
         /*
          * @fn commit()
@@ -57,13 +51,6 @@ class Manager final :
          *                     error log to be committed.
          */
         void commit(uint64_t transactionId, std::string errMsg) override;
-
-    private:
-        /** @brief Persistent sdbusplus DBus bus connection. */
-        sdbusplus::bus::bus _bus;
-
-        /** @brief sdbusplus org.freedesktop.DBus.ObjectManager reference. */
-        sdbusplus::server::manager::manager _manager;
 };
 
 } // namespace logging

log_manager_main.cpp
@@ -1,34 +1,29 @@
+#include <iostream>
 #include "config.h"
 #include "log_manager.hpp"
-#include <sdbusplus/bus.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <exception>
 
 int main(int argc, char *argv[])
 {
-    try {
-        auto manager = phosphor::logging::Manager(
-                sdbusplus::bus::new_system(),
-                BUSNAME_INTERNAL,
-                OBJ_INTERNAL);
+    auto busManager = sdbusplus::bus::new_default();
+    auto busEntry = sdbusplus::bus::new_default();
 
-        auto bus = sdbusplus::bus::new_default();
+    auto manager = phosphor::logging::Manager(busManager, OBJ_INTERNAL);
 
-        // Add sdbusplus ObjectManager.
-        sdbusplus::server::manager::manager(bus, OBJ_ENTRY);
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager(busManager, OBJ_INTERNAL);
+    sdbusplus::server::manager::manager(busEntry, OBJ_ENTRY);
 
-        bus.request_name(BUSNAME_ENTRY);
+    busManager.request_name(BUSNAME_INTERNAL);
+    busEntry.request_name(BUSNAME_ENTRY);
 
-        // TODO Create error log dbus object on demand, when the Commit interface
-        // creates an error log it'd call this entry interface to create an object.
+    // TODO Create error log dbus object on demand, when the Commit interface
+    // creates an error log it'd call this entry interface to create an object.
 
-        manager.run();
-
-        exit(EXIT_SUCCESS);
+    while(true)
+    {
+        busManager.process_discard();
+        busManager.wait();
     }
-    catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    exit(EXIT_FAILURE);
+
+    return 0;
 }

edtanous/openbmc

phosphor-logging/log_manager.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <sdbusplus/server.hpp>
+#include <sdbusplus/bus.hpp>
 #include "xyz/openbmc_project/Logging/Internal/Manager/server.hpp"
 
 namespace phosphor
@@ -23,28 +23,22 @@ using ManagerIface =
  *  @details A concrete implementation for the
  *  xyz.openbmc_project.Logging.Internal.Manager DBus API.
  */
-class Manager final :
-    public details::ServerObject<details::ManagerIface>
+class Manager : public details::ServerObject<details::ManagerIface>
 {
     public:
         Manager() = delete;
         Manager(const Manager&) = delete;
         Manager& operator=(const Manager&) = delete;
-        Manager(Manager&&) = default;
-        Manager& operator=(Manager&&) = default;
-        ~Manager() = default;
+        Manager(Manager&&) = delete;
+        Manager& operator=(Manager&&) = delete;
+        virtual ~Manager() = default;
 
-        /** @brief Constructor for the Log Manager object
-         *  @param[in] bus - DBus bus to attach to.
-         *  @param[in] busname - Name of DBus bus to own.
-         *  @param[in] obj - Object path to attach to.
+        /** @brief Constructor to put object onto bus at a dbus path.
+         *  @param[in] bus - Bus to attach to.
+         *  @param[in] path - Path to attach at.
          */
-        Manager(sdbusplus::bus::bus&& bus,
-                    const char* busname,
-                    const char* obj);
-
-        /** @brief Start processing DBus messages. */
-        void run() noexcept;
+        Manager(sdbusplus::bus::bus& bus, const char* path) :
+                details::ServerObject<details::ManagerIface>(bus, path) {};
 
         /*
          * @fn commit()
@@ -57,13 +51,6 @@ class Manager final :
          *                     error log to be committed.
          */
         void commit(uint64_t transactionId, std::string errMsg) override;
-
-    private:
-        /** @brief Persistent sdbusplus DBus bus connection. */
-        sdbusplus::bus::bus _bus;
-
-        /** @brief sdbusplus org.freedesktop.DBus.ObjectManager reference. */
-        sdbusplus::server::manager::manager _manager;
 };
 
 } // namespace logging

phosphor-logging/log_manager_main.cpp
@@ -1,22 +1,27 @@
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
 #include "config.h"
 #include "log_manager.hpp"
-#include <sdbusplus/bus.hpp>
-#include <cstdlib>
-#include <iostream>
-#include <exception>
 
 int main(int argc, char *argv[])
 {
-    try {
-        auto manager = phosphor::logging::Manager(
-                sdbusplus::bus::new_system(),
-                BUSNAME_LOGGING,
-                OBJ_INTERNAL);
-        manager.run();
-        exit(EXIT_SUCCESS);
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus, OBJ_INTERNAL);
+
+    phosphor::logging::Manager manager(bus, OBJ_INTERNAL);
+
+    bus.request_name(BUSNAME_LOGGING);
+
+    // TODO Create error log dbus object on demand, when the Commit interface
+    // creates an error log it'd call this entry interface to create an object.
+
+    while(true)
+    {
+        bus.process_discard();
+        bus.wait();
     }
-    catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    exit(EXIT_FAILURE);
+
+    return 0;
 }