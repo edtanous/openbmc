openbmc/phosphor-net-ipmid

Authentication Algo keeps record of the Confidentiality algo
The confidentiality algo negotiated during Open Session Request
would be stored in the Authentication algorithm and is activated
once the session setup is successful.

Change-Id: I4d5efd71a992dd0cf505bcf7a6d9ebb394bcb880
Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

sessions_manager.hpp
@@ -49,13 +49,15 @@ class Manager
          * @param[in] priv - Privilege level requested
          * @param[in] authAlgo - Authentication Algorithm
          * @param[in] intAlgo - Integrity Algorithm
+         * @param[in] confAlgo - Confidentiality Algorithm
          *
          * @return session handle on success and nullptr on failure
          *
          */
         std::weak_ptr<Session> startSession(SessionID remoteConsoleSessID,
                 Privilege priv, cipher::rakp_auth::Algorithms authAlgo,
-                cipher::integrity::Algorithms intAlgo);
+                cipher::integrity::Algorithms intAlgo,
+                cipher::conf::Algorithms confAlgo);
 
         /*
          * @brief Stop IPMI Session

edtanous/openbmc

phosphor-net-ipmid/sessions_manager.hpp
@@ -49,13 +49,15 @@ class Manager
          * @param[in] priv - Privilege level requested
          * @param[in] authAlgo - Authentication Algorithm
          * @param[in] intAlgo - Integrity Algorithm
+         * @param[in] cryptAlgo - Confidentiality Algorithm
          *
          * @return session handle on success and nullptr on failure
          *
          */
         std::weak_ptr<Session> startSession(SessionID remoteConsoleSessID,
                 Privilege priv, cipher::rakp_auth::Algorithms authAlgo,
-                cipher::integrity::Algorithms intAlgo);
+                cipher::integrity::Algorithms intAlgo,
+                cipher::crypt::Algorithms cryptAlgo);
 
         /*
          * @brief Stop IPMI Session