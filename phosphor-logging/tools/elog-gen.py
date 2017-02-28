openbmc/phosphor-logging

elog-gen.py: Set INFO as default error log severity level
Some error yaml files in the phosphor-dbus-interfaces repo do not
specify the error log severity. Default this value to INFO.

Change-Id: Ifd335ba7d5e3e8238365036a2bfa7a199e014cfc
Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

tools/elog-gen.py
@@ -201,7 +201,11 @@ def get_elog_data(i_elog_yaml,
             parent = i['inherits'][0].split(".").pop()
         parents[i['name']] = parent
         error_msg[i['name']] = i['description']
-        error_lvl[i['name']] = match['level']
+        try:
+            error_lvl[i['name']] = match['level']
+        except:
+            print ("No level found for: " + m['name'] + ", using INFO")
+            error_lvl[i['name']] = "INFO"
         tmp_meta = []
         # grab all the meta data fields and info
         for j in match['meta']:

edtanous/openbmc

phosphor-logging/tools/elog-gen.py
@@ -204,7 +204,11 @@ def get_elog_data(i_elog_yaml,
             parent = i['inherits'][0].split(".").pop()
         parents[i['name']] = parent
         error_msg[i['name']] = match['description']
-        error_lvl[i['name']] = i['level']
+        try:
+            error_lvl[i['name']] = i['level']
+        except:
+            print ("No level found for: " + i['name'] + ", using INFO")
+            error_lvl[i['name']] = "INFO"
         tmp_meta = []
         # grab all the meta data fields and info
         for j in i['meta']: