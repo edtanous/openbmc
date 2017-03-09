openbmc/openbmc-test-automation

Test LED groups using new xyz interface.
  - Created testcases to set on and off all different LEDs
     using new xyz interface.
  - Added keywords to Get and Set LEDs state.

Resolves openbmc/openbmc-test-automation#159

Change-Id: I0cc7a7226a7b2b10c3f326f4071bf192ebd65e70
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

data/variables.py
@@ -35,6 +35,9 @@ BMC_STATE_URI = '/xyz/openbmc_project/state/BMC0/'
 HOST_STATE_URI = '/xyz/openbmc_project/state/host0/'
 CHASSIS_STATE_URI = '/xyz/openbmc_project/state/chassis0/'
 
+# Led URI variable
+LED_GROUP_URI = '/xyz/openbmc_project/ledmanager/groups/'
+
 '''
   QEMU HTTPS variable:
 

edtanous/openbmc

  - Added keywords to Get and Set LEDs state using new
     xyz interface
  - Added skip list for Barreleye.
  - Skipped new LED group TCs for NA system i.e. Barreleye
     and Palmetto.

openbmc-test-automation/data/variables.py
@@ -47,6 +47,10 @@ ACTIVE = 'xyz.openbmc_project.Software.Activation.Activations.Active'
 # Inventory URI
 HOST_INVENTORY_URI = '/xyz/openbmc_project/inventory/'
 
+# Led URI variable
+LED_GROUPS_URI = '/xyz/openbmc_project/led/groups/'
+
+
 '''
   QEMU HTTPS variable:
 