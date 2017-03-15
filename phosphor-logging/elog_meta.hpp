openbmc/phosphor-logging

callouts: implement metadata handler
Implement handler of the metadata CALLOUT_DEVICE_PATH. This handler will
convert a device path to an inventory path and call out the latter.

Change-Id: Icbf9c841884ec139f35ea2ac94648e355b421bb2
Signed-off-by: Deepak Kodihalli <dkodihal@in.ibm.com>

elog_meta.hpp
@@ -92,6 +92,18 @@ inline void handler<example::xyz::openbmc_project::
     }
 }
 
+#if defined PROCESS_META
+
+template <>
+void handler<xyz::openbmc_project::Common::
+             Callout::Device::CALLOUT_DEVICE_PATH>(
+    std::string&& match,
+    const std::vector<std::string>& data,
+    AssociationList& list);
+
+#endif // PROCESS_META
+
+
 } // namespace metadata
 } // namespace logging
 } // namespace phosphor

edtanous/openbmc

phosphor-logging/elog_meta.hpp
@@ -100,6 +100,18 @@ inline void build<example::xyz::openbmc_project::
     }
 }
 
+#if defined PROCESS_META
+
+template <>
+void build<xyz::openbmc_project::Common::
+           Callout::Device::CALLOUT_DEVICE_PATH>(
+    std::string&& match,
+    const std::vector<std::string>& data,
+    AssociationList& list);
+
+#endif // PROCESS_META
+
+
 } // namespace associations
 } // namespace metadata
 } // namespace logging