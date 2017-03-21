openbmc/openpower-pnor-code-mgmt

Add base support for PNOR code management
This is the starting code needed to build the application.

Change-Id: Ie2937ed6246b8646b9e5b4d09318bc4aa7b31c35
Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

version_host_software_manager.cpp
@@ -1,0 +1,14 @@
+#include <iostream>
+#include "version_host_software_manager.hpp"
+
+namespace phosphor
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace phosphor
+

edtanous/openbmc

openpower-pnor-code-mgmt/version_host_software_manager.cpp
@@ -1,0 +1,13 @@
+#include "version_host_software_manager.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace manager
+{
+
+} // namespace manager
+} // namespace software
+} // namepsace openpower
+