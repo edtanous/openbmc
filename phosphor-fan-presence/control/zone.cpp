openbmc/phosphor-fan-presence

Fan control: Create Zone class
This class represents a fan zone, which is a collection
of fans that are always set to a speed together.

Change-Id: I5646f07b7b136aad216dabded3b5da3e9da0a531
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

control/zone.cpp
@@ -1,0 +1,51 @@
+/**
+ * Copyright © 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "zone.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+
+
+Zone::Zone(sdbusplus::bus::bus& bus,
+           const ZoneDefinition& def) :
+    _bus(bus),
+    _initialSpeed(std::get<initialSpeedPos>(def)),
+    _zoneNum(std::get<zoneNumPos>(def))
+{
+    auto& fanDefs = std::get<fanListPos>(def);
+
+    for (auto& def : fanDefs)
+    {
+        _fans.emplace_back(std::make_unique<Fan>(bus, def));
+    }
+}
+
+
+void Zone::setSpeed(uint64_t speed)
+{
+    for (auto& fan : _fans)
+    {
+        fan->setSpeed(speed);
+    }
+}
+
+}
+}
+}

edtanous/openbmc

phosphor-fan-presence/control/zone.cpp
@@ -1,0 +1,51 @@
+/**
+ * Copyright © 2017 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "zone.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+
+
+Zone::Zone(sdbusplus::bus::bus& bus,
+           const ZoneDefinition& def) :
+    _bus(bus),
+    _fullSpeed(std::get<fullSpeedPos>(def)),
+    _zoneNum(std::get<zoneNumPos>(def))
+{
+    auto& fanDefs = std::get<fanListPos>(def);
+
+    for (auto& def : fanDefs)
+    {
+        _fans.emplace_back(std::make_unique<Fan>(bus, def));
+    }
+}
+
+
+void Zone::setSpeed(uint64_t speed)
+{
+    for (auto& fan : _fans)
+    {
+        fan->setSpeed(speed);
+    }
+}
+
+}
+}
+}