openbmc/phosphor-bmc-code-mgmt

Create activation interface on BMC version dbus creation
Create an activation interface on BMC version dbus creation.
For now, set all versions with a purpose of "BMC" to Active.
Resolves openbmc/openbmc#1532

Change-Id: Ia253c5744bf7808191f2f1814734dab426edc1a5
Signed-off-by: Gunnar Mills <gmills@us.ibm.com>

item_updater.cpp
@@ -1,7 +1,10 @@
 #include <string>
-#include "item_updater.hpp"
+#include <phosphor-logging/log.hpp>
 #include "config.h"
+#include "item_updater.hpp"
+#include "xyz/openbmc_project/Software/Version/server.hpp"
 
+#include <iostream>
 namespace phosphor
 {
 namespace software
@@ -9,17 +12,71 @@ namespace software
 namespace updater
 {
 
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+
+using namespace phosphor::logging;
+
 int ItemUpdater::createActivation(sd_bus_message* msg,
                                   void* userData,
                                   sd_bus_error* retErr)
 {
-    auto versionId = 1;
     auto* updater = static_cast<ItemUpdater*>(userData);
-    updater->activations.insert(std::make_pair(
-            std::to_string(versionId),
-            std::make_unique<Activation>(
-                    updater->busItem,
-                    SOFTWARE_OBJPATH)));
+    auto mapper = updater->busItem.new_method_call(
+            MAPPER_BUSNAME,
+            MAPPER_PATH,
+            MAPPER_INTERFACE,
+            "GetSubTreePaths");
+    mapper.append(SOFTWARE_OBJPATH,
+                  1, // Depth
+                  std::vector<std::string>({VERSION_IFACE}));
+
+    auto mapperResponseMsg = updater->busItem.call(mapper);
+    if (mapperResponseMsg.is_method_error())
+    {
+        log<level::ERR>("Error in mapper call",
+                        entry("PATH=%s", SOFTWARE_OBJPATH),
+                        entry("INTERFACE=%s", VERSION_IFACE));
+        return -1;
+    }
+
+    std::vector<std::string> mapperResponse;
+    mapperResponseMsg.read(mapperResponse);
+    if (mapperResponse.empty())
+    {
+        log<level::ERR>("Error reading mapper response",
+                        entry("PATH=%s", SOFTWARE_OBJPATH),
+                        entry("INTERFACE=%s", VERSION_IFACE));
+        return -1;
+    }
+
+    for (const auto& resp : mapperResponse)
+    {
+        // Version id is the last item in the path
+        auto pos = resp.rfind("/");
+        if (pos == std::string::npos)
+        {
+            log<level::ERR>("No version id found in object path",
+                    entry("OBJPATH=%s", resp));
+            return -1;
+        }
+
+        auto versionId = resp.substr(pos + 1);
+
+        // For now set all BMC code versions to active
+        auto activationState = server::Activation::Activations::Active;
+
+        if (updater->activations.find(versionId) == updater->activations.end())
+        {
+            updater->activations.insert(std::make_pair(
+                    versionId,
+                    std::make_unique<Activation>(
+                            updater->busItem,
+                            resp,
+                            versionId,
+                            activationState)));
+        }
+    }
     return 0;
 }
 

edtanous/openbmc

phosphor-bmc-code-mgmt/item_updater.cpp
@@ -1,6 +1,8 @@
 #include <string>
-#include "item_updater.hpp"
+#include <phosphor-logging/log.hpp>
 #include "config.h"
+#include "item_updater.hpp"
+#include "xyz/openbmc_project/Software/Version/server.hpp"
 
 namespace phosphor
 {
@@ -9,17 +11,73 @@ namespace software
 namespace updater
 {
 
+// When you see server:: you know we're referencing our base class
+namespace server = sdbusplus::xyz::openbmc_project::Software::server;
+
+using namespace phosphor::logging;
+
 int ItemUpdater::createActivation(sd_bus_message* msg,
                                   void* userData,
                                   sd_bus_error* retErr)
 {
-    auto versionId = 1;
     auto* updater = static_cast<ItemUpdater*>(userData);
-    updater->activations.insert(std::make_pair(
-            std::to_string(versionId),
-            std::make_unique<Activation>(
-                    updater->bus,
-                    SOFTWARE_OBJPATH)));
+    auto m = sdbusplus::message::message(msg);
+
+    sdbusplus::message::object_path objPath;
+    std::map<std::string,
+        std::map<std::string,
+        sdbusplus::message::variant<std::string>>> interfaces;
+    m.read(objPath, interfaces);
+    std::string path(std::move(objPath));
+
+    for (const auto& intf : interfaces)
+    {
+        if (intf.first.compare(VERSION_IFACE))
+        {
+            continue;
+        }
+
+        for (const auto& property : intf.second)
+        {
+            if (!property.first.compare("Purpose"))
+            {
+                // Only process the BMC images
+                std::string value = sdbusplus::message::variant_ns::get <
+                                    std::string > (property.second);
+                if (value.compare(convertForMessage(server::Version::
+                                                    VersionPurpose::
+                                                    BMC).c_str()))
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    // Version id is the last item in the path
+    auto pos = path.rfind("/");
+    if (pos == std::string::npos)
+    {
+        log<level::ERR>("No version id found in object path",
+                        entry("OBJPATH=%s", path));
+        return -1;
+    }
+
+    auto versionId = path.substr(pos + 1);
+
+    if (updater->activations.find(versionId) == updater->activations.end())
+    {
+        // For now set all BMC code versions to active
+        auto activationState = server::Activation::Activations::Active;
+
+        updater->activations.insert(std::make_pair(
+                                        versionId,
+                                        std::make_unique<Activation>(
+                                            updater->bus,
+                                            path,
+                                            versionId,
+                                            activationState)));
+    }
     return 0;
 }
 