openbmc/openbmc-test-automation

Test enclosure fault led after assertion on CPU LED.
This change includes:
   - Added test case to test enclosure fault led after assertion
     on CPU LED.
   - Moved "Get System LED State" keyword into utils.robot

Resolves openbmc/openbmc-test-automation#432

Change-Id: Ic57b488a2087e18fd5e690d800ab4ac2e08eac2b
Signed-off-by: Sweta Potthuri <spotthur@in.ibm.com>

lib/utils.robot
@@ -943,6 +943,16 @@ Set MAC Address
     ${bmc_mac_addr}=  Execute Command On BMC  cat /sys/class/net/eth0/address
     Should Be Equal  ${bmc_mac_addr}  ${mac_address}
 
+Get System LED State
+    [Documentation]  Returns the state of given system LED.
+    [Tags]  Get_System_LED_State
+    [Arguments]  ${led_name}
+    # Description of argument(s):
+    # led_name     System LED name (e.g. heartbeat, identify, beep).
+
+    ${state}=  Read Attribute  ${LED_PHYSICAL_URI}${led_name}  State
+    [Return]  ${state.rsplit('.', 1)[1]}
+
 ###############################################################################
 Delete Error logs
     [Documentation]  Delete error logs.

edtanous/openbmc

   - Added test case to verify enclosure fault LED on assertion.
   - Moved "Get System LED State" keyword from led.robot into utils.robot.
   - Added the test case in skip list of barrelye and palmetto as it is NA.

openbmc-test-automation/lib/utils.robot
@@ -931,6 +931,16 @@ Set BMC Boot Count
     # Set BOOT_TIME variable to current boot time.
     Set Global Variable  ${BOOT_COUNT}  ${count}
 
+Get System LED State
+    [Documentation]  Return the state of given system LED.
+    [Arguments]  ${led_name}
+
+    # Description of argument(s):
+    # led_name     System LED name (e.g. heartbeat, identify, beep).
+
+    ${state}=  Read Attribute  ${LED_PHYSICAL_URI}${led_name}  State
+    [Return]  ${state.rsplit('.', 1)[1]}
+
 ###############################################################################
 Delete Error logs
     [Documentation]  Delete error logs.