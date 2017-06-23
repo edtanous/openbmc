openbmc/openbmc-test-automation

Creating a new skip list for extended
 - Creating a skip file 'skip_test_extended' in test_lists
   and adding tags  wherever required

Resolves openbmc/openbmc-test-automation#701

Change-Id: I0ece11e9140bf0bc59b6a0ea86c38a7b94c58c6f
Signed-off-by: Sweta Potthuri <spotthur@in.ibm.com>

extended/network_diagnostic_report.robot
@@ -7,6 +7,8 @@ Library       ../lib/utilities.py
 Resource      ../lib/utils.robot
 Test Setup    Check If Tool Exist
 
+Force Tags  Network_diagnostics
+
 *** Test Cases ***
 
 Check Network Latency

extended/run_keyword.robot
@@ -29,6 +29,7 @@ Documentation  Run the caller's keyword string.
 # NOTE: Robot searches PYTHONPATH for libraries.
 Library   run_keyword.py
 
+Force Tags  Run_Keyword_Test
 
 *** Variables ***
 # Initialize program parameters variables.

extended/test_slp.robot
@@ -9,6 +9,8 @@ Resource        ../lib/utils.robot
 # Get the SLP services available, make it suite global.
 Suite Setup     Get Service Types Output
 
+Force Tags  Verify_SLP_Service_Types
+
 *** Variables ***
 ${service_types}    findsrvtypes
 ${service_agents}   findsrvs

edtanous/openbmc

openbmc-test-automation/extended/network_diagnostic_report.robot
@@ -7,6 +7,8 @@ Library       ../lib/utilities.py
 Resource      ../lib/utils.robot
 Test Setup    Check If Tool Exist
 
+Force Tags  Check_Network_Latency
+
 *** Test Cases ***
 
 Check Network Latency

openbmc-test-automation/extended/run_keyword.robot
@@ -29,6 +29,7 @@ Documentation  Run the caller's keyword string.
 # NOTE: Robot searches PYTHONPATH for libraries.
 Library   run_keyword.py
 
+Force Tags  Run_Keyword_Pgm
 
 *** Variables ***
 # Initialize program parameters variables.

openbmc-test-automation/extended/test_slp.robot
@@ -9,6 +9,8 @@ Resource        ../lib/utils.robot
 # Get the SLP services available, make it suite global.
 Suite Setup     Get Service Types Output
 
+Force Tags  SLP_Service_Test
+
 *** Variables ***
 ${service_types}    findsrvtypes
 ${service_agents}   findsrvs