openbmc/openpower-pnor-code-mgmt

PNOR: Restore RedundancyPriority on reset
This commit extends the functionality of the PNOR software updater by
preserving RedundancyPriority values through any kind of reset.

This is accomplished by storing priority values in serial files in
/var/lib/obmc/openpower-pnor-code-mgmt/ using the Cereal library. Each
time a priority value is modified, the value in the corresponding
version file is adjusted.

When the item updater resets, it reads back the priority values from
these files.

Resolves openbmc/openbmc#2040

Change-Id: I908431801e541a1e5b39bcf49ae057f7e340eecc
Signed-off-by: Michael Tritz <mtritz@us.ibm.com>

serialize.cpp
@@ -1,0 +1,51 @@
+#include "config.h"
+#include <experimental/filesystem>
+#include <cereal/archives/binary.hpp>
+#include <fstream>
+#include "serialize.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace updater
+{
+
+namespace fs = std::experimental::filesystem;
+
+void storeToFile(std::string versionId, uint8_t priority)
+{
+    if(!fs::is_directory(SERIAL_DIR))
+    {
+        fs::create_directory(SERIAL_DIR);
+    }
+    std::string path = SERIAL_DIR + versionId;
+
+    std::ofstream os(path.c_str(), std::ios::binary);
+    cereal::BinaryOutputArchive oarchive(os);
+    oarchive(priority);
+}
+
+void restoreFromFile(std::string versionId, uint8_t *priority)
+{
+    std::string path = SERIAL_DIR + versionId;
+    if (fs::exists(path))
+    {
+        std::ifstream is(path.c_str(), std::ios::in | std::ios::binary);
+        cereal::BinaryInputArchive iarchive(is);
+        iarchive(*priority);
+    }
+}
+
+void removeFile(std::string versionId)
+{
+    std::string path = SERIAL_DIR + versionId;
+    if (fs::exists(path))
+    {
+        fs::remove(path);
+    }
+}
+
+} // namespace updater
+} // namespace software
+} // namespace openpower

edtanous/openbmc

openpower-pnor-code-mgmt/serialize.cpp
@@ -1,0 +1,51 @@
+#include "config.h"
+#include <experimental/filesystem>
+#include <cereal/archives/binary.hpp>
+#include <fstream>
+#include "serialize.hpp"
+
+namespace openpower
+{
+namespace software
+{
+namespace updater
+{
+
+namespace fs = std::experimental::filesystem;
+
+void storeToFile(std::string versionId, uint8_t priority)
+{
+    if(!fs::is_directory(PERSIST_DIR))
+    {
+        fs::create_directory(PERSIST_DIR);
+    }
+    std::string path = PERSIST_DIR + versionId;
+
+    std::ofstream os(path.c_str(), std::ios::binary);
+    cereal::BinaryOutputArchive oarchive(os);
+    oarchive(priority);
+}
+
+void restoreFromFile(std::string versionId, uint8_t *priority)
+{
+    std::string path = PERSIST_DIR + versionId;
+    if (fs::exists(path))
+    {
+        std::ifstream is(path.c_str(), std::ios::in | std::ios::binary);
+        cereal::BinaryInputArchive iarchive(is);
+        iarchive(*priority);
+    }
+}
+
+void removeFile(std::string versionId)
+{
+    std::string path = PERSIST_DIR + versionId;
+    if (fs::exists(path))
+    {
+        fs::remove(path);
+    }
+}
+
+} // namespace updater
+} // namespace software
+} // namespace openpower