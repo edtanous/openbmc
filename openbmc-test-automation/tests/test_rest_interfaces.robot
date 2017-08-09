openbmc/openbmc-test-automation

Multiple requests on BMC using single REST session
Resolves  openbmc/openbmc-test-automation#784

Change-Id: I293993a79cc11d5176b95a9de8a00b509669c5ac
Signed-off-by: George Keishing <gkeishin@in.ibm.com>

tests/test_rest_interfaces.robot
@@ -47,6 +47,21 @@ REST Delete All Sessions And Expect Error
     ...  Get Request  openbmc  /xyz/openbmc_project/
 
 
+Multiple Requests On BMC Using Single REST Session
+    [Documentation]  Trigger multiple REST operation using an active
+    ...  connection session.
+    [Tags]  Multiple_Requests_On_BMC_Using_Single_REST_Session
+
+    Initialize OpenBMC
+
+    # Session object "openbmc".
+    ${resp}=  Get Request  openbmc  /xyz/openbmc_project/state/
+    Should Be Equal As Strings  ${resp.status_code}  ${HTTP_OK}
+
+    ${resp}=  Get Request  openbmc  /xyz/openbmc_project/software/enumerate
+    Should Be Equal As Strings  ${resp.status_code}  ${HTTP_OK}
+
+
 Verify REST JSON Data On Success
     [Documentation]  Verify JSON data success response messages.
     [Tags]  Verify_REST_JSON_Data_On_Success

edtanous/openbmc

openbmc-test-automation/tests/test_rest_interfaces.robot
@@ -47,6 +47,21 @@ REST Delete All Sessions And Expect Error
     ...  Get Request  openbmc  /xyz/openbmc_project/
 
 
+Multiple Requests On BMC Using Single REST Session
+    [Documentation]  Trigger multiple REST operations using an active
+    ...  connection session.
+    [Tags]  Multiple_Requests_On_BMC_Using_Single_REST_Session
+
+    Initialize OpenBMC
+
+    # Session object "openbmc".
+    ${resp}=  Get Request  openbmc  /xyz/openbmc_project/state/
+    Should Be Equal As Strings  ${resp.status_code}  ${HTTP_OK}
+
+    ${resp}=  Get Request  openbmc  /xyz/openbmc_project/software/enumerate
+    Should Be Equal As Strings  ${resp.status_code}  ${HTTP_OK}
+
+
 Verify REST JSON Data On Success
     [Documentation]  Verify JSON data success response messages.
     [Tags]  Verify_REST_JSON_Data_On_Success