openbmc/openbmc-test-automation

Added test case to verify BMC version via xCAT.
Resolves openbmc/openbmc-test-automation#819

Change-Id: I5b5a6c5c6a990264ee4e0d132c56f6092403e17a
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

xcat/test_xcat_inventory.robot
@@ -1,0 +1,35 @@
+*** Settings ***
+Documentation   Module for testing BMC inventory via XCAT.
+
+Resource        ../lib/xcat/resource.txt
+Resource        ../lib/xcat/xcat_utils.robot
+Resource        ../lib/state_manager.robot
+
+Suite Setup     XCAT Suite Setup
+
+*** Variables ***
+
+*** Test Cases ***
+
+Verify BMC Version Via XCAT
+    [Documentation]  Verify BMC version using XCAT and REST.
+    [Tags]  Verify_BMC_Version_Via_XCAT
+
+    ${xcat_resp}=  Execute Command On XCAT  rinv  firm
+    ${rest_resp}=  Get BMC Version
+
+    Should contain  ${xcat_resp}  ${rest_resp}
+
+
+*** Keywords ***
+
+XCAT Suite Setup
+    [Documentation]  XCAT suite setup.
+
+    Open Connection And Login To XCAT
+
+    # Check if XCAT is installed.
+    ${cmd_output}=  Execute Command  ${XCAT_DIR_PATH}/lsxcatd -v
+    Should Not Be Empty  ${cmd_output}  msg=XCAT not installed.
+
+    Add Nodes To XCAT  ${OPENBMC_HOST}

edtanous/openbmc

openbmc-test-automation/xcat/test_xcat_inventory.robot
@@ -1,0 +1,38 @@
+*** Settings ***
+Documentation   Module for testing BMC inventory via XCAT.
+
+Resource        ../lib/xcat/resource.txt
+Resource        ../lib/xcat/xcat_utils.robot
+Resource        ../lib/state_manager.robot
+
+Suite Setup     XCAT Suite Setup
+
+*** Variables ***
+
+*** Test Cases ***
+
+Verify BMC Version Via XCAT
+    [Documentation]  Verify BMC version using XCAT and REST.
+    [Tags]  Verify_BMC_Version_Via_XCAT
+
+    # Get BMC version info via xcat
+    ${version_via_xcat}=  Execute Command On XCAT  rinv  firm
+
+    # Get BMC version info via rest
+    ${version_via_rest}=  Get BMC Version
+
+    Should contain  ${version_via_xcat}  ${version_via_rest}
+
+
+*** Keywords ***
+
+XCAT Suite Setup
+    [Documentation]  XCAT suite setup.
+
+    Open Connection And Login To XCAT
+
+    # Check if XCAT is installed.
+    ${cmd_output}=  Execute Command  ${XCAT_DIR_PATH}/lsxcatd -v
+    Should Not Be Empty  ${cmd_output}  msg=XCAT not installed.
+
+    Add Nodes To XCAT  ${OPENBMC_HOST}