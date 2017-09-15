openbmc/phosphor-bmc-code-mgmt

Add checks for currently running bmc image before erasing
- Check if the image to be erased is currently running on the BMC. If it
  is, fail to erase the image.

Change-Id: Ief2ba2e5e16f6664eeb429699b826cae78ef9291
Signed-off-by: Eddie James <eajames@us.ibm.com>

image_manager.cpp
@@ -184,6 +184,15 @@ void Manager::erase(std::string entryId)
     {
         return;
     }
+
+    if (it->second->isActive())
+    {
+        log<level::ERR>(("Error: Version " + entryId + \
+                         " is currently running on the BMC." \
+                         " Unable to remove.").c_str());
+        return;
+    }
+
     // Delete image dir
     fs::path imageDirPath = (*(it->second)).path();
     if (fs::exists(imageDirPath))

version.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <phosphor-logging/log.hpp>
+#include "config.h"
 #include "version.hpp"
 
 namespace phosphor
@@ -99,6 +100,11 @@ std::string Version::getBMCVersion(const std::string& releaseFilePath)
     }
 
     return version;
+}
+
+bool Version::isActive()
+{
+    return versionStr == getBMCVersion(OS_RELEASE_FILE);
 }
 
 } // namespace manager

edtanous/openbmc

phosphor-bmc-code-mgmt/image_manager.cpp
@@ -184,6 +184,15 @@ void Manager::erase(std::string entryId)
     {
         return;
     }
+
+    if (it->second->isFunctional())
+    {
+        log<level::ERR>(("Error: Version " + entryId + \
+                         " is currently running on the BMC." \
+                         " Unable to remove.").c_str());
+        return;
+    }
+
     // Delete image dir
     fs::path imageDirPath = (*(it->second)).path();
     if (fs::exists(imageDirPath))

phosphor-bmc-code-mgmt/version.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <stdexcept>
 #include <phosphor-logging/log.hpp>
+#include "config.h"
 #include "version.hpp"
 
 namespace phosphor
@@ -99,6 +100,11 @@ std::string Version::getBMCVersion(const std::string& releaseFilePath)
     }
 
     return version;
+}
+
+bool Version::isFunctional()
+{
+    return versionStr == getBMCVersion(OS_RELEASE_FILE);
 }
 
 } // namespace manager