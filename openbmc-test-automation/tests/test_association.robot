openbmc/openbmc-test-automation

Update error log and association test for log deletion
Resolve  openbmc/openbmc-test-automation#990

Change-Id: I1be65814bb30f933fc7f041344c4f9eae8b09987
Signed-off-by: George Keishing <gkeishin@in.ibm.com>

tests/test_association.robot
@@ -189,7 +189,7 @@ Set Resolved Field And Verify Callout Deletion
     [Documentation]  Set the "Resolved" error log and verify callout is deleted
     [Tags]  Set_Resolved_Field_And_Verify_Callout_Deletion
 
-    Delete Error logs
+    Delete All Error Log
     Create Test Error With Callout
     ${elog_entry}=  Get URL List  ${BMC_LOGGING_ENTRY}
     ${resp}=  OpenBMC Get Request  ${elog_entry[0]}

tests/test_error_log.robot
@@ -439,7 +439,7 @@ Verify Test Error Log
 Delete Error Logs And Verify
     [Documentation]  Delete all error logs and verify.
 
-    Delete Error Logs
+    Delete All Error Log
     ${resp}=  OpenBMC Get Request  ${BMC_LOGGING_ENTRY}/list  quiet=${1}
     Should Be Equal As Strings  ${resp.status_code}  ${HTTP_NOT_FOUND}
 

edtanous/openbmc

openbmc-test-automation/tests/test_association.robot
@@ -188,7 +188,7 @@ Set Resolved Field And Verify Callout Deletion
     [Documentation]  Set the "Resolved" error log and verify callout is deleted
     [Tags]  Set_Resolved_Field_And_Verify_Callout_Deletion
 
-    Delete Error logs
+    Delete All Error Logs
     Create Test Error With Callout
     ${elog_entry}=  Get URL List  ${BMC_LOGGING_ENTRY}
     ${resp}=  OpenBMC Get Request  ${elog_entry[0]}
@@ -271,7 +271,7 @@ Test Setup Execution
    REST Power On
    ${status}=  Run Keyword And Return Status  Callout Test Binary Exist
    Run Keyword If  ${status} == ${False}  Install Tarball
-   Delete Error Logs
+   Delete All Error Logs
 
 
 Install Tarball

openbmc-test-automation/tests/test_error_log.robot
@@ -90,7 +90,7 @@ Test Error Entry Numbering Reset On Restart
 
     Create Test Error Log
     Verify Test Error Log
-    Delete Error Logs
+    Delete All Error Logs
     Execute Command On BMC
     ...  systemctl restart xyz.openbmc_project.Logging.service
     Sleep  10s  reason=Wait for logging service to restart properly.
@@ -136,7 +136,7 @@ Create Test Error And Verify Resolved Field
     # In this test context we are making sure "Resolved" field is "0"
     # by default.
 
-    Delete Error Logs
+    Delete All Error Logs
     Create Test Error Log
     ${elog_entry}=  Get URL List  ${BMC_LOGGING_ENTRY}
     ${resolved}=  Read Attribute  ${elog_entry[0]}  Resolved
@@ -171,7 +171,7 @@ Create Test Errors And Verify Time Stamp
     #    "associations": []
     # },
 
-    Delete Error Logs
+    Delete All Error Logs
     Create Test Error Log
     Create Test Error Log
     # The error log generated is associated with the epoc time and unique
@@ -232,7 +232,7 @@ Verify Watchdog Timedout Error
     REST Power On
 
     # Clear errors if there are any.
-    Delete Error Logs
+    Delete All Error Logs
 
     Trigger Host Watchdog Error
 
@@ -438,7 +438,7 @@ Verify Test Error Log
 Delete Error Logs And Verify
     [Documentation]  Delete all error logs and verify.
 
-    Delete Error Logs
+    Delete All Error Logs
     ${resp}=  OpenBMC Get Request  ${BMC_LOGGING_ENTRY}/list  quiet=${1}
     Should Be Equal As Strings  ${resp.status_code}  ${HTTP_NOT_FOUND}
 
@@ -467,5 +467,5 @@ Post Test Case Execution
    # 4. Clear all REST sessions.
 
    FFDC On Test Case Fail
-   Delete Error Logs
+   Delete All Error Logs
    Close All Connections