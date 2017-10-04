openbmc/openpower-proc-control

Add FSI scan procedure
This procedure will perform both the FSI master
and FSI hub scans.

It replaces doing the scans by writing to sysfs
using the echo command so that real errors can
be created on failures.

Currently the only way to tell if the master scan
fails is by checking for missing sysfs files, and
this code will do that by checking for the hub
scan file.

Change-Id: Id4b91592b8c8b9a5fc9f1a56f4d89e142a6c6b74
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

proc_control.cpp
@@ -21,11 +21,13 @@
 #include "registration.hpp"
 #include "elog-errors.hpp"
 #include <org/open_power/Proc/CFAM/error.hpp>
+#include <org/open_power/Proc/FSI/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 
 using namespace openpower::util;
 namespace cfam = sdbusplus::org::open_power::Proc::CFAM::Error;
 namespace common = sdbusplus::xyz::openbmc_project::Common::Error;
+namespace fsi = sdbusplus::org::open_power::Proc::FSI::Error;
 
 void usage(char** argv, const ProcedureMap& procedures)
 {
@@ -88,6 +90,17 @@ int main(int argc, char** argv)
         commit<common::InvalidArgument>();
         return -1;
     }
+    catch (fsi::MasterDetectionFailure& e)
+    {
+        commit<fsi::MasterDetectionFailure>();
+        return -1;
+    }
+    catch (fsi::SlaveDetectionFailure& e)
+    {
+        commit<fsi::SlaveDetectionFailure>();
+        return -1;
+    }
+
 
     return 0;
 }

edtanous/openbmc

openpower-proc-control/proc_control.cpp
@@ -19,6 +19,7 @@
 #include <phosphor-logging/log.hpp>
 #include <phosphor-logging/elog.hpp>
 #include <phosphor-logging/elog-errors.hpp>
+#include <org/open_power/Proc/FSI/error.hpp>
 #include <xyz/openbmc_project/Common/error.hpp>
 #include <xyz/openbmc_project/Common/Device/error.hpp>
 #include <xyz/openbmc_project/Common/File/error.hpp>
@@ -31,6 +32,7 @@ namespace device_error = sdbusplus::xyz::openbmc_project::
         Common::Device::Error;
 namespace file_error = sdbusplus::xyz::openbmc_project::
         Common::File::Error;
+namespace fsi_error = sdbusplus::org::open_power::Proc::FSI::Error;
 
 void usage(char** argv, const ProcedureMap& procedures)
 {
@@ -93,6 +95,17 @@ int main(int argc, char** argv)
         commit<common_error::InvalidArgument>();
         return -1;
     }
+    catch (fsi_error::MasterDetectionFailure& e)
+    {
+        commit<fsi_error::MasterDetectionFailure>();
+        return -1;
+    }
+    catch (fsi_error::SlaveDetectionFailure& e)
+    {
+        commit<fsi_error::SlaveDetectionFailure>();
+        return -1;
+    }
+
 
     return 0;
 }