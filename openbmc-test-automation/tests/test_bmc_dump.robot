openbmc/openbmc-test-automation

Added TCs to trigger dump at host off and booted state.
Resolves openbmc/openbmc-test-automation#1058

Change-Id: I91963e6a5f8c95ceef8c48328857ec02f6eea984
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

tests/test_bmc_dump.robot
@@ -24,11 +24,21 @@ Pre Dump BMC Performance Test
     Open Connection And Log In
     Check BMC Performance
 
-Verify User Initiated BMC Dump
-    [Documentation]  Create user initiated BMC dump and verify dump
-    ...  entry for it.
-    [Tags]  Verify_User_Initiated_Dump
+Verify User Initiated BMC Dump At Host Off
+    [Documentation]  Create user initiated BMC dump at host off state and
+    ...  verify dump entry for it.
+    [Tags]  Verify_User_Initiated_Dump_At_Host_Off
 
+    REST Power Off  stack_mode=skip  quiet=1
+    Create User Initiated Dump
+
+
+Verify User Initiated BMC Dump At Host Booted
+    [Documentation]  Create user initiated BMC dump at host booted state and
+    ...  verify dump entry for it.
+    [Tags]  Verify_User_Initiated_Dump_At_Host_Booted
+
+    REST Power On  stack_mode=skip  quiet=1
     Create User Initiated Dump
 
 

edtanous/openbmc

openbmc-test-automation/tests/test_bmc_dump.robot
@@ -24,11 +24,21 @@ Pre Dump BMC Performance Test
     Open Connection And Log In
     Check BMC Performance
 
-Verify User Initiated BMC Dump
-    [Documentation]  Create user initiated BMC dump and verify dump
-    ...  entry for it.
-    [Tags]  Verify_User_Initiated_Dump
+Verify User Initiated BMC Dump When Powered Off
+    [Documentation]  Create user initiated BMC dump at host off state and
+    ...  verify dump entry for it.
+    [Tags]  Verify_User_Initiated_BMC_Dump_When_Powered_Off
 
+    REST Power Off  stack_mode=skip  quiet=1
+    Create User Initiated Dump
+
+
+Verify User Initiated BMC Dump When Host Booted
+    [Documentation]  Create user initiated BMC dump at host booted state and
+    ...  verify dump entry for it.
+    [Tags]  Verify_User_Initiated_BMC_Dump_When_Host_Booted
+
+    REST Power On  stack_mode=skip  quiet=1
     Create User Initiated Dump
 
 Verify Dump Persistency On Service Restart