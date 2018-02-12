openbmc/phosphor-host-ipmid

Inventory:- making changes in the mako and the C++ structure
As fru yaml would be having extra properties like
entityID,entityInstance so changes were needed to make the
change in the corresponding c++ structure.

Change-Id: I314d1556cf07a3e212996615c1cd2f28fb6d9bba
Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

read_fru_data.cpp
@@ -79,19 +79,14 @@ void processFruPropChange(sdbusplus::message::message& msg)
     {
         bool found = false;
         auto& fruId = fru.first;
-        auto& instanceList = fru.second;
-        for (auto& instance : instanceList)
+        auto& fruInfo = fru.second;
+        if (fruInfo.instancePath == path)
         {
-            if(instance.first == path)
+            if (found)
             {
-                found = true;
+                cache::fruMap.erase(fruId);
                 break;
             }
-        }
-        if (found)
-        {
-            cache::fruMap.erase(fruId);
-            break;
         }
     }
 }
@@ -130,13 +125,11 @@ FruInventoryData readDataFromInventory(const FRUId& fruNum)
     }
 
     FruInventoryData data;
-    auto& instanceList = iter->second;
-    for (auto& instance : instanceList)
-    {
-        for (auto& intf : instance.second)
+    auto& fruInfo = iter->second;
+        for (auto& intf : fruInfo.interfaces)
         {
             ipmi::PropertyMap allProp = readAllProperties(
-                    intf.first, instance.first);
+                    intf.first, fruInfo.instancePath);
             for (auto& properties : intf.second)
             {
                 auto iter = allProp.find(properties.first);
@@ -147,7 +140,6 @@ FruInventoryData readDataFromInventory(const FRUId& fruNum)
                 }
             }
         }
-    }
     return data;
 }
 

edtanous/openbmc

phosphor-host-ipmid/read_fru_data.cpp
@@ -82,7 +82,7 @@ void processFruPropChange(sdbusplus::message::message& msg)
         auto& instanceList = fru.second;
         for (auto& instance : instanceList)
         {
-            if(instance.first == path)
+            if(instance.path == path)
             {
                 found = true;
                 break;
@@ -133,10 +133,10 @@ FruInventoryData readDataFromInventory(const FRUId& fruNum)
     auto& instanceList = iter->second;
     for (auto& instance : instanceList)
     {
-        for (auto& intf : instance.second)
+        for (auto& intf : instance.interfaces)
         {
             ipmi::PropertyMap allProp = readAllProperties(
-                    intf.first, instance.first);
+                    intf.first, instance.path);
             for (auto& properties : intf.second)
             {
                 auto iter = allProp.find(properties.first);