openbmc/openbmc-test-automation

Verify get DCMI capabilities.
Resolves openbmc/openbmc-test-automation#1192

Change-Id: I18f84badcd2cc80a6e444ab3811664e586d20a30
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

tests/ipmi/test_general_ipmi.robot
@@ -146,6 +146,37 @@ Test Watchdog Off Via IPMI And Verify Using REST
     ${watchdog_state}=  Read Attribute  ${HOST_WATCHDOG_URI}  Enabled
     Should Be Equal  ${watchdog_state}  ${0}
     ...  msg=msg=Verification failed for watchdog off check.
+
+
+Verify Get DCMI Capabilities
+    [Documentation]  Verify get DCMI capabilities command output.
+    [Tags]  Verify_Get_DCMI_Capabilities
+
+    ${cmd_output}=  Run IPMI Standard Command  dcmi discover
+
+    @{supported_capabilities}=  Create List
+    # Supported DCMI capabilities:
+    ...  Mandatory platform capabilties
+    ...  Optional platform capabilties
+    ...  Power management available
+    ...  Managebility access capabilties
+    ...  In-band KCS channel available
+    # Mandatory platform attributes:
+    ...  200 SEL entries
+    ...  SEL automatic rollover is enabled
+    # Optional Platform Attributes:
+    ...  Slave address of device: 0h (8bits)(Satellite/External controller)
+    ...  Channel number is 0h (Primary BMC)
+    ...  Device revision is 0
+    # Manageability Access Attributes:
+    ...  Primary LAN channel is available for OOB
+    ...  Secondary LAN channel is not available for OOB
+    ...  No serial channel is available
+
+    :FOR  ${capability}  IN  @{supported_capabilities}
+    \  Should Contain  ${cmd_output}  ${capability}  case_insensitive=True
+
+
 *** Keywords ***
 
 Set Management Controller ID String

edtanous/openbmc

openbmc-test-automation/tests/ipmi/test_general_ipmi.robot
@@ -205,6 +205,36 @@ Test Ambient Temperature Via IPMI
     ...  msg=Ambient temperature above allowed threshold ${allowed_temp_diff}.
 
 
+Verify Get DCMI Capabilities
+    [Documentation]  Verify get DCMI capabilities command output.
+    [Tags]  Verify_Get_DCMI_Capabilities
+
+    ${cmd_output}=  Run IPMI Standard Command  dcmi discover
+
+    @{supported_capabilities}=  Create List
+    # Supported DCMI capabilities:
+    ...  Mandatory platform capabilties
+    ...  Optional platform capabilties
+    ...  Power management available
+    ...  Managebility access capabilties
+    ...  In-band KCS channel available
+    # Mandatory platform attributes:
+    ...  200 SEL entries
+    ...  SEL automatic rollover is enabled
+    # Optional Platform Attributes:
+    ...  Slave address of device: 0h (8bits)(Satellite/External controller)
+    ...  Channel number is 0h (Primary BMC)
+    ...  Device revision is 0
+    # Manageability Access Attributes:
+    ...  Primary LAN channel number: 1 is available
+    ...  Secondary LAN channel is not available for OOB
+    ...  No serial channel is available
+
+    :FOR  ${capability}  IN  @{supported_capabilities}
+    \  Should Contain  ${cmd_output}  ${capability}  ignore_case=True
+    ...  msg=Supported DCMI capabilities not present.
+
+
 *** Keywords ***
 
 Set Management Controller ID String