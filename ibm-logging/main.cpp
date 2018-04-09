openbmc/ibm-logging

Start the D-Bus loop in main()
Instantiate the Manager class as well as a D-Bus
object manager, and start the D-Bus processing loop.

Change-Id: Ide5adf8774739a259ca68f3a1bdd76a1dde3c610
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

main.cpp
@@ -13,8 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include "manager.hpp"
+#include "config.h"
 
 int main()
 {
+    auto bus = sdbusplus::bus::new_default();
+
+    sdbusplus::server::manager::manager objManager(bus, IBM_LOGGING_PATH);
+
+    ibm::logging::Manager manager{bus};
+
+    bus.request_name(IBM_LOGGING_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
     return 0;
 }

edtanous/openbmc

ibm-logging/main.cpp
@@ -13,8 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/manager.hpp>
+#include "manager.hpp"
+#include "config.h"
 
 int main()
 {
+    auto bus = sdbusplus::bus::new_default();
+
+    sdbusplus::server::manager::manager objManager(bus, LOGGING_PATH);
+
+    ibm::logging::Manager manager{bus};
+
+    bus.request_name(IBM_LOGGING_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
     return 0;
 }