openbmc/ibm-logging

Find an entry in the policy table
Find the policy details entry based on the error message
and a modifier string.

Not all details entries have a modifier, and if it is empty
it will match on anything.

Change-Id: I86c2901cc8e2172959d8e8c674f0082b7c8f91d9
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

policy_table.cpp
@@ -76,6 +76,38 @@ void Table::load(const std::string& jsonFile)
     }
 }
 
+optional_ns::optional<DetailsIterator> Table::find(
+        const std::string& error,
+        const std::string& modifier) const
+{
+    //First find the entry based on the error, and then find which
+    //underlying details object it is with the help of the modifier.
+
+    auto policy = policies.find(error);
+
+    if (policy != policies.end())
+    {
+        //Not all policy entries have a modifier defined, so if it is
+        //empty that will return as a match.
+
+        auto details = std::find_if(
+                policy->second.begin(),
+                policy->second.end(),
+                [&modifier](const auto& d)
+                {
+                    return d.modifier.empty() || (modifier == d.modifier);
+                });
+
+        if (details != policy->second.end())
+        {
+            return details;
+        }
+    }
+
+    return {};
+}
+
+
 }
 }
 }

edtanous/openbmc

ibm-logging/policy_table.cpp
@@ -76,6 +76,38 @@ void Table::load(const std::string& jsonFile)
     }
 }
 
+optional_ns::optional<DetailsReference> Table::find(
+        const std::string& error,
+        const std::string& modifier) const
+{
+    //First find the entry based on the error, and then find which
+    //underlying details object it is with the help of the modifier.
+
+    auto policy = policies.find(error);
+
+    if (policy != policies.end())
+    {
+        //Not all policy entries have a modifier defined, so if it is
+        //empty that will return as a match.
+
+        auto details = std::find_if(
+                policy->second.begin(),
+                policy->second.end(),
+                [&modifier](const auto& d)
+                {
+                    return d.modifier.empty() || (modifier == d.modifier);
+                });
+
+        if (details != policy->second.end())
+        {
+            return DetailsReference(*details);
+        }
+    }
+
+    return {};
+}
+
+
 }
 }
 }