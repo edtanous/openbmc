openbmc/ibm-logging

Enable clang-format
Ideally would have done this from the beginning,
but better later than never.

Change-Id: Ib4c73085c4817496c9f2ee505c19149a67b394dc
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

policy_table.cpp
@@ -38,7 +38,7 @@ Table::Table(const std::string& jsonFile)
     else
     {
         log<level::INFO>("Policy table JSON file does not exist",
-                entry("FILE=%s", jsonFile.c_str()));
+                         entry("FILE=%s", jsonFile.c_str()));
     }
 }
 
@@ -70,33 +70,30 @@ void Table::load(const std::string& jsonFile)
     catch (std::exception& e)
     {
         log<level::ERR>("Failed loading policy table json file",
-                entry("FILE=%s", jsonFile.c_str()),
-                entry("ERROR=%s", e.what()));
+                        entry("FILE=%s", jsonFile.c_str()),
+                        entry("ERROR=%s", e.what()));
         loaded = false;
     }
 }
 
-optional_ns::optional<DetailsIterator> Table::find(
-        const std::string& error,
-        const std::string& modifier) const
+optional_ns::optional<DetailsIterator>
+    Table::find(const std::string& error, const std::string& modifier) const
 {
-    //First find the entry based on the error, and then find which
-    //underlying details object it is with the help of the modifier.
+    // First find the entry based on the error, and then find which
+    // underlying details object it is with the help of the modifier.
 
     auto policy = policies.find(error);
 
     if (policy != policies.end())
     {
-        //Not all policy entries have a modifier defined, so if it is
-        //empty that will return as a match.
+        // Not all policy entries have a modifier defined, so if it is
+        // empty that will return as a match.
 
         auto details = std::find_if(
-                policy->second.begin(),
-                policy->second.end(),
-                [&modifier](const auto& d)
-                {
-                    return d.modifier.empty() || (modifier == d.modifier);
-                });
+            policy->second.begin(), policy->second.end(),
+            [&modifier](const auto& d) {
+                return d.modifier.empty() || (modifier == d.modifier);
+            });
 
         if (details != policy->second.end())
         {
@@ -106,8 +103,6 @@ optional_ns::optional<DetailsIterator> Table::find(
 
     return {};
 }
-
-
 }
 }
 }

policy_table.hpp
@@ -12,7 +12,6 @@ namespace logging
 namespace policy
 {
 
-
 /**
  *  The details of a policy table entry:
  *  - search modifier
@@ -43,90 +42,85 @@ namespace optional_ns = std::experimental;
  */
 class Table
 {
-    public:
+  public:
+    Table() = delete;
+    ~Table() = default;
+    Table(const Table&) = default;
+    Table& operator=(const Table&) = default;
+    Table(Table&&) = default;
+    Table& operator=(Table&&) = default;
 
-        Table() = delete;
-        ~Table() = default;
-        Table(const Table&) = default;
-        Table& operator=(const Table&) = default;
-        Table(Table&&) = default;
-        Table& operator=(Table&&) = default;
+    /**
+     * Constructor
+     *
+     * @param[in] jsonFile - the path to the policy JSON.
+     */
+    explicit Table(const std::string& jsonFile);
 
-        /**
-         * Constructor
-         *
-         * @param[in] jsonFile - the path to the policy JSON.
-         */
-        explicit Table(const std::string& jsonFile);
+    /**
+     * Says if the JSON has been loaded successfully.
+     *
+     * @return bool
+     */
+    inline bool isLoaded() const
+    {
+        return loaded;
+    }
 
-        /**
-         * Says if the JSON has been loaded successfully.
-         *
-         * @return bool
-         */
-        inline bool isLoaded() const
-        {
-            return loaded;
-        }
+    /**
+     * Finds an entry in the policy table based on the
+     * error and the search modifier.
+     *
+     * @param[in] error - the error, like xyz.openbmc_project.Error.X
+     * @param[in] modifier - the search modifier, used to find the entry
+     *                   when multiple ones share the same error
+     *
+     * @return optional<DetailsIterator> - an interator to the details
+     *                                     entry
+     */
+    optional_ns::optional<DetailsIterator>
+        find(const std::string& error, const std::string& modifier) const;
 
-        /**
-         * Finds an entry in the policy table based on the
-         * error and the search modifier.
-         *
-         * @param[in] error - the error, like xyz.openbmc_project.Error.X
-         * @param[in] modifier - the search modifier, used to find the entry
-         *                   when multiple ones share the same error
-         *
-         * @return optional<DetailsIterator> - an interator to the details
-         *                                     entry
-         */
-        optional_ns::optional<DetailsIterator> find(
-                const std::string& error,
-                const std::string& modifier) const;
+    /**
+     * The default event ID to use when a match in the table
+     * wasn't found.
+     *
+     * @return std::string
+     */
+    inline std::string defaultEID() const
+    {
+        return DEFAULT_POLICY_EID;
+    }
 
-        /**
-         * The default event ID to use when a match in the table
-         * wasn't found.
-         *
-         * @return std::string
-         */
-        inline std::string defaultEID() const
-        {
-            return DEFAULT_POLICY_EID;
-        }
+    /**
+     * The default error message to use when a match in the table
+     * wasn't found.
+     *
+     * @return std::string
+     */
+    inline std::string defaultMsg() const
+    {
+        return DEFAULT_POLICY_MSG;
+    }
 
-        /**
-         * The default error message to use when a match in the table
-         * wasn't found.
-         *
-         * @return std::string
-         */
-        inline std::string defaultMsg() const
-        {
-            return DEFAULT_POLICY_MSG;
-        }
+  private:
+    /**
+     * Loads the JSON data into the PolicyMap map
+     *
+     * @param[in] jsonFile - the path to the .json file
+     */
+    void load(const std::string& jsonFile);
 
-    private:
+    /**
+     * Reflects if the JSON was successfully loaded or not.
+     */
+    bool loaded = false;
 
-        /**
-         * Loads the JSON data into the PolicyMap map
-         *
-         * @param[in] jsonFile - the path to the .json file
-         */
-        void load(const std::string& jsonFile);
-
-        /**
-         * Reflects if the JSON was successfully loaded or not.
-         */
-        bool loaded = false;
-
-        /**
-         * The policy table
-         */
-        PolicyMap policies;
+    /**
+     * The policy table
+     */
+    PolicyMap policies;
 };
-
-
 }
 }
 }

edtanous/openbmc

ibm-logging/policy_table.cpp
@@ -38,7 +38,7 @@ Table::Table(const std::string& jsonFile)
     else
     {
         log<level::INFO>("Policy table JSON file does not exist",
-                entry("FILE=%s", jsonFile.c_str()));
+                         entry("FILE=%s", jsonFile.c_str()));
     }
 }
 
@@ -70,33 +70,30 @@ void Table::load(const std::string& jsonFile)
     catch (std::exception& e)
     {
         log<level::ERR>("Failed loading policy table json file",
-                entry("FILE=%s", jsonFile.c_str()),
-                entry("ERROR=%s", e.what()));
+                        entry("FILE=%s", jsonFile.c_str()),
+                        entry("ERROR=%s", e.what()));
         loaded = false;
     }
 }
 
-optional_ns::optional<DetailsReference> Table::find(
-        const std::string& error,
-        const std::string& modifier) const
+optional_ns::optional<DetailsReference>
+    Table::find(const std::string& error, const std::string& modifier) const
 {
-    //First find the entry based on the error, and then find which
-    //underlying details object it is with the help of the modifier.
+    // First find the entry based on the error, and then find which
+    // underlying details object it is with the help of the modifier.
 
     auto policy = policies.find(error);
 
     if (policy != policies.end())
     {
-        //Not all policy entries have a modifier defined, so if it is
-        //empty that will return as a match.
+        // Not all policy entries have a modifier defined, so if it is
+        // empty that will return as a match.
 
         auto details = std::find_if(
-                policy->second.begin(),
-                policy->second.end(),
-                [&modifier](const auto& d)
-                {
-                    return d.modifier.empty() || (modifier == d.modifier);
-                });
+            policy->second.begin(), policy->second.end(),
+            [&modifier](const auto& d) {
+                return d.modifier.empty() || (modifier == d.modifier);
+            });
 
         if (details != policy->second.end())
         {
@@ -106,8 +103,6 @@ optional_ns::optional<DetailsReference> Table::find(
 
     return {};
 }
-
-
 }
 }
 }

ibm-logging/policy_table.hpp
@@ -12,7 +12,6 @@ namespace logging
 namespace policy
 {
 
-
 /**
  *  The details of a policy table entry:
  *  - search modifier
@@ -43,98 +42,94 @@ namespace optional_ns = std::experimental;
  */
 class Table
 {
-    public:
+  public:
+    Table() = delete;
+    ~Table() = default;
+    Table(const Table&) = default;
+    Table& operator=(const Table&) = default;
+    Table(Table&&) = default;
+    Table& operator=(Table&&) = default;
 
-        Table() = delete;
-        ~Table() = default;
-        Table(const Table&) = default;
-        Table& operator=(const Table&) = default;
-        Table(Table&&) = default;
-        Table& operator=(Table&&) = default;
+    /**
+     * Constructor
+     *
+     * @param[in] jsonFile - the path to the policy JSON.
+     */
+    explicit Table(const std::string& jsonFile);
 
-        /**
-         * Constructor
-         *
-         * @param[in] jsonFile - the path to the policy JSON.
-         */
-        explicit Table(const std::string& jsonFile);
+    /**
+     * Says if the JSON has been loaded successfully.
+     *
+     * @return bool
+     */
+    inline bool isLoaded() const
+    {
+        return loaded;
+    }
 
-        /**
-         * Says if the JSON has been loaded successfully.
-         *
-         * @return bool
-         */
-        inline bool isLoaded() const
-        {
-            return loaded;
-        }
+    /**
+     * Finds an entry in the policy table based on the
+     * error and the search modifier.
+     *
+     * @param[in] error - the error, like xyz.openbmc_project.Error.X
+     * @param[in] modifier - the search modifier, used to find the entry
+     *                   when multiple ones share the same error
+     *
+     * @return optional<DetailsReference> - the details entry
+     */
+    optional_ns::optional<DetailsReference>
+        find(const std::string& error, const std::string& modifier) const;
 
-        /**
-         * Finds an entry in the policy table based on the
-         * error and the search modifier.
-         *
-         * @param[in] error - the error, like xyz.openbmc_project.Error.X
-         * @param[in] modifier - the search modifier, used to find the entry
-         *                   when multiple ones share the same error
-         *
-         * @return optional<DetailsReference> - the details entry
-         */
-        optional_ns::optional<DetailsReference> find(
-                const std::string& error,
-                const std::string& modifier) const;
+    /**
+     * The default event ID to use when a match in the table
+     * wasn't found.
+     *
+     * @return std::string
+     */
+    inline std::string defaultEID() const
+    {
+        return defaultPolicyEID;
+    }
 
-        /**
-         * The default event ID to use when a match in the table
-         * wasn't found.
-         *
-         * @return std::string
-         */
-        inline std::string defaultEID() const
-        {
-            return defaultPolicyEID;
-        }
+    /**
+     * The default error message to use when a match in the table
+     * wasn't found.
+     *
+     * @return std::string
+     */
+    inline std::string defaultMsg() const
+    {
+        return defaultPolicyMessage;
+    }
 
-        /**
-         * The default error message to use when a match in the table
-         * wasn't found.
-         *
-         * @return std::string
-         */
-        inline std::string defaultMsg() const
-        {
-            return defaultPolicyMessage;
-        }
+  private:
+    /**
+     * The default event ID
+     */
+    const std::string defaultPolicyEID{DEFAULT_POLICY_EID};
 
-    private:
-        /**
-         * The default event ID
-         */
-        const std::string defaultPolicyEID{DEFAULT_POLICY_EID};
+    /**
+     * The default event message
+     */
+    const std::string defaultPolicyMessage{DEFAULT_POLICY_MSG};
 
-        /**
-         * The default event message
-         */
-        const std::string defaultPolicyMessage{DEFAULT_POLICY_MSG};
+    /**
+     * Loads the JSON data into the PolicyMap map
+     *
+     * @param[in] jsonFile - the path to the .json file
+     */
+    void load(const std::string& jsonFile);
 
-        /**
-         * Loads the JSON data into the PolicyMap map
-         *
-         * @param[in] jsonFile - the path to the .json file
-         */
-        void load(const std::string& jsonFile);
+    /**
+     * Reflects if the JSON was successfully loaded or not.
+     */
+    bool loaded = false;
 
-        /**
-         * Reflects if the JSON was successfully loaded or not.
-         */
-        bool loaded = false;
-
-        /**
-         * The policy table
-         */
-        PolicyMap policies;
+    /**
+     * The policy table
+     */
+    PolicyMap policies;
 };
-
-
 }
 }
 }