openbmc/openbmc-test-automation

SNMP Automation.
This change includes:
    1. Configuration of SNMP manager with default and non default ports
    2. Configuration of Multiple SNMP managers with default and non default ports
    3. Deconfigure SNMP manager and delete SNMP rest object
    4. Deconfigure multiple SNMP manager and delete SNMP rest objects
    5. Configure SNMP manager with invalid IP and invalid port.

This resolves openbmc/openbmc-test-automation#1391

Change-Id: Ic1e2e6f7488b87420b936adf23848f6259654423
Signed-off-by: Prashanth Katti <prkatti1@in.ibm.com>

data/variables.py
@@ -16,7 +16,8 @@ SETTINGS_URI = '/org/openbmc/settings/'
 WATCHDOG_URI = OPENBMC_BASE_URI + 'watchdog/'
 TIME_MANAGER_URI = OPENBMC_BASE_URI + 'time/'
 NETWORK_MANAGER = OPENBMC_BASE_URI + 'network/'
-
+# SNMP
+SNMP_MANAGER = NETWORK_MANAGER + 'snmp/manager/'
 # Sensors base variables.
 SENSORS_URI = OPENBMC_BASE_URI + 'sensors/'
 

edtanous/openbmc

openbmc-test-automation/data/variables.py
@@ -16,7 +16,8 @@ SETTINGS_URI = '/org/openbmc/settings/'
 WATCHDOG_URI = OPENBMC_BASE_URI + 'watchdog/'
 TIME_MANAGER_URI = OPENBMC_BASE_URI + 'time/'
 NETWORK_MANAGER = OPENBMC_BASE_URI + 'network/'
-
+# SNMP
+SNMP_MANAGER_URI = NETWORK_MANAGER + 'snmp/manager/'
 # Sensors base variables.
 SENSORS_URI = OPENBMC_BASE_URI + 'sensors/'
 