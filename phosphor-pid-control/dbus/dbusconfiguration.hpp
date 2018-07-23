openbmc/phosphor-pid-control

Add ability to configure from dbus
This allows configuration of PID control from dbus using
entity manager. Sample configuration will be pushed to
entity-manager repo shortly.

Tested-by: Used yaml configuration and dbus / entity-manager
           based configuration and pid seemed to work the same.
	   Verified printout of configuration matched generated
	   cpp files.

Change-Id: Ia7b016e53262791ffcccdb9b21c1ccddae2926bc
Signed-off-by: James Feist <james.feist@linux.intel.com>

dbus/dbusconfiguration.hpp
@@ -1,0 +1,24 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+#include <conf.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace dbusConfiguration
+{
+void init(sdbusplus::bus::bus &bus);
+} // namespace dbusConfiguration

dbus/dbuswrite.cpp
@@ -1,0 +1,71 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include <dbus/dbuswrite.hpp>
+#include <iostream>
+#include <sdbusplus/bus.hpp>
+
+std::unique_ptr<sdbusplus::bus::bus> writeBus = nullptr;
+
+// since we are in the write thread we need access to our own bus
+// todo: if this is written using async this won't be needed
+void initBus()
+{
+    if (writeBus == nullptr)
+    {
+        writeBus = std::make_unique<sdbusplus::bus::bus>(
+            sdbusplus::bus::new_default());
+    }
+}
+
+void DbusWritePercent::write(double value)
+{
+    float minimum = getMin();
+    float maximum = getMax();
+
+    float range = maximum - minimum;
+    float offset = range * value;
+    float ovalue = offset + minimum;
+    initBus();
+    auto mesg =
+        writeBus->new_method_call(_connectionName.c_str(), _path.c_str(),
+                                  "org.freedesktop.DBus.Properties", "Set");
+    mesg.append(pwmInterface, "Target",
+                sdbusplus::message::variant<uint64_t>(ovalue));
+    auto resp = writeBus->call(mesg);
+    if (resp.is_method_error())
+    {
+        std::cerr << "Error sending message to " << _path << "\n";
+    }
+    return;
+}
+
+void DbusWrite::write(double value)
+{
+    initBus();
+    auto mesg =
+        writeBus->new_method_call(_connectionName.c_str(), _path.c_str(),
+                                  "org.freedesktop.DBus.Properties", "Set");
+    mesg.append(pwmInterface, "Target",
+                sdbusplus::message::variant<uint64_t>(value));
+    auto resp = writeBus->call(mesg);
+    if (resp.is_method_error())
+    {
+        std::cerr << "Error sending message to " << _path << "\n";
+    }
+
+    return;
+}

dbus/dbuswrite.hpp
@@ -1,0 +1,63 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+#include <string>
+
+#include <dbus/util.hpp>
+#include <interfaces.hpp>
+#include <sdbusplus/bus.hpp>
+
+constexpr const char *pwmInterface = "xyz.openbmc_project.Control.FanPwm";
+
+class DbusWritePercent : public WriteInterface
+{
+  public:
+    DbusWritePercent(const std::string &path, int64_t min, int64_t max,
+                     DbusHelperInterface &helper) :
+        WriteInterface(min, max),
+        _path(path)
+    {
+        auto tempBus = sdbusplus::bus::new_default();
+        _connectionName = helper.GetService(tempBus, pwmInterface, path);
+    }
+
+    void write(double value) override;
+
+  private:
+    std::string _path;
+    std::string _connectionName;
+};
+
+class DbusWrite : public WriteInterface
+{
+  public:
+    DbusWrite(const std::string &path, int64_t min, int64_t max,
+              DbusHelperInterface &helper) :
+        WriteInterface(min, max),
+        _path(path)
+    {
+        auto tempBus = sdbusplus::bus::new_default();
+        _connectionName = helper.GetService(tempBus, pwmInterface, path);
+    }
+
+    void write(double value) override;
+
+  private:
+    std::string _path;
+    std::string _connectionName;
+};

main.cpp
@@ -29,6 +29,8 @@
 
 /* Configuration. */
 #include "conf.hpp"
+#include "config.h"
+#include <dbus/dbusconfiguration.hpp>
 
 /* Misc. */
 #include "util.hpp"
@@ -88,6 +90,10 @@ int main(int argc, char* argv[])
     }
 
     auto ModeControlBus = sdbusplus::bus::new_default();
+    if (configureDbus)
+    {
+        dbusConfiguration::init(ModeControlBus);
+    }
     SensorManager mgmr;
     std::unordered_map<int64_t, std::unique_ptr<PIDZone>> zones;
 

sensors/builder.cpp
@@ -22,6 +22,7 @@
 #include "conf.hpp"
 
 #include "dbus/dbuspassive.hpp"
+#include "dbus/dbuswrite.hpp"
 #include "interfaces.hpp"
 #include "notimpl/readonly.hpp"
 #include "notimpl/writeonly.hpp"
@@ -101,6 +102,23 @@ SensorManager BuildSensors(
                                  info->writepath,
                                  info->min,
                                  info->max);
+                    }
+
+                    break;
+                case IOInterfaceType::DBUSPASSIVE:
+                    if (info->max > 0)
+                    {
+                        wi = std::make_unique<DbusWritePercent>(info->writepath,
+                                                                info->min,
+                                                                info->max,
+                                                                helper);
+                    }
+                    else
+                    {
+                        wi = std::make_unique<DbusWrite>(info->writepath,
+                                                        info->min,
+                                                        info->max,
+                                                        helper);
                     }
 
                     break;

edtanous/openbmc

phosphor-pid-control/dbus/dbusconfiguration.hpp
@@ -1,0 +1,24 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+#include <conf.hpp>
+#include <sdbusplus/bus.hpp>
+
+namespace dbus_configuration
+{
+void init(sdbusplus::bus::bus &bus);
+} // namespace dbus_configuration

phosphor-pid-control/dbus/dbuswrite.cpp
@@ -1,0 +1,72 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include <dbus/dbuswrite.hpp>
+#include <iostream>
+#include <sdbusplus/bus.hpp>
+
+// this bus object is treated as a singleton because the class is constructed in
+// a different thread than it is used, and as bus objects are relatively
+// expensive we'd prefer to only have one
+std::unique_ptr<sdbusplus::bus::bus> writeBus = nullptr;
+
+void initBus()
+{
+    if (writeBus == nullptr)
+    {
+        writeBus = std::make_unique<sdbusplus::bus::bus>(
+            sdbusplus::bus::new_default());
+    }
+}
+
+void DbusWritePercent::write(double value)
+{
+    float minimum = getMin();
+    float maximum = getMax();
+
+    float range = maximum - minimum;
+    float offset = range * value;
+    float ovalue = offset + minimum;
+    initBus();
+    auto mesg =
+        writeBus->new_method_call(connectionName.c_str(), path.c_str(),
+                                  "org.freedesktop.DBus.Properties", "Set");
+    mesg.append(pwmInterface, "Target",
+                sdbusplus::message::variant<uint64_t>(ovalue));
+    auto resp = writeBus->call(mesg);
+    if (resp.is_method_error())
+    {
+        std::cerr << "Error sending message to " << path << "\n";
+    }
+    return;
+}
+
+void DbusWrite::write(double value)
+{
+    initBus();
+    auto mesg =
+        writeBus->new_method_call(connectionName.c_str(), path.c_str(),
+                                  "org.freedesktop.DBus.Properties", "Set");
+    mesg.append(pwmInterface, "Target",
+                sdbusplus::message::variant<uint64_t>(value));
+    auto resp = writeBus->call(mesg);
+    if (resp.is_method_error())
+    {
+        std::cerr << "Error sending message to " << path << "\n";
+    }
+
+    return;
+}

phosphor-pid-control/dbus/dbuswrite.hpp
@@ -1,0 +1,63 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#pragma once
+
+#include <string>
+
+#include <dbus/util.hpp>
+#include <interfaces.hpp>
+#include <sdbusplus/bus.hpp>
+
+constexpr const char *pwmInterface = "xyz.openbmc_project.Control.FanPwm";
+
+class DbusWritePercent : public WriteInterface
+{
+  public:
+    DbusWritePercent(const std::string &path, int64_t min, int64_t max,
+                     DbusHelperInterface &helper) :
+        WriteInterface(min, max),
+        path(path)
+    {
+        auto tempBus = sdbusplus::bus::new_default();
+        connectionName = helper.GetService(tempBus, pwmInterface, path);
+    }
+
+    void write(double value) override;
+
+  private:
+    std::string path;
+    std::string connectionName;
+};
+
+class DbusWrite : public WriteInterface
+{
+  public:
+    DbusWrite(const std::string &path, int64_t min, int64_t max,
+              DbusHelperInterface &helper) :
+        WriteInterface(min, max),
+        path(path)
+    {
+        auto tempBus = sdbusplus::bus::new_default();
+        connectionName = helper.GetService(tempBus, pwmInterface, path);
+    }
+
+    void write(double value) override;
+
+  private:
+    std::string path;
+    std::string connectionName;
+};

phosphor-pid-control/main.cpp
@@ -29,6 +29,8 @@
 
 /* Configuration. */
 #include "conf.hpp"
+#include "config.h"
+#include <dbus/dbusconfiguration.hpp>
 
 /* Misc. */
 #include "util.hpp"
@@ -88,6 +90,10 @@ int main(int argc, char* argv[])
     }
 
     auto ModeControlBus = sdbusplus::bus::new_default();
+    if (configureDbus)
+    {
+        dbus_configuration::init(ModeControlBus);
+    }
     SensorManager mgmr;
     std::unordered_map<int64_t, std::unique_ptr<PIDZone>> zones;
 

phosphor-pid-control/sensors/builder.cpp
@@ -22,6 +22,7 @@
 #include "conf.hpp"
 
 #include "dbus/dbuspassive.hpp"
+#include "dbus/dbuswrite.hpp"
 #include "interfaces.hpp"
 #include "notimpl/readonly.hpp"
 #include "notimpl/writeonly.hpp"
@@ -101,6 +102,23 @@ SensorManager BuildSensors(
                                  info->writepath,
                                  info->min,
                                  info->max);
+                    }
+
+                    break;
+                case IOInterfaceType::DBUSACTIVE:
+                    if (info->max > 0)
+                    {
+                        wi = std::make_unique<DbusWritePercent>(info->writepath,
+                                                                info->min,
+                                                                info->max,
+                                                                helper);
+                    }
+                    else
+                    {
+                        wi = std::make_unique<DbusWrite>(info->writepath,
+                                                        info->min,
+                                                        info->max,
+                                                        helper);
                     }
 
                     break;