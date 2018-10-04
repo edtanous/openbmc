openbmc/phosphor-user-manager

ldap: Add persistence for LDAP mapper D-Bus objects
Change-Id: Ib8979a7c655f74c332d80e7fb221ef03e9a3f83c
Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

phosphor-ldap-mapper/ldap_mapper_entry.cpp
@@ -7,6 +7,7 @@
 #include "config.h"
 #include "ldap_mapper_entry.hpp"
 #include "ldap_mapper_mgr.hpp"
+#include "ldap_mapper_serialize.hpp"
 
 namespace phosphor
 {
@@ -29,6 +30,15 @@ LDAPMapperEntry::LDAPMapperEntry(sdbusplus::bus::bus &bus, const char *path,
     EntryIface::emit_object_added();
 }
 
+LDAPMapperEntry::LDAPMapperEntry(sdbusplus::bus::bus &bus, const char *path,
+                                 LDAPMapperMgr &parent) :
+    EntryIface(bus, path),
+    DeleteIface(bus, path),
+    groupName(std::experimental::filesystem::path(path).filename()),
+    manager(parent)
+{
+}
+
 void LDAPMapperEntry::delete_(void)
 {
     manager.deletePrivilegeMapper(groupName);
@@ -42,7 +52,9 @@ std::string LDAPMapperEntry::privilege(std::string value)
     }
 
     manager.checkPrivilegeLevel(value);
-    return EntryIface::privilege(value);
+    auto val = EntryIface::privilege(value);
+    serialize(*this, groupName);
+    return val;
 }
 
 std::string LDAPMapperEntry::privilege(void) const

edtanous/openbmc

phosphor-user-manager/phosphor-ldap-mapper/ldap_mapper_entry.cpp
@@ -7,6 +7,7 @@
 #include "config.h"
 #include "ldap_mapper_entry.hpp"
 #include "ldap_mapper_mgr.hpp"
+#include "ldap_mapper_serialize.hpp"
 
 namespace phosphor
 {
@@ -31,6 +32,14 @@ LDAPMapperEntry::LDAPMapperEntry(sdbusplus::bus::bus &bus, const char *path,
     Ifaces::emit_object_added();
 }
 
+LDAPMapperEntry::LDAPMapperEntry(sdbusplus::bus::bus &bus, const char *path,
+                                 LDAPMapperMgr &parent) :
+    Ifaces(bus, path, true),
+    id(std::stol(std::experimental::filesystem::path(path).filename())),
+    manager(parent)
+{
+}
+
 void LDAPMapperEntry::delete_(void)
 {
     manager.deletePrivilegeMapper(id);
@@ -44,7 +53,9 @@ std::string LDAPMapperEntry::groupName(std::string value)
     }
 
     manager.checkPrivilegeMapper(value);
-    return Ifaces::groupName(value);
+    auto val = Ifaces::groupName(value);
+    serialize(*this, id);
+    return val;
 }
 
 std::string LDAPMapperEntry::privilege(std::string value)
@@ -55,7 +66,9 @@ std::string LDAPMapperEntry::privilege(std::string value)
     }
 
     manager.checkPrivilegeLevel(value);
-    return Ifaces::privilege(value);
+    auto val = Ifaces::privilege(value);
+    serialize(*this, id);
+    return val;
 }
 
 } // namespace user