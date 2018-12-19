openbmc/phosphor-host-ipmid

Channel commands updated to handle self channel
Channel commands are updated to handle self channel
number 0xE. When user request 0xE self channel number, it
will be converted to proper channel based on the interface
the command is requested.

Unit-Test:
Verified channel commands are working as expected.

Change-Id: If542f8e88b609d7b92f4a96acb572611dd883555
Signed-off-by: Richard Marian Thomaiyar <richard.marian.thomaiyar@linux.intel.com>

user_channel/channelcommands.cpp
@@ -149,8 +149,7 @@ ipmi_ret_t ipmiSetChannelAccess(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = getConvertedSelfChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Set channel access - Parameter out of range");
@@ -255,8 +254,7 @@ ipmi_ret_t ipmiGetChannelAccess(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = getConvertedSelfChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Get channel access - Parameter out of range");
@@ -322,8 +320,7 @@ ipmi_ret_t ipmiGetChannelInfo(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = getConvertedSelfChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Get channel info - Parameter out of range");

edtanous/openbmc

phosphor-host-ipmid/user_channel/channelcommands.cpp
@@ -169,8 +169,7 @@ ipmi_ret_t ipmiSetChannelAccess(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = convertCurrentChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Set channel access - Parameter out of range");
@@ -275,8 +274,7 @@ ipmi_ret_t ipmiGetChannelAccess(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = convertCurrentChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Get channel access - Parameter out of range");
@@ -342,8 +340,7 @@ ipmi_ret_t ipmiGetChannelInfo(ipmi_netfn_t netfn, ipmi_cmd_t cmd,
         return IPMI_CC_REQ_DATA_LEN_INVALID;
     }
 
-    // TODO: Self channel number (0xE) has to be determined.
-    uint8_t chNum = req->chNum;
+    uint8_t chNum = convertCurrentChannelNum(req->chNum);
     if (!isValidChannel(chNum))
     {
         log<level::DEBUG>("Get channel info - Parameter out of range");