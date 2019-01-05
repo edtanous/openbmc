openbmc/phosphor-ipmi-flash

tools: implement blob close
Implement blob close call.

Change-Id: If7c845e1e3b542e6f0253136eb8b6fd40bc96f3d
Signed-off-by: Patrick Venture <venture@google.com>

test/blob_interface_mock.hpp
@@ -14,6 +14,7 @@ class BlobInterfaceMock : public BlobInterface
     MOCK_METHOD2(openBlob,
                  std::uint16_t(const std::string&,
                                blobs::FirmwareBlobHandler::UpdateFlags));
+    MOCK_METHOD(closeBlob, void(std::uint16_t));
 };
 
 } // namespace host_tool

edtanous/openbmc

phosphor-ipmi-flash/test/blob_interface_mock.hpp
@@ -14,6 +14,7 @@ class BlobInterfaceMock : public BlobInterface
     MOCK_METHOD2(openBlob,
                  std::uint16_t(const std::string&,
                                blobs::FirmwareBlobHandler::UpdateFlags));
+    MOCK_METHOD1(closeBlob, void(std::uint16_t));
 };
 
 } // namespace host_tool