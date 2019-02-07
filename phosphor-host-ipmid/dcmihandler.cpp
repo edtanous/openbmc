openbmc/phosphor-host-ipmid

Create libipmid and libipmid-host
This starts a transition to common ipmid libraries that providers can
link against. It will allow for a cleaner separation between common
ipmid functionality and daemon type specific code. This is needed so we
can resolve all of the symbols in the providers at link time instead of
discovering bad linkage by building and running a full ipmi daemon.

In future commits libraries will be packaged for libipmid and libipmid-host
which provide all of the symbols used by the current set of ipmid
providers.

This is the first step, it just separates and renames the headers.
Legacy symlinks are still kept around for compatability. It also adds
stub libraries so that external users can start linking as intended.

Change-Id: I6bbd7a146362012d26812a7b039d1c4075862cbd
Signed-off-by: William A. Kennington III <wak@google.com>

dcmihandler.cpp
@@ -5,7 +5,7 @@
 #include "net.hpp"
 #include "utils.hpp"
 
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 
 #include <bitset>
 #include <cmath>

libipmid-host/cmd.hpp
@@ -1,5 +1,4 @@
-#include "ipmid-host-cmd-utils.hpp"
-
+#include <libipmid-host/cmd-utils.hpp>
 #include <memory>
 #include <sdbusplus/bus.hpp>
 

libipmid/oemopenbmc.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 
-#include <host-ipmid/oemrouter.hpp>
+#include <libipmid/oemrouter.hpp>
 
 namespace oem
 {

libipmid/oemrouter.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 
 #include <array>
 #include <cstdint>
 #include <functional>
-#include <host-ipmid/iana.hpp>
+#include <libipmid/iana.hpp>
 #include <vector>
 
 namespace oem

ipmid.cpp
@@ -17,10 +17,10 @@
 
 #include <algorithm>
 #include <cstring>
-#include <host-ipmid/ipmid-host-cmd.hpp>
-#include <host-ipmid/oemrouter.hpp>
 #include <iostream>
 #include <iterator>
+#include <libipmid-host/cmd.hpp>
+#include <libipmid/oemrouter.hpp>
 #include <map>
 #include <memory>
 #include <phosphor-logging/log.hpp>

ipmid.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 #include <stdio.h>
 
 // When the requester sends in a netfn and a command along with data, this

storageaddsel.cpp
@@ -4,7 +4,7 @@
 #include "storagehandler.hpp"
 #include "types.hpp"
 
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 #include <mapper.h>
 #include <systemd/sd-bus.h>
 

transporthandler.cpp
@@ -6,7 +6,7 @@
 #include "utils.hpp"
 
 #include <arpa/inet.h>
-#include <host-ipmid/ipmid-api.h>
+#include <libipmid/api.h>
 
 #include <chrono>
 #include <fstream>

edtanous/openbmc

phosphor-host-ipmid/dcmihandler.cpp
@@ -5,7 +5,7 @@
 #include "user_channel/channel_layer.hpp"
 #include "utils.hpp"
 
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 
 #include <bitset>
 #include <cmath>

phosphor-host-ipmid/include/ipmid-host/cmd.hpp
@@ -1,5 +1,4 @@
-#include "ipmid-host-cmd-utils.hpp"
-
+#include <ipmid-host/cmd-utils.hpp>
 #include <memory>
 #include <sdbusplus/bus.hpp>
 

phosphor-host-ipmid/include/ipmid/oemopenbmc.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 
-#include <host-ipmid/oemrouter.hpp>
+#include <ipmid/oemrouter.hpp>
 
 namespace oem
 {

phosphor-host-ipmid/include/ipmid/oemrouter.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 
 #include <array>
 #include <cstdint>
 #include <functional>
-#include <host-ipmid/iana.hpp>
+#include <ipmid/iana.hpp>
 #include <vector>
 
 namespace oem

phosphor-host-ipmid/ipmid.cpp
@@ -17,9 +17,9 @@
 
 #include <algorithm>
 #include <cstring>
-#include <host-ipmid/ipmid-host-cmd.hpp>
-#include <host-ipmid/oemrouter.hpp>
 #include <iostream>
+#include <ipmid-host/cmd.hpp>
+#include <ipmid/oemrouter.hpp>
 #include <iterator>
 #include <map>
 #include <memory>

phosphor-host-ipmid/ipmid.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 #include <stdio.h>
 
 // When the requester sends in a netfn and a command along with data, this

phosphor-host-ipmid/storageaddsel.cpp
@@ -4,7 +4,7 @@
 #include "storagehandler.hpp"
 #include "types.hpp"
 
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 #include <mapper.h>
 #include <systemd/sd-bus.h>
 

phosphor-host-ipmid/transporthandler.cpp
@@ -6,7 +6,7 @@
 #include "utils.hpp"
 
 #include <arpa/inet.h>
-#include <host-ipmid/ipmid-api.h>
+#include <ipmid/api.h>
 
 #include <chrono>
 #include <fstream>