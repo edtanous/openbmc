openbmc/phosphor-host-ipmid

dcmihandler: Add DCMI Power management support check
According to section 6.6 Power management of DCMI specification,
if power management feature is disabled,
the related commands (Get/Set/Activate/Deactive Power Limit)
should be unsupported.

Change-Id: I887f33babb2fe7a1ab97bb2d0720a693171e48a1
Signed-off-by: Kirill Pakhomov <k.pakhomov@yadro.com>

dcmihandler.hpp
@@ -55,7 +55,8 @@ static constexpr auto systemBusName = "org.freedesktop.systemd1";
 static constexpr auto systemPath = "/org/freedesktop/systemd1";
 static constexpr auto systemIntf = "org.freedesktop.systemd1.Manager";
 static constexpr auto kDCMICapabilitiesConfig = "/usr/share/ipmi-providers/dcmi_cap.json";
-
+static constexpr auto kDCMIPowerMgmtCapability = "PowerManagement";
+static constexpr auto kDCMIPowerMgmtSupported = 0x1;
 
 namespace assettag
 {
@@ -160,6 +161,13 @@ struct SetAssetTagResponse
     uint8_t groupID;   //!< Group extension identification.
     uint8_t tagLength; //!< Total asset tag length.
 } __attribute__((packed));
+
+/** @brief Check whether DCMI power management is supported
+ *         in the DCMI Capabilities config file.
+ *
+ *  @return True if DCMI power management is supported
+ */
+bool isDCMIPowerMgmtSupported();
 
 /** @brief Read the object tree to fetch the object path that implemented the
  *         Asset tag interface.

edtanous/openbmc

phosphor-host-ipmid/dcmihandler.hpp
@@ -56,6 +56,8 @@ static constexpr auto systemPath = "/org/freedesktop/systemd1";
 static constexpr auto systemIntf = "org.freedesktop.systemd1.Manager";
 static constexpr auto gDCMICapabilitiesConfig =
     "/usr/share/ipmi-providers/dcmi_cap.json";
+static constexpr auto gDCMIPowerMgmtCapability = "PowerManagement";
+static constexpr auto gDCMIPowerMgmtSupported = 0x1;
 
 namespace assettag
 {
@@ -160,6 +162,13 @@ struct SetAssetTagResponse
     uint8_t groupID;   //!< Group extension identification.
     uint8_t tagLength; //!< Total asset tag length.
 } __attribute__((packed));
+
+/** @brief Check whether DCMI power management is supported
+ *         in the DCMI Capabilities config file.
+ *
+ *  @return True if DCMI power management is supported
+ */
+bool isDCMIPowerMgmtSupported();
 
 /** @brief Read the object tree to fetch the object path that implemented the
  *         Asset tag interface.