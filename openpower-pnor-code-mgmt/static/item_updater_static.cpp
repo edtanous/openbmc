openbmc/openpower-pnor-code-mgmt

Static layout: Implement PNOR code update
Implement the PNOR code update by pflash tool in openpower-pnor-update
service, and update the related associations.

Tested: Verify PNOR code update succeeds.

Change-Id: I53781d6420071200ac2ed6837f7a79bf5e1162c2
Signed-off-by: Lei YU <mine260309@gmail.com>

static/item_updater_static.cpp
@@ -3,53 +3,13 @@
 #include "item_updater_static.hpp"
 
 #include "activation_static.hpp"
+#include "pnor_utils.hpp"
 #include "version.hpp"
 
 #include <filesystem>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <string>
-
-namespace utils
-{
-
-template <typename... Ts>
-std::string concat_string(Ts const&... ts)
-{
-    std::stringstream s;
-    ((s << ts << " "), ...) << std::endl;
-    return s.str();
-}
-
-// Helper function to run pflash command
-template <typename... Ts>
-std::string pflash(Ts const&... ts)
-{
-    std::array<char, 512> buffer;
-    std::string cmd = concat_string("pflash", ts...);
-    std::stringstream result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
-                                                  pclose);
-    if (!pipe)
-    {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-    {
-        result << buffer.data();
-    }
-    return result.str();
-}
-
-std::string getPNORVersion()
-{
-    // Read the VERSION partition skipping the first 4K
-    auto r = pflash("-P", "VERSION", "-r", "/dev/stderr", "--skip=4096",
-                    "2>&1 > /dev/null");
-    return r;
-}
-
-} // namespace utils
 
 namespace openpower
 {
@@ -166,7 +126,7 @@ void ItemUpdaterStatic::reset()
 
 bool ItemUpdaterStatic::isVersionFunctional(const std::string& versionId)
 {
-    return true;
+    return versionId == functionalVersionId;
 }
 
 void ItemUpdaterStatic::freePriority(uint8_t value,
@@ -176,10 +136,29 @@ void ItemUpdaterStatic::freePriority(uint8_t value,
 
 void ItemUpdaterStatic::deleteAll()
 {
+    // Static layout has only one active and function pnor
+    // There is no implementation for this interface
 }
 
 void ItemUpdaterStatic::freeSpace()
 {
+    // For now assume static layout only has 1 active PNOR,
+    // so erase the active PNOR
+    for (const auto& iter : activations)
+    {
+        if (iter.second.get()->activation() ==
+            server::Activation::Activations::Active)
+        {
+            erase(iter.second->versionId);
+            break;
+        }
+    }
+}
+
+void ItemUpdaterStatic::updateFunctionalAssociation(const std::string& id)
+{
+    functionalVersionId = id;
+    ItemUpdater::updateFunctionalAssociation(id);
 }
 
 void GardReset::reset()

static/item_updater_static.hpp
@@ -35,6 +35,8 @@ class ItemUpdaterStatic : public ItemUpdater
 
     void freeSpace() override;
 
+    void updateFunctionalAssociation(const std::string& id) override;
+
     bool isVersionFunctional(const std::string& versionId) override;
 
   private:
@@ -61,6 +63,9 @@ class ItemUpdaterStatic : public ItemUpdater
     /** @brief Host factory reset - clears PNOR partitions for each
      * Activation D-Bus object */
     void reset() override;
+
+    /** @brief The functional version ID */
+    std::string functionalVersionId;
 };
 
 } // namespace updater

edtanous/openbmc

openpower-pnor-code-mgmt/static/item_updater_static.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
+#include <sstream>
 #include <string>
 
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
@@ -210,7 +211,7 @@ void ItemUpdaterStatic::reset()
 
 bool ItemUpdaterStatic::isVersionFunctional(const std::string& versionId)
 {
-    return true;
+    return versionId == functionalVersionId;
 }
 
 void ItemUpdaterStatic::freePriority(uint8_t value,
@@ -220,10 +221,30 @@ void ItemUpdaterStatic::freePriority(uint8_t value,
 
 void ItemUpdaterStatic::deleteAll()
 {
+    // Static layout has only one active and function pnor
+    // There is no implementation for this interface
 }
 
 void ItemUpdaterStatic::freeSpace()
 {
+    // For now assume static layout only has 1 active PNOR,
+    // so erase the active PNOR
+    for (const auto& iter : activations)
+    {
+        if (iter.second.get()->activation() ==
+            server::Activation::Activations::Active)
+        {
+            erase(iter.second->versionId);
+            break;
+        }
+    }
+}
+
+void ItemUpdaterStatic::updateFunctionalAssociation(
+    const std::string& versionId)
+{
+    functionalVersionId = versionId;
+    ItemUpdater::updateFunctionalAssociation(versionId);
 }
 
 void GardReset::reset()

openpower-pnor-code-mgmt/static/item_updater_static.hpp
@@ -35,6 +35,8 @@ class ItemUpdaterStatic : public ItemUpdater
 
     void freeSpace() override;
 
+    void updateFunctionalAssociation(const std::string& versionId) override;
+
     bool isVersionFunctional(const std::string& versionId) override;
 
   private:
@@ -61,6 +63,9 @@ class ItemUpdaterStatic : public ItemUpdater
     /** @brief Host factory reset - clears PNOR partitions for each
      * Activation D-Bus object */
     void reset() override;
+
+    /** @brief The functional version ID */
+    std::string functionalVersionId;
 };
 
 } // namespace updater