openbmc/openbmc-test-automation

__init__.robot created to get FW version
Resolves openbmc/openbmc-test-automation#1703

Signed-off-by: Sivas SRR <sivas.srr@in.ibm.com>
Change-Id: Id97dc9845d01e9ef60f3ef333210722a36e09e6c
Signed-off-by: Sivas SRR <sivas.srr@in.ibm.com>

lib/common_utils.robot
@@ -834,6 +834,21 @@ Get PNOR Attributes
     [Return]  ${pnor_attrs}
 
 
+GET BMC PNOR Version
+    [Documentation]  Returns BMC & PNOR version from openbmc shell.
+
+    ${bmc_version} =  GET BMC Version
+    ${pnor_version} =  GET PNOR Version
+    Log  ${bmc_version}
+    Rprint Timen  BMC Version:
+    Rprint Vars  ${bmc_version}
+    Log  ${pnor_version}
+    Rprint Timen  HOST Firmware Version:
+    Rprint Vars  ${pnor_version}
+
+    [Return]  ${bmc_version}  ${pnor_version}
+
+
 Copy Address Translation Utils To HOST OS
     [Documentation]  Copy address translation utils to host OS.
 

edtanous/openbmc

openbmc-test-automation/lib/common_utils.robot
@@ -834,6 +834,19 @@ Get PNOR Attributes
     [Return]  ${pnor_attrs}
 
 
+GET BMC PNOR Version
+    [Documentation]  Return BMC & PNOR version from openbmc shell.
+
+    ${bmc_version}=  GET BMC Version
+    ${pnor_version}=  GET PNOR Version
+    Log  ${bmc_version}
+    Rprint Vars  bmc_version
+    Log  ${pnor_version}
+    Rprint Vars  pnor_version
+
+    [Return]  ${bmc_version}  ${pnor_version}
+
+
 Copy Address Translation Utils To HOST OS
     [Documentation]  Copy address translation utils to host OS.
 