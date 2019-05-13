openbmc/ibm-pldm-oem

Add unit test for ReadFileIntoMemory and WriteFileFromMemory
The code is refactored to support mocking the transferDataHost API.
Unit testcases are added for ReadFileIntoMemory and WriteFileFromMemory
responder implementations.

Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>

Change-Id: Iab9966a7aea602cc2abcdf386f077001368e66d9

libpldmresponder/file_io.cpp
@@ -27,8 +27,8 @@ namespace fs = std::filesystem;
 namespace dma
 {
 
-int transferDataHost(const fs::path& file, uint32_t offset, uint32_t length,
-                     uint64_t address, bool upstream)
+int DMA::transferDataHost(const fs::path& file, uint32_t offset,
+                          uint32_t length, uint64_t address, bool upstream)
 {
     static const size_t pageSize = getpagesize();
     uint32_t numPages = length / pageSize;
@@ -107,37 +107,6 @@ int transferDataHost(const fs::path& file, uint32_t offset, uint32_t length,
 
 } // namespace dma
 
-void transferAll(fs::path& path, uint32_t offset, uint32_t length,
-                 uint64_t address, bool upstream, pldm_msg* response)
-{
-    uint32_t origLength = length;
-
-    while (length > dma::maxSize)
-    {
-        auto rc = dma::transferDataHost(path, offset, dma::maxSize, address,
-                                        upstream);
-        if (rc < 0)
-        {
-            encode_rw_file_memory_resp(0, PLDM_ERROR, 0, response);
-            return;
-        }
-
-        offset += dma::maxSize;
-        length -= dma::maxSize;
-        address += dma::maxSize;
-    }
-
-    auto rc = dma::transferDataHost(path, offset, length, address, upstream);
-    if (rc < 0)
-    {
-        encode_rw_file_memory_resp(0, PLDM_ERROR, 0, response);
-        return;
-    }
-
-    encode_rw_file_memory_resp(0, PLDM_SUCCESS, origLength, response);
-    return;
-}
-
 void readFileIntoMemory(const uint8_t* request, size_t payloadLength,
                         pldm_msg* response)
 {
@@ -157,17 +126,17 @@ void readFileIntoMemory(const uint8_t* request, size_t payloadLength,
     decode_rw_file_memory_req(request, payloadLength, &fileHandle, &offset,
                               &length, &address);
 
-    if (!fs::exists(path))
-    {
-        std::cerr << "File does not exist\n";
-        encode_rw_file_memory_resp(0, PLDM_INVALID_FILE_HANDLE, 0, response);
-        return;
-    }
-
     if (length % dma::minSize)
     {
         std::cerr << "Readlength is not a multiple of 16\n";
         encode_rw_file_memory_resp(0, PLDM_INVALID_READ_LENGTH, 0, response);
+        return;
+    }
+
+    if (!fs::exists(path))
+    {
+        std::cerr << "File does not exist\n";
+        encode_rw_file_memory_resp(0, PLDM_INVALID_FILE_HANDLE, 0, response);
         return;
     }
 
@@ -179,7 +148,9 @@ void readFileIntoMemory(const uint8_t* request, size_t payloadLength,
         return;
     }
 
-    transferAll(path, offset, length, address, true, response);
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, path, offset, length, address, true, response);
 }
 
 void writeFileFromMemory(const uint8_t* request, size_t payloadLength,
@@ -200,6 +171,12 @@ void writeFileFromMemory(const uint8_t* request, size_t payloadLength,
 
     decode_rw_file_memory_req(request, payloadLength, &fileHandle, &offset,
                               &length, &address);
+    if (length % dma::minSize)
+    {
+        std::cerr << "Writelength is not a multiple of 16\n";
+        encode_rw_file_memory_resp(0, PLDM_INVALID_WRITE_LENGTH, 0, response);
+        return;
+    }
 
     if (!fs::exists(path))
     {
@@ -208,14 +185,9 @@ void writeFileFromMemory(const uint8_t* request, size_t payloadLength,
         return;
     }
 
-    if (length % dma::minSize)
-    {
-        std::cerr << "Writelength is not a multiple of 16\n";
-        encode_rw_file_memory_resp(0, PLDM_INVALID_WRITE_LENGTH, 0, response);
-        return;
-    }
-
-    transferAll(path, offset, length, address, false, response);
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, path, offset, length, address, false, response);
 }
 
 } // namespace responder

edtanous/openbmc

ibm-pldm-oem/libpldmresponder/file_io.cpp
@@ -40,8 +40,8 @@ struct AspeedXdmaOp
 
 constexpr auto xdmaDev = "/dev/xdma";
 
-int transferDataHost(const fs::path& path, uint32_t offset, uint32_t length,
-                     uint64_t address, bool upstream)
+int DMA::transferDataHost(const fs::path& path, uint32_t offset,
+                          uint32_t length, uint64_t address, bool upstream)
 {
     static const size_t pageSize = getpagesize();
     uint32_t numPages = length / pageSize;
@@ -126,38 +126,6 @@ int transferDataHost(const fs::path& path, uint32_t offset, uint32_t length,
 
 } // namespace dma
 
-void transferAll(uint8_t command, fs::path& path, uint32_t offset,
-                 uint32_t length, uint64_t address, bool upstream,
-                 pldm_msg* response)
-{
-    uint32_t origLength = length;
-
-    while (length > dma::maxSize)
-    {
-        auto rc = dma::transferDataHost(path, offset, dma::maxSize, address,
-                                        upstream);
-        if (rc < 0)
-        {
-            encode_rw_file_memory_resp(0, command, PLDM_ERROR, 0, response);
-            return;
-        }
-
-        offset += dma::maxSize;
-        length -= dma::maxSize;
-        address += dma::maxSize;
-    }
-
-    auto rc = dma::transferDataHost(path, offset, length, address, upstream);
-    if (rc < 0)
-    {
-        encode_rw_file_memory_resp(0, command, PLDM_ERROR, 0, response);
-        return;
-    }
-
-    encode_rw_file_memory_resp(0, command, PLDM_SUCCESS, origLength, response);
-    return;
-}
-
 void readFileIntoMemory(const uint8_t* request, size_t payloadLength,
                         pldm_msg* response)
 {
@@ -209,8 +177,10 @@ void readFileIntoMemory(const uint8_t* request, size_t payloadLength,
         return;
     }
 
-    transferAll(PLDM_READ_FILE_INTO_MEMORY, path, offset, length, address, true,
-                response);
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, PLDM_READ_FILE_INTO_MEMORY, path, offset, length,
+                     address, true, response);
 }
 
 void writeFileFromMemory(const uint8_t* request, size_t payloadLength,
@@ -259,8 +229,10 @@ void writeFileFromMemory(const uint8_t* request, size_t payloadLength,
         return;
     }
 
-    transferAll(PLDM_WRITE_FILE_FROM_MEMORY, path, offset, length, address,
-                false, response);
+    using namespace dma;
+    DMA intf;
+    transferAll<DMA>(&intf, PLDM_WRITE_FILE_FROM_MEMORY, path, offset, length,
+                     address, false, response);
 }
 
 } // namespace responder