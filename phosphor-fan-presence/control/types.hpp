openbmc/phosphor-fan-presence

Have timers be a trigger type in microseconds
Extract and generate timers in microseconds for all event triggers and
actions that take a timer as a parameter.

Tested:
    Timers are created on events with timer triggers defined
    Inspect that generated timers are created in microseconds
    No timer is created for events without a timer trigger

Change-Id: I345f247782779ef142fe0d569632be2b6c11f0c3
Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

control/types.hpp
@@ -64,7 +64,7 @@ using Service = std::tuple<std::string, bool>;
 
 constexpr auto intervalPos = 0;
 constexpr auto typePos = 1;
-using Timer = std::tuple<std::chrono::seconds,
+using Timer = std::tuple<std::chrono::microseconds,
                          util::Timer::TimerType>;
 
 constexpr auto sigMatchPos = 0;

control/zone.cpp
@@ -335,7 +335,7 @@ void Zone::initEvent(const SetSpeedEvent& event)
     }
     // Attach a timer to run the action of an event
     auto eventTimer = std::get<timerPos>(event);
-    if (std::get<intervalPos>(eventTimer) != seconds(0))
+    if (std::get<intervalPos>(eventTimer) != microseconds(0))
     {
         // Associate event data with timer
         std::unique_ptr<EventData> eventData =

edtanous/openbmc

phosphor-fan-presence/control/types.hpp
@@ -71,7 +71,7 @@ enum class TimerType
     oneshot,
     repeating,
 };
-using TimerConf = std::tuple<std::chrono::seconds,
+using TimerConf = std::tuple<std::chrono::microseconds,
                              TimerType>;
 
 constexpr auto sigMatchPos = 0;

phosphor-fan-presence/control/zone.cpp
@@ -376,7 +376,7 @@ void Zone::initEvent(const SetSpeedEvent& event)
     }
     // Attach a timer to run the action of an event
     auto timerConf = std::get<timerConfPos>(event);
-    if (std::get<intervalPos>(timerConf) != seconds(0))
+    if (std::get<intervalPos>(timerConf) != microseconds(0))
     {
         addTimer(std::get<groupPos>(event),
                  std::get<actionsPos>(event),