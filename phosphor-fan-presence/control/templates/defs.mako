openbmc/phosphor-fan-presence

Call event triggers on initialization
Move timers to be an event trigger function that is called upon event
initialization. Previously, event timers were created and started
directly within the initialization of the event, this moves the creation
and starting of a defined event timer to within a selectable event
trigger function.

With the creation of event triggers, an event can be defined with a list
of supported triggers used to execute the actions, if defined, of the
event. In the case no actions are given, the trigger is still used.

Tested:
    Timer triggers are created/started correctly for an event
    Events with no actions have timers created/started as defined

Change-Id: I9ab13a4269b2035095cadbbe2c613a16df469589
Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

control/templates/defs.mako
@@ -58,11 +58,13 @@ make_action(action::${a['name']}
 ),
 %endfor
 },
-Timer{
+std::vector<Trigger>{
     %if ('timer' in event['triggers']) and \
         (event['triggers']['timer'] is not None):
+    make_trigger(trigger::timer(Timer{
     ${event['triggers']['timer']['interval']},
     ${event['triggers']['timer']['type']}
+    }))
     %endif
 },
 std::vector<Signal>{

control/templates/fan_zone_defs.mako.cpp
@@ -11,6 +11,7 @@ def indent(str, depth):
 #include "handlers.hpp"
 #include "preconditions.hpp"
 #include "matches.hpp"
+#include "triggers.hpp"
 
 using namespace phosphor::fan::control;
 
@@ -120,11 +121,13 @@ const std::vector<ZoneGroup> Manager::_zoneLayouts
                         ),
                         %endif
                         },
-                        Timer{
+                        std::vector<Trigger>{
                             %if ('timer' in event['pc']['triggers']) and \
                                 (event['pc']['triggers']['timer'] is not None):
+                            make_trigger(trigger::timer(Timer{
                             ${event['pc']['triggers']['pctime']['interval']},
                             ${event['pc']['triggers']['pctime']['type']}
+                        }))
                             %endif
                         },
                         std::vector<Signal>{

control/triggers.hpp
@@ -1,0 +1,30 @@
+#pragma once
+
+#include "types.hpp"
+#include "zone.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+namespace trigger
+{
+
+/**
+ * @brief A trigger to start a timer for an event
+ * @details Creates and starts a timer according to the configuration given
+ * that will call an event's actions upon each timer expiration.
+ *
+ * @param[in] tConf - Timer configuration parameters
+ *
+ * @return Trigger lambda function
+ *     A Trigger function that creates and starts a timer
+ */
+Trigger timer(Timer&& tConf);
+
+} // namespace trigger
+} // namespace control
+} // namespace fan
+} // namespace phosphor

edtanous/openbmc

phosphor-fan-presence/control/templates/defs.mako
@@ -58,11 +58,13 @@ make_action(action::${a['name']}
 ),
 %endfor
 },
-TimerConf{
+std::vector<Trigger>{
     %if ('timer' in event['triggers']) and \
         (event['triggers']['timer'] is not None):
+    make_trigger(trigger::timer(TimerConf{
     ${event['triggers']['timer']['interval']},
     ${event['triggers']['timer']['type']}
+    }))
     %endif
 },
 std::vector<Signal>{

phosphor-fan-presence/control/templates/fan_zone_defs.mako.cpp
@@ -11,6 +11,7 @@ def indent(str, depth):
 #include "handlers.hpp"
 #include "preconditions.hpp"
 #include "matches.hpp"
+#include "triggers.hpp"
 
 using namespace phosphor::fan::control;
 
@@ -155,11 +156,13 @@ const std::vector<ZoneGroup> Manager::_zoneLayouts
                         ),
                         %endif
                         },
-                        TimerConf{
+                        std::vector<Trigger>{
                             %if ('timer' in event['pc']['triggers']) and \
                                 (event['pc']['triggers']['timer'] is not None):
+                            make_trigger(trigger::timer(TimerConf{
                             ${event['pc']['triggers']['pctime']['interval']},
                             ${event['pc']['triggers']['pctime']['type']}
+                        }))
                             %endif
                         },
                         std::vector<Signal>{

phosphor-fan-presence/control/triggers.hpp
@@ -1,0 +1,30 @@
+#pragma once
+
+#include "types.hpp"
+#include "zone.hpp"
+
+namespace phosphor
+{
+namespace fan
+{
+namespace control
+{
+namespace trigger
+{
+
+/**
+ * @brief A trigger to start a timer for an event
+ * @details Creates and starts a timer according to the configuration given
+ * that will call an event's actions upon each timer expiration.
+ *
+ * @param[in] tConf - Timer configuration parameters
+ *
+ * @return Trigger lambda function
+ *     A Trigger function that creates and starts a timer
+ */
+Trigger timer(TimerConf&& tConf);
+
+} // namespace trigger
+} // namespace control
+} // namespace fan
+} // namespace phosphor