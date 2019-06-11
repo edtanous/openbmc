openbmc/openbmc-test-automation

Base code for certificate testing via GUI
Change-Id: Iff2feb0d6d900691089fdc6bac1bfd3ab545ebb9
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

gui/test/server_config/test_obmc_gui_certificate.robot
@@ -1,0 +1,45 @@
+*** Settings ***
+
+Documentation  Test OpenBMC GUI "Certificate management" sub-menu  of
+...            "Server configuration".
+
+Resource        ../../lib/resource.robot
+
+Suite Setup     Launch Browser And Login OpenBMC GUI
+Suite Teardown  Close Browser
+Test Setup      Test Setup Execution
+
+
+*** Variables ***
+
+${xpath_select_certificate_management}  //a[@href='#/configuration/certificate']
+${xpath_select_server_configuration}    //*[@id="nav__top-level"]/li[4]/button
+${xpath_add_certificate_button}         //button[text() ="Add new certificate"]
+
+*** Test Cases ***
+
+Verify Existence Of All Section In Certificate Page
+    [Documentation]  Verify existence of all sections in certificate page.
+    [Tags]  Verify_Existence_Of_All_Section_In_Certificate_Page
+
+    Page should contain  Certificate
+    Page should contain  Valid from
+    Page should contain  Valid until
+
+Verify Existence Of Add Certificate Button
+    [Documentation]  Verify existence of add certificate button.
+    [Tags]  Verify_Existence_Of_Add_Certificate_Button
+
+    Page Should Contain Element  ${xpath_add_certificate_button}
+
+
+*** Keywords ***
+
+Test Setup Execution
+   [Documentation]  Do test case setup tasks.
+
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_configuration}
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_certificate_management}
+    Wait Until Page Contains  SSL certificates

edtanous/openbmc

openbmc-test-automation/gui/test/server_config/test_obmc_gui_certificate.robot
@@ -1,0 +1,47 @@
+*** Settings ***
+
+Documentation  Test OpenBMC GUI "Certificate management" sub-menu of
+...            "Server configuration".
+
+Resource        ../../lib/resource.robot
+
+Suite Setup     Launch Browser And Login OpenBMC GUI
+Suite Teardown  Close Browser
+Test Setup      Test Setup Execution
+
+
+*** Variables ***
+
+${xpath_select_certificate_management}  //a[@href='#/configuration/certificate']
+${xpath_select_server_configuration}    //*[@id="nav__top-level"]/li[4]/button
+${xpath_add_certificate_button}         //button[text() ="Add new certificate"]
+
+
+*** Test Cases ***
+
+Verify Existence Of All Sections In Certificate Page
+    [Documentation]  Verify existence of all sections in certificate page.
+    [Tags]  Verify_Existence_Of_All_Sections_In_Certificate_Page
+
+    Page should contain  Certificate
+    Page should contain  Valid from
+    Page should contain  Valid until
+
+
+Verify Existence Of Add Certificate Button
+    [Documentation]  Verify existence of add certificate button.
+    [Tags]  Verify_Existence_Of_Add_Certificate_Button
+
+    Page Should Contain Element  ${xpath_add_certificate_button}
+
+
+*** Keywords ***
+
+Test Setup Execution
+   [Documentation]  Do test case setup tasks.
+
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_configuration}
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_certificate_management}
+    Wait Until Page Contains  SSL certificates