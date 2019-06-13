openbmc/bmcweb

Updating Roles to version 1.2.1 to be in compliance with PSMEv2.4 specification.
RedfishServiceValiddator passed with success on this service.

Signed-off-by: Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>
Change-Id: I9161984c51037ebb7f34fdb36bee34026d28be03
Signed-off-by: Zbigniew Kurzynski <zbigniew.kurzynski@intel.com>

redfish-core/lib/roles.hpp
@@ -105,13 +105,14 @@ class Roles : public Node
         }
 
         res.jsonValue = {
-            {"@odata.type", "#Role.v1_0_2.Role"},
+            {"@odata.type", "#Role.v1_2_1.Role"},
             {"@odata.context", "/redfish/v1/$metadata#Role.Role"},
             {"Name", "User Role"},
             {"Description", roleId + " User Role"},
             {"OemPrivileges", nlohmann::json::array()},
             {"IsPredefined", true},
             {"Id", roleId},
+            {"RoleId", roleId},
             {"@odata.id", "/redfish/v1/AccountService/Roles/" + roleId},
             {"AssignedPrivileges", std::move(privArray)}};
         res.end();

edtanous/openbmc

bmcweb/redfish-core/lib/roles.hpp
@@ -105,13 +105,14 @@ class Roles : public Node
         }
 
         res.jsonValue = {
-            {"@odata.type", "#Role.v1_0_2.Role"},
+            {"@odata.type", "#Role.v1_2_2.Role"},
             {"@odata.context", "/redfish/v1/$metadata#Role.Role"},
             {"Name", "User Role"},
             {"Description", roleId + " User Role"},
             {"OemPrivileges", nlohmann::json::array()},
             {"IsPredefined", true},
             {"Id", roleId},
+            {"RoleId", roleId},
             {"@odata.id", "/redfish/v1/AccountService/Roles/" + roleId},
             {"AssignedPrivileges", std::move(privArray)}};
         res.end();