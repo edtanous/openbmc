openbmc/phosphor-hwmon

mainloop: move out the interface update method
Move the interface update method out of the read() loop.

Signed-off-by: Patrick Venture <venture@google.com>
Change-Id: I430d20ded4efd89c691eff368d005791f0138929

mainloop.cpp
@@ -73,6 +73,31 @@ decltype(
     Thresholds<CriticalObject>::alarmHi) Thresholds<CriticalObject>::alarmHi =
     &CriticalObject::criticalAlarmHigh;
 
+void updateInterfaces(InterfaceMap& ifaces, int64_t value)
+{
+    for (auto& iface : ifaces)
+    {
+        switch (iface.first)
+        {
+            case InterfaceType::VALUE:
+            {
+                auto& valueIface =
+                    std::any_cast<std::shared_ptr<ValueObject>&>(iface.second);
+                valueIface->value(value);
+            }
+            break;
+            case InterfaceType::WARN:
+                checkThresholds<WarningObject>(iface.second, value);
+                break;
+            case InterfaceType::CRIT:
+                checkThresholds<CriticalObject>(iface.second, value);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 std::string MainLoop::getID(SensorSet::container_t::const_reference sensor)
 {
     std::string id;
@@ -416,29 +441,7 @@ void MainLoop::read()
                     value = sensor->adjustValue(value);
                 }
 
-                for (auto& iface : obj)
-                {
-                    switch (iface.first)
-                    {
-                        case InterfaceType::VALUE:
-                        {
-                            auto& valueIface =
-                                std::any_cast<std::shared_ptr<ValueObject>&>(
-                                    iface.second);
-                            valueIface->value(value);
-                        }
-                        break;
-                        case InterfaceType::WARN:
-                            checkThresholds<WarningObject>(iface.second, value);
-                            break;
-                        case InterfaceType::CRIT:
-                            checkThresholds<CriticalObject>(iface.second,
-                                                            value);
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                updateInterfaces(obj, value);
             }
             catch (const std::system_error& e)
             {

mainloop.hpp
@@ -147,3 +147,8 @@ class MainLoop
     std::optional<ObjectStateData>
         getObject(SensorSet::container_t::const_reference sensor);
 };
+
+/** @brief Given a value and map of interfaces, update values and check
+ * thresholds.
+ */
+void updateInterfaces(InterfaceMap& ifaces, int64_t value);

edtanous/openbmc

phosphor-hwmon/mainloop.cpp
@@ -73,6 +73,31 @@ decltype(
     Thresholds<CriticalObject>::alarmHi) Thresholds<CriticalObject>::alarmHi =
     &CriticalObject::criticalAlarmHigh;
 
+void updateSensorInterfaces(InterfaceMap& ifaces, int64_t value)
+{
+    for (auto& iface : ifaces)
+    {
+        switch (iface.first)
+        {
+            case InterfaceType::VALUE:
+            {
+                auto& valueIface =
+                    std::any_cast<std::shared_ptr<ValueObject>&>(iface.second);
+                valueIface->value(value);
+            }
+            break;
+            case InterfaceType::WARN:
+                checkThresholds<WarningObject>(iface.second, value);
+                break;
+            case InterfaceType::CRIT:
+                checkThresholds<CriticalObject>(iface.second, value);
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 std::string MainLoop::getID(SensorSet::container_t::const_reference sensor)
 {
     std::string id;
@@ -413,29 +438,7 @@ void MainLoop::read()
                     value = sensor->adjustValue(value);
                 }
 
-                for (auto& iface : obj)
-                {
-                    switch (iface.first)
-                    {
-                        case InterfaceType::VALUE:
-                        {
-                            auto& valueIface =
-                                std::any_cast<std::shared_ptr<ValueObject>&>(
-                                    iface.second);
-                            valueIface->value(value);
-                        }
-                        break;
-                        case InterfaceType::WARN:
-                            checkThresholds<WarningObject>(iface.second, value);
-                            break;
-                        case InterfaceType::CRIT:
-                            checkThresholds<CriticalObject>(iface.second,
-                                                            value);
-                            break;
-                        default:
-                            break;
-                    }
-                }
+                updateSensorInterfaces(obj, value);
             }
             catch (const std::system_error& e)
             {

phosphor-hwmon/mainloop.hpp
@@ -147,3 +147,8 @@ class MainLoop
     std::optional<ObjectStateData>
         getObject(SensorSet::container_t::const_reference sensor);
 };
+
+/** @brief Given a value and map of interfaces, update values and check
+ * thresholds.
+ */
+void updateSensorInterfaces(InterfaceMap& ifaces, int64_t value);