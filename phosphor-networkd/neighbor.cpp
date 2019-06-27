openbmc/phosphor-networkd

types: Remove custom MacAddr type
The standard library has an ether_addr type that does the same job.

Change-Id: I98685b0b60dd07453ae2362adb33e0a7c9c3132c
Signed-off-by: William A. Kennington III <wak@google.com>

neighbor.cpp
@@ -50,7 +50,7 @@ void parseNeighbor(std::vector<NeighborInfo>& ret, const nlmsghdr& hdr,
         auto [hdr, data] = netlink::extractRtAttr(msg);
         if (hdr.rta_type == NDA_LLADDR)
         {
-            info.mac = mac_address::fromBuf(data);
+            info.mac = copyFrom<ether_addr>(data, "Bad neighbor MAC");
         }
         else if (hdr.rta_type == NDA_DST)
         {

test/test_neighbor.cpp
@@ -164,7 +164,7 @@ TEST(ParseNeighbor, Full)
     EXPECT_EQ(ifstr, info[0].interface);
     EXPECT_FALSE(info[0].permanent);
     EXPECT_TRUE(info[0].mac);
-    EXPECT_EQ(0, std::memcmp(&mac, info[0].mac->data(), sizeof(mac)));
+    EXPECT_TRUE(equal(mac, *info[0].mac));
     EXPECT_TRUE(equal(addr, std::get<in6_addr>(info[0].address)));
 }
 

edtanous/openbmc

phosphor-networkd/neighbor.cpp
@@ -50,7 +50,7 @@ void parseNeighbor(const nlmsghdr& hdr, std::string_view msg,
         auto [hdr, data] = netlink::extractRtAttr(msg);
         if (hdr.rta_type == NDA_LLADDR)
         {
-            neighbor.mac = mac_address::fromBuf(data);
+            neighbor.mac = copyFrom<ether_addr>(data, "Bad neighbor MAC");
         }
         else if (hdr.rta_type == NDA_DST)
         {

phosphor-networkd/test/test_neighbor.cpp
@@ -164,7 +164,7 @@ TEST(ParseNeighbor, Full)
     EXPECT_EQ(ifstr, neighbors[0].interface);
     EXPECT_FALSE(neighbors[0].permanent);
     EXPECT_TRUE(neighbors[0].mac);
-    EXPECT_EQ(0, std::memcmp(&mac, neighbors[0].mac->data(), sizeof(mac)));
+    EXPECT_TRUE(equal(mac, *neighbors[0].mac));
     EXPECT_TRUE(equal(addr, std::get<in6_addr>(neighbors[0].address)));
 }
 