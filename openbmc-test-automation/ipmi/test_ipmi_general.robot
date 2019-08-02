openbmc/openbmc-test-automation

Moved IPMI DCMI capabilities in new bucket
Change-Id: I3bba5908aaf61d5154bb75d6e24a35064e16f69f
Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

ipmi/test_ipmi_general.robot
@@ -1,0 +1,37 @@
+*** Settings ***
+
+Documentation    Module to test IPMI asset tag functionality.
+Resource         ../lib/ipmi_client.robot
+Resource         ../lib/openbmc_ffdc.robot
+
+#Test Teardown    FFDC On Test Case Fail
+
+*** Test Cases ***
+
+Verify Get DCMI Capabilities
+    [Documentation]  Verify get DCMI capabilities command output.
+    [Tags]  Verify_Get_DCMI_Capabilities
+    ${cmd_output}=  Run IPMI Standard Command  dcmi discover
+
+    @{supported_capabilities}=  Create List
+    # Supported DCMI capabilities:
+    ...  Mandatory platform capabilities
+    ...  Optional platform capabilities
+    ...  Power management available
+    ...  Managebility access capabilities
+    ...  In-band KCS channel available
+    # Mandatory platform attributes:
+    ...  200 SEL entries
+    ...  SEL automatic rollover is enabled
+    # Optional Platform Attributes:
+    ...  Slave address of device: 0h (8bits)(Satellite/External controller)
+    ...  Channel number is 0h (Primary BMC)
+    ...  Device revision is 0
+    # Manageability Access Attributes:
+    ...  Primary LAN channel number: 1 is available
+    ...  Secondary LAN channel is not available for OOB
+    ...  No serial channel is available
+
+    :FOR  ${capability}  IN  @{supported_capabilities}
+    \  Should Contain  ${cmd_output}  ${capability}  ignore_case=True
+    ...  msg=Supported DCMI capabilities not present.

edtanous/openbmc

openbmc-test-automation/ipmi/test_ipmi_general.robot
@@ -1,0 +1,37 @@
+*** Settings ***
+
+Documentation    Module to test IPMI asset tag functionality.
+Resource         ../lib/ipmi_client.robot
+Resource         ../lib/openbmc_ffdc.robot
+
+Test Teardown    FFDC On Test Case Fail
+
+*** Test Cases ***
+
+Verify Get DCMI Capabilities
+    [Documentation]  Verify get DCMI capabilities command output.
+    [Tags]  Verify_Get_DCMI_Capabilities
+    ${cmd_output}=  Run IPMI Standard Command  dcmi discover
+
+    @{supported_capabilities}=  Create List
+    # Supported DCMI capabilities:
+    ...  Mandatory platform capabilties
+    ...  Optional platform capabilties
+    ...  Power management available
+    ...  Managebility access capabilties
+    ...  In-band KCS channel available
+    # Mandatory platform attributes:
+    ...  200 SEL entries
+    ...  SEL automatic rollover is enabled
+    # Optional Platform Attributes:
+    ...  Slave address of device: 0h (8bits)(Satellite/External controller)
+    ...  Channel number is 0h (Primary BMC)
+    ...  Device revision is 0
+    # Manageability Access Attributes:
+    ...  Primary LAN channel number: 1 is available
+    ...  Secondary LAN channel is not available for OOB
+    ...  No serial channel is available
+
+    :FOR  ${capability}  IN  @{supported_capabilities}
+    \  Should Contain  ${cmd_output}  ${capability}  ignore_case=True
+    ...  msg=Supported DCMI capabilities not present.