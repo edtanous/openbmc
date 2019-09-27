openbmc/phosphor-logging

PEL: Move PEL section IDs into a header file
These will eventually need to be known to a piece of code that
unflattens a PEL into the appropriate section class objects based on the
section ID field in the PEL data.

Signed-off-by: Matt Spinler <spinler@us.ibm.com>
Change-Id: I90b9d4be49b2e3807a620745fa663f94f7f4e62c

extensions/openpower-pels/pel_types.hpp
@@ -1,0 +1,34 @@
+#pragma once
+
+namespace openpower
+{
+namespace pels
+{
+
+/**
+ * @brief PEL section IDs
+ */
+enum class SectionID
+{
+    privateHeader = 0x5048,      // 'PH'
+    userHeader = 0x5548,         // 'UH'
+    primarySRC = 0x5053,         // 'PS'
+    secondarySRC = 0x5353,       // 'SS'
+    extendedUserHeader = 0x4558, // 'EH'
+    failingMTMS = 0x4D54,        // 'MT'
+    dumpLocation = 0x4448,       // 'DH'
+    firmwareError = 0x5357,      // 'SW'
+    impactedPart = 0x4C50,       // 'LP'
+    logicalResource = 0x4C52,    // 'LR'
+    hmcID = 0x484D,              // 'HM'
+    epow = 0x4550,               // 'EP'
+    ioEvent = 0x4945,            // 'IE'
+    mfgInfo = 0x4D49,            // 'MI'
+    callhome = 0x4348,           // 'CH'
+    userData = 0x5544,           // 'UD'
+    envInfo = 0x4549,            // 'EI'
+    extUserData = 0x4544         // 'ED'
+};
+
+} // namespace pels
+} // namespace openpower

edtanous/openbmc

phosphor-logging/extensions/openpower-pels/pel_types.hpp
@@ -1,0 +1,34 @@
+#pragma once
+
+namespace openpower
+{
+namespace pels
+{
+
+/**
+ * @brief PEL section IDs
+ */
+enum class SectionID
+{
+    privateHeader = 0x5048,      // 'PH'
+    userHeader = 0x5548,         // 'UH'
+    primarySRC = 0x5053,         // 'PS'
+    secondarySRC = 0x5353,       // 'SS'
+    extendedUserHeader = 0x4548, // 'EH'
+    failingMTMS = 0x4D54,        // 'MT'
+    dumpLocation = 0x4448,       // 'DH'
+    firmwareError = 0x5357,      // 'SW'
+    impactedPart = 0x4C50,       // 'LP'
+    logicalResource = 0x4C52,    // 'LR'
+    hmcID = 0x484D,              // 'HM'
+    epow = 0x4550,               // 'EP'
+    ioEvent = 0x4945,            // 'IE'
+    mfgInfo = 0x4D49,            // 'MI'
+    callhome = 0x4348,           // 'CH'
+    userData = 0x5544,           // 'UD'
+    envInfo = 0x4549,            // 'EI'
+    extUserData = 0x4544         // 'ED'
+};
+
+} // namespace pels
+} // namespace openpower