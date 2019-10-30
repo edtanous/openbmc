openbmc/openbmc-test-automation

Added test suite for GUI Server Led page
Signed-off-by: Anusha Dathatri <adathatr@in.ibm.com>
Change-Id: I0f0f1250a0730be9367e9b8f9035401277fa8ada

gui/test/server_control/test_obmc_gui_server_led.robot
@@ -1,0 +1,43 @@
+*** Settings ***
+
+Documentation  Test OpenBMC GUI "Server led" sub-menu of "Server control".
+
+Resource        ../../lib/resource.robot
+
+Suite Setup     Launch Browser And Login OpenBMC GUI
+Suite Teardown  Close Browser
+Test Setup      Test Setup Execution
+
+
+*** Variables ***
+
+${xpath_led_light_control}           //*[@for="toggle__switch-round"]
+
+
+*** Test Cases ***
+
+Verify Existence Of All Sections In Server Led Page
+    [Documentation]  Verify existence of all sections in Server led page.
+    [Tags]  Verify_Existence_Of_All_Sections_In_Server_Led_Page
+
+    Page Should Contain  LED light control
+    Page Should Contain  Server LED light
+
+
+Verify Existence Of All Buttons In Server Led Page
+    [Documentation]  Verify existence of all buttons in Server led page.
+    [Tags]  Verify_Existence_Of_All_Buttons_In_Server_Led_Page
+
+    Page Should Contain Element  ${xpath_led_light_control}
+
+
+*** Keywords ***
+
+Test Setup Execution
+   [Documentation]  Do test case setup tasks.
+
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_control}
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_led}
+    Wait Until Page Contains  Server LED

edtanous/openbmc

openbmc-test-automation/gui/test/server_control/test_obmc_gui_server_led.robot
@@ -1,0 +1,43 @@
+*** Settings ***
+
+Documentation  Test OpenBMC GUI "Server LED" sub-menu of "Server control".
+
+Resource        ../../lib/resource.robot
+
+Suite Setup     Launch Browser And Login OpenBMC GUI
+Suite Teardown  Close Browser
+Test Setup      Test Setup Execution
+
+
+*** Variables ***
+
+${xpath_led_light_control}           //*[@for="toggle__switch-round"]
+
+
+*** Test Cases ***
+
+Verify Existence Of All Sections In Server LED Page
+    [Documentation]  Verify existence of all sections in Server LED page.
+    [Tags]  Verify_Existence_Of_All_Sections_In_Server_LED_Page
+
+    Page Should Contain  LED light control
+    Page Should Contain  Server LED light
+
+
+Verify Existence Of All Buttons In Server LED Page
+    [Documentation]  Verify existence of all buttons in Server LED page.
+    [Tags]  Verify_Existence_Of_All_Buttons_In_Server_LED_Page
+
+    Page Should Contain Element  ${xpath_led_light_control}
+
+
+*** Keywords ***
+
+Test Setup Execution
+    [Documentation]  Do test case setup tasks.
+
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_control}
+    Wait Until Page Does Not Contain Element  ${xpath_refresh_circle}
+    Click Element  ${xpath_select_server_led}
+    Wait Until Page Contains  Server LED