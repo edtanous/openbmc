openbmc/phosphor-psu-code-mgmt

Activation: remove old objects after update done
The update on PSUs is one-by-one, after each PSU is updated, notify
ItemUpdater by onUpdateDone() so that the old object for the PSU is
removed.

Tested: With dummy PSU image and update service, verify the old
        objects are removed on Witherspoon.

Change-Id: I212b8cba9570ad96083d362bf57691fdabb4e42f

src/activation.cpp
@@ -127,8 +127,10 @@ void Activation::onUpdateDone()
     auto assocs = associations();
     assocs.emplace_back(ACTIVATION_FWD_ASSOCIATION, ACTIVATION_REV_ASSOCIATION,
                         currentUpdatingPsu);
-    currentUpdatingPsu.clear();
     associations(assocs);
+
+    activationListner->onUpdateDone(versionId, currentUpdatingPsu);
+    currentUpdatingPsu.clear();
 
     psuQueue.pop();
     doUpdate(); // Update the next psu
@@ -218,7 +220,6 @@ void Activation::finishActivation()
     storeImage();
     activationProgress->progress(100);
 
-    // TODO: delete the old software object
     deleteImageManagerObject();
 
     associationInterface->createActiveAssociation(objPath);

edtanous/openbmc

phosphor-psu-code-mgmt/src/activation.cpp
@@ -127,8 +127,10 @@ void Activation::onUpdateDone()
     auto assocs = associations();
     assocs.emplace_back(ACTIVATION_FWD_ASSOCIATION, ACTIVATION_REV_ASSOCIATION,
                         currentUpdatingPsu);
-    currentUpdatingPsu.clear();
     associations(assocs);
+
+    activationListener->onUpdateDone(versionId, currentUpdatingPsu);
+    currentUpdatingPsu.clear();
 
     psuQueue.pop();
     doUpdate(); // Update the next psu
@@ -218,7 +220,6 @@ void Activation::finishActivation()
     storeImage();
     activationProgress->progress(100);
 
-    // TODO: delete the old software object
     deleteImageManagerObject();
 
     associationInterface->createActiveAssociation(objPath);