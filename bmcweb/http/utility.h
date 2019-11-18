openbmc/bmcweb

Implement constant time string compare for token
The sessions implementation previously used operator== for session
comparisons.  While unlikely to be attackable in the current
implementation, due to the time smearing in a number of cases, modern
security practices recommend using constant time comparison.

Tested By:
Logged into the webui, and observed no change to login flows.  Logged
into redfish using Token Auth, and observed no changes.  Closed a
previous session, then reopened with the new session information to
verify user sessions are restored properly and still work.

Change-Id: Ie759e4da67ba004fd8c327f177951ac756ea6799
Signed-off-by: Ed Tanous <ed.tanous@intel.com># Please enter the commit message for your changes. Lines starting

crow/include/crow/utility.h
@@ -2,6 +2,8 @@
 
 #include "nlohmann/json.hpp"
 
+#include <openssl/crypto.h>
+
 #include <boost/utility/string_view.hpp>
 #include <cstdint>
 #include <cstring>
@@ -665,6 +667,27 @@ inline bool base64Decode(const boost::string_view input, std::string& output)
 
     return true;
 }
+
+inline bool constantTimeStringCompare(const boost::string_view a,
+                                      const boost::string_view b)
+{
+    // Important note, this function is ONLY constant time if the two input
+    // sizes are the same
+    if (a.size() != b.size())
+    {
+        return false;
+    }
+    return CRYPTO_memcmp(a.data(), b.data(), a.size()) == 0;
+}
+
+struct ConstantTimeCompare
+{
+    bool operator()(const boost::string_view a,
+                    const boost::string_view b) const
+    {
+        return constantTimeStringCompare(a, b);
+    }
+};
 
 } // namespace utility
 } // namespace crow

edtanous/openbmc

Signed-off-by: Ed Tanous <ed.tanous@intel.com>
Signed-off-by: James Feist <james.feist@linux.intel.com>

bmcweb/http/utility.h
@@ -2,6 +2,8 @@
 
 #include "nlohmann/json.hpp"
 
+#include <openssl/crypto.h>
+
 #include <cstdint>
 #include <cstring>
 #include <functional>
@@ -778,6 +780,26 @@ inline std::string dateTimeNow()
     std::time_t time = std::time(nullptr);
     return getDateTime(time);
 }
+
+inline bool constantTimeStringCompare(const std::string_view a,
+                                      const std::string_view b)
+{
+    // Important note, this function is ONLY constant time if the two input
+    // sizes are the same
+    if (a.size() != b.size())
+    {
+        return false;
+    }
+    return CRYPTO_memcmp(a.data(), b.data(), a.size()) == 0;
+}
+
+struct ConstantTimeCompare
+{
+    bool operator()(const std::string_view a, const std::string_view b) const
+    {
+        return constantTimeStringCompare(a, b);
+    }
+};
 
 } // namespace utility
 } // namespace crow