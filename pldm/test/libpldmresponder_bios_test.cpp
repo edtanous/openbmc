openbmc/pldm

bios: table construct: Add support for integer attribute
Add support for integer attribute to construct attribute
table and attribute value table

Signed-off-by: John Wang <wangzqbj@inspur.com>
Change-Id: I98a027a32d935cef704b3e7052f87c1af57fddf8

test/libpldmresponder_bios_test.cpp
@@ -592,3 +592,70 @@ TEST_F(TestSingleTypeBIOSTable, getBIOSAttributeValueTableBasedOnStringTypeTest)
     }
 
 } // end TEST
+
+TEST_F(TestSingleTypeBIOSTable,
+       getBIOSAttributeValueTableBasedOnIntegerTypeTest)
+{
+    // Copy integer json file to the destination
+    TestSingleTypeBIOSTable::CopySingleJsonFile(bios_parser::bIOSIntegerJson);
+    auto fpath = TestSingleTypeBIOSTable::destBiosPath.c_str();
+
+    std::array<uint8_t, sizeof(pldm_msg_hdr) + PLDM_GET_BIOS_TABLE_REQ_BYTES>
+        requestPayload{};
+    auto request = reinterpret_cast<pldm_msg*>(requestPayload.data());
+    struct pldm_get_bios_table_req* req =
+        (struct pldm_get_bios_table_req*)request->payload;
+
+    // Get string table with integer json file only
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_STRING_TABLE;
+
+    size_t requestPayloadLength = requestPayload.size() - sizeof(pldm_msg_hdr);
+    auto str_response =
+        internal::buildBIOSTables(request, requestPayloadLength, fpath, fpath);
+
+    // Get attribute table with integer json file only
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_ATTR_TABLE;
+
+    auto attr_response =
+        internal::buildBIOSTables(request, requestPayloadLength, fpath, fpath);
+
+    // Get attribute value table with integer type
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_ATTR_VAL_TABLE;
+
+    // Test attribute SBE_IMAGE_MINIMUM_VALID_ECS here, which has no dbus
+    for (uint8_t times = 0; times < 2; times++)
+    { // first time first table second time existing table
+        auto response = internal::buildBIOSTables(request, requestPayloadLength,
+                                                  fpath, fpath);
+        auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
+
+        struct pldm_get_bios_table_resp* resp =
+            reinterpret_cast<struct pldm_get_bios_table_resp*>(
+                responsePtr->payload);
+
+        ASSERT_EQ(0, resp->completion_code);
+        ASSERT_EQ(0, resp->next_transfer_handle);
+        ASSERT_EQ(PLDM_START_AND_END, resp->transfer_flag);
+        uint8_t* tableData = reinterpret_cast<uint8_t*>(resp->table_data);
+
+        while (true)
+        {
+            struct pldm_bios_attr_val_table_entry* ptr =
+                reinterpret_cast<struct pldm_bios_attr_val_table_entry*>(
+                    tableData);
+            uint16_t attrHdl = ptr->attr_handle;
+            uint8_t attrType = ptr->attr_type;
+            EXPECT_EQ(PLDM_BIOS_INTEGER_READ_ONLY, attrType);
+            tableData += sizeof(attrHdl) + sizeof(attrType);
+            auto cv = *(reinterpret_cast<uint64_t*>(tableData));
+            EXPECT_EQ(2, cv);
+            break; // testing for first row
+        }
+    }
+}

edtanous/openbmc

pldm/test/libpldmresponder_bios_test.cpp
@@ -593,3 +593,63 @@ TEST_F(TestSingleTypeBIOSTable, getBIOSAttributeValueTableBasedOnStringTypeTest)
     }
 
 } // end TEST
+
+TEST_F(TestSingleTypeBIOSTable,
+       getBIOSAttributeValueTableBasedOnIntegerTypeTest)
+{
+    // Copy integer json file to the destination
+    TestSingleTypeBIOSTable::CopySingleJsonFile(bios_parser::bIOSIntegerJson);
+    auto fpath = TestSingleTypeBIOSTable::destBiosPath.c_str();
+
+    std::array<uint8_t, sizeof(pldm_msg_hdr) + PLDM_GET_BIOS_TABLE_REQ_BYTES>
+        requestPayload{};
+    auto request = reinterpret_cast<pldm_msg*>(requestPayload.data());
+    struct pldm_get_bios_table_req* req =
+        (struct pldm_get_bios_table_req*)request->payload;
+
+    // Get string table with integer json file only
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_STRING_TABLE;
+
+    size_t requestPayloadLength = requestPayload.size() - sizeof(pldm_msg_hdr);
+    internal::buildBIOSTables(request, requestPayloadLength, fpath, fpath);
+
+    // Get attribute table with integer json file only
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_ATTR_TABLE;
+
+    auto attr_response =
+        internal::buildBIOSTables(request, requestPayloadLength, fpath, fpath);
+
+    // Get attribute value table with integer type
+    req->transfer_handle = 9;
+    req->transfer_op_flag = PLDM_GET_FIRSTPART;
+    req->table_type = PLDM_BIOS_ATTR_VAL_TABLE;
+
+    // Test attribute SBE_IMAGE_MINIMUM_VALID_ECS here, which has no dbus
+    for (uint8_t times = 0; times < 2; times++)
+    { // first time first table second time existing table
+        auto response = internal::buildBIOSTables(request, requestPayloadLength,
+                                                  fpath, fpath);
+        auto responsePtr = reinterpret_cast<pldm_msg*>(response.data());
+
+        struct pldm_get_bios_table_resp* resp =
+            reinterpret_cast<struct pldm_get_bios_table_resp*>(
+                responsePtr->payload);
+
+        EXPECT_EQ(0, resp->completion_code);
+        EXPECT_EQ(0, resp->next_transfer_handle);
+        EXPECT_EQ(PLDM_START_AND_END, resp->transfer_flag);
+        uint8_t* tableData = reinterpret_cast<uint8_t*>(resp->table_data);
+        auto ptr =
+            reinterpret_cast<struct pldm_bios_attr_val_table_entry*>(tableData);
+        uint16_t attrHdl = ptr->attr_handle;
+        uint8_t attrType = ptr->attr_type;
+        EXPECT_EQ(PLDM_BIOS_INTEGER_READ_ONLY, attrType);
+        tableData += sizeof(attrHdl) + sizeof(attrType);
+        auto cv = *(reinterpret_cast<uint64_t*>(tableData));
+        EXPECT_EQ(2, cv);
+    }
+}