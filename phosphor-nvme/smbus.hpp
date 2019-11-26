openbmc/phosphor-nvme

Add basic function for phosphor-nvme.
Add basic function to monitor temperature of
NVMe drive for phosphor-nvme.

Change-Id: Iebe3c8bf4b5cb958defab5b24a88f4bce477ad5e
Signed-off-by: Tony Lee <tony.lee@quantatw.com>

smbus.hpp
@@ -1,0 +1,35 @@
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "i2c-dev.h"
+
+namespace phosphor
+{
+namespace smbus
+{
+
+class Smbus
+{
+  public:
+    Smbus(){};
+
+    int open_i2c_dev(int i2cbus, char* filename, size_t size, int quiet);
+
+    int smbusInit(int smbus_num);
+
+    void smbusClose(int smbus_num);
+
+    int SendSmbusRWBlockCmdRAW(int smbus_num, int8_t device_addr,
+                               uint8_t* tx_data, uint8_t tx_len,
+                               uint8_t* rsp_data);
+};
+
+} // namespace smbus
+} // namespace phosphor

edtanous/openbmc

phosphor-nvme/smbus.hpp
@@ -1,0 +1,34 @@
+#pragma once
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+namespace phosphor
+{
+namespace smbus
+{
+
+class Smbus
+{
+  public:
+    Smbus(){};
+
+    int openI2cDev(int i2cbus, char* filename, size_t size, int quiet);
+
+    int smbusInit(int smbus_num);
+
+    void smbusClose(int smbus_num);
+
+    int SendSmbusRWBlockCmdRAW(int smbus_num, int8_t device_addr,
+                               uint8_t* tx_data, uint8_t tx_len,
+                               uint8_t* rsp_data);
+};
+
+} // namespace smbus
+} // namespace phosphor