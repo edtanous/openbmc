openbmc/intel-ipmi-oem

Sensor list time improvements
A lot inefficient code was added since these commands
were originally implemented. Fix the caches and add yield
method calls where calls were expensive.

Tested: ipmitool sensor list for 104 sensors was 18 seconds
over lan

Change-Id: I9ec2d42303839257629001f4cbb5cc1417989754
Signed-off-by: James Feist <james.feist@linux.intel.com>

include/storagecommands.hpp
@@ -117,9 +117,10 @@ namespace storage
 {
 
 constexpr const size_t type12Count = 2;
-ipmi_ret_t getFruSdrs(size_t index, get_sdr::SensorDataFruRecord& resp);
+ipmi_ret_t getFruSdrs(boost::asio::yield_context yield, size_t index,
+                      get_sdr::SensorDataFruRecord& resp);
 
-ipmi_ret_t getFruSdrCount(size_t& count);
+ipmi_ret_t getFruSdrCount(boost::asio::yield_context yield, size_t& count);
 
 std::vector<uint8_t> getType12SDRs(uint16_t index, uint16_t recordId);
 } // namespace storage

edtanous/openbmc

intel-ipmi-oem/include/storagecommands.hpp
@@ -117,9 +117,10 @@ namespace storage
 {
 
 constexpr const size_t type12Count = 2;
-ipmi_ret_t getFruSdrs(size_t index, get_sdr::SensorDataFruRecord& resp);
+ipmi_ret_t getFruSdrs(ipmi::Context::ptr ctx, size_t index,
+                      get_sdr::SensorDataFruRecord& resp);
 
-ipmi_ret_t getFruSdrCount(size_t& count);
+ipmi_ret_t getFruSdrCount(ipmi::Context::ptr ctx, size_t& count);
 
 std::vector<uint8_t> getType12SDRs(uint16_t index, uint16_t recordId);
 } // namespace storage