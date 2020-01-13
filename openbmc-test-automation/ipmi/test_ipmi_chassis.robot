openbmc/openbmc-test-automation

Replaced REST with Redfish for IPMI test cases
IPMI test cases modified:
   - Verify Host PowerOff Via IPMI
   - Verify Host PowerOn Via IPMI

Change-Id: I6a69b26880a34011e64373bd1dd66176272436c0
Signed-off-by: Sushma M M <sushmm99@in.ibm.com>

ipmi/test_ipmi_chassis.robot
@@ -27,3 +27,19 @@ IPMI Chassis Status Off
     ${resp}=  Run IPMI Standard Command  chassis status
     ${power_status}=  Get Lines Containing String  ${resp}  System Power
     Should Contain  ${power_status}  off
+
+Verify Host PowerOff Via IPMI
+    [Documentation]   Verify host power off status using external IPMI command.
+    [Tags]  Verify_Host_PowerOff_Via_IPMI
+
+    Redfish Power On
+    Run External IPMI Standard Command  chassis power off
+    Wait Until Keyword Succeeds  3 min  10 sec  Is Host Off Via IPMI
+
+Verify Host PowerOn Via IPMI
+    [Documentation]   Verify host power on status using external IPMI command.
+    [Tags]  Verify_Host_PowerOn_Via_IPMI
+
+    Redfish Power Off
+    Run External IPMI Standard Command  chassis power on
+    Wait Until Keyword Succeeds  3 min  10 sec  Is Host On Via IPMI

edtanous/openbmc

openbmc-test-automation/ipmi/test_ipmi_chassis.robot
@@ -27,3 +27,19 @@ IPMI Chassis Status Off
     ${resp}=  Run IPMI Standard Command  chassis status
     ${power_status}=  Get Lines Containing String  ${resp}  System Power
     Should Contain  ${power_status}  off
+
+Verify Host PowerOff Via IPMI
+    [Documentation]   Verify host power off operation using external IPMI command.
+    [Tags]  Verify_Host_PowerOff_Via_IPMI
+
+    IPMI Power Off
+    ${ipmi_state}=  Get Host State Via External IPMI
+    Valid Value  ipmi_state  ['off']
+
+Verify Host PowerOn Via IPMI
+    [Documentation]   Verify host power on operation using external IPMI command.
+    [Tags]  Verify_Host_PowerOn_Via_IPMI
+
+    IPMI Power On
+    ${ipmi_state}=  Get Host State Via External IPMI
+    Valid Value  ipmi_state  ['on']