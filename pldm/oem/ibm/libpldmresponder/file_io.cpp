openbmc/pldm

pldm: Moving utils function into the common directory
Moving libpldmresponder/utils function into the common root directory.
build and make that as a library for ues.

Signed-off-by: George Liu <liuxiwei@inspur.com>
Change-Id: I4fbd0da4bc6f27e9a9ed711962bd8737d8b55ebf

oem/ibm/libpldmresponder/file_io.cpp
@@ -4,7 +4,7 @@
 
 #include "file_io_by_type.hpp"
 #include "file_table.hpp"
-#include "libpldmresponder/utils.hpp"
+#include "utility.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <fcntl.h>

pldmd.cpp
@@ -3,7 +3,7 @@
 #include "libpldmresponder/base.hpp"
 #include "libpldmresponder/bios.hpp"
 #include "libpldmresponder/platform.hpp"
-#include "libpldmresponder/utils.hpp"
+#include "utility.hpp"
 
 #include <err.h>
 #include <getopt.h>
@@ -166,7 +166,7 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    responder::utils::CustomFD socketFd(sockfd);
+    pldm::utils::CustomFD socketFd(sockfd);
 
     struct sockaddr_un addr
     {

tool/pldm_cmd_helper.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "libpldmresponder/utils.hpp"
+#include "utility.hpp"
 
 #include <err.h>
 #include <sys/socket.h>
@@ -22,7 +22,7 @@ namespace pldmtool
 namespace helper
 {
 
-using namespace pldm::responder::utils;
+using namespace pldm::utils;
 constexpr uint8_t PLDM_ENTITY_ID = 8;
 constexpr uint8_t MCTP_MSG_TYPE_PLDM = 1;
 constexpr uint8_t PLDM_LOCAL_INSTANCE_ID = 0;

edtanous/openbmc

pldm/oem/ibm/libpldmresponder/file_io.cpp
@@ -4,7 +4,7 @@
 
 #include "file_io_by_type.hpp"
 #include "file_table.hpp"
-#include "libpldmresponder/utils.hpp"
+#include "utils.hpp"
 #include "xyz/openbmc_project/Common/error.hpp"
 
 #include <fcntl.h>
@@ -75,7 +75,7 @@ int DMA::transferDataHost(int fd, uint32_t offset, uint32_t length,
         return rc;
     }
 
-    utils::CustomFD xdmaFd(dmaFd);
+    pldm::utils::CustomFD xdmaFd(dmaFd);
 
     void* vgaMem;
     vgaMem = mmap(nullptr, pageAlignedLength, upstream ? PROT_WRITE : PROT_READ,

pldm/pldmd.cpp
@@ -3,7 +3,7 @@
 #include "libpldmresponder/base.hpp"
 #include "libpldmresponder/bios.hpp"
 #include "libpldmresponder/platform.hpp"
-#include "libpldmresponder/utils.hpp"
+#include "utils.hpp"
 
 #include <err.h>
 #include <getopt.h>
@@ -163,7 +163,7 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    responder::utils::CustomFD socketFd(sockfd);
+    pldm::utils::CustomFD socketFd(sockfd);
 
     struct sockaddr_un addr
     {

pldm/tool/pldm_cmd_helper.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "libpldmresponder/utils.hpp"
+#include "utils.hpp"
 
 #include <err.h>
 #include <sys/socket.h>
@@ -23,7 +23,7 @@ namespace pldmtool
 namespace helper
 {
 
-using namespace pldm::responder::utils;
+using namespace pldm::utils;
 constexpr uint8_t PLDM_ENTITY_ID = 8;
 constexpr uint8_t MCTP_MSG_TYPE_PLDM = 1;
 constexpr uint8_t PLDM_LOCAL_INSTANCE_ID = 0;