openbmc/openpower-proc-control

Create PEL for boot errors with error XML data and traces
Add callback method into boot library to receive
status of boot load.

Add callback method into PHAL library to get back
traces logged so that the same can be used in
creation of PEL during failure.

Tested:
"User Data 1": {
    "Section Version": "1",
    "Sub-section type": "1",
    "Created by": "0x2000",
    "CBS_CS_IDLE_VALUE": "00000002",
    "CBS_CS_READ": "84000002",
    "HWP Error description": "CBS did not complete (did not arrive in IDLE
state) within timeout",
    "HW_DELAY": "0009c400",
    "HwpReturnCode": "RC_CBS_NOT_IN_IDLE_STATE",
    "LOG000 2020-03-08 06:26:34": "inf: p10_start_cbs:58 ",
    "LOG001 2020-03-08 06:26:34": "p10_start_cbs: Entering ...",
    "LOG002 2020-03-08 06:26:34": "err: CBS_NOT_IN_IDLE_STATE:36720 ",
    "LOG003 2020-03-08 06:26:34": "CBS did not complete (did not arrive in IDLE
state) within timeout",
    "LOG004 2020-03-08 06:26:34": "err: _setHwpError:114 ",
    "LOG005 2020-03-08 06:26:34": "_setHwpError: Creating HWP error 0xa97f5",
    "LOG006 2020-03-08 06:26:34": "err: p10_start_cbs:119 ",
    "LOG007 2020-03-08 06:26:34": "ERROR: CBS HAS NOT REACHED IDLE STATE VALUE
0x002 ",
    "LOOP_COUNT": "000000c8",
    "_PID": "495"
}

Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
Change-Id: I06491f09041edcd16d8db5890c74a4db8d08e920

phalerror/phal_error.cpp
@@ -1,0 +1,100 @@
+#include "phal_error.hpp"
+
+#include "create_pel.hpp"
+
+#include <libekb.H>
+#include <libipl.H>
+
+#include <iomanip>
+#include <phosphor-logging/elog.hpp>
+#include <sstream>
+
+namespace openpower
+{
+namespace pel
+{
+using namespace phosphor::logging;
+
+namespace detail
+{
+
+// keys need to be unique so using counter value to generate unique key
+static int counter = 0;
+
+// list of debug traces
+static std::vector<std::pair<std::string, std::string>> traceLog;
+
+void processLogTraceCallback(void* private_data, const char* fmt, va_list ap)
+{
+    va_list vap;
+    va_copy(vap, ap);
+    std::vector<char> log(1 + std::vsnprintf(nullptr, 0, fmt, ap));
+    std::vsnprintf(log.data(), log.size(), fmt, vap);
+    va_end(vap);
+    std::string logstr(log.begin(), log.end());
+
+    // ignore stray characaters in log traces coming from ekb
+    if (logstr.length() < 5)
+    {
+        return;
+    }
+
+    char timeBuf[80];
+    time_t t = time(0);
+    tm myTm{};
+    gmtime_r(&t, &myTm);
+    strftime(timeBuf, 80, "%Y-%m-%d %H:%M:%S", &myTm);
+
+    // key values need to be unique for PEL
+    // TODO #openbmc/dev/issues/1563
+    // If written to Json no need to worry about unique KEY
+    std::stringstream str;
+    str << std::setfill('0');
+    str << "LOG" << std::setw(3) << counter;
+    str << " " << timeBuf;
+    traceLog.push_back(std::make_pair(str.str(), std::move(logstr)));
+    counter++;
+}
+
+void processBootErrorCallback(bool status)
+{
+    log<level::INFO>("processBootCallback ", entry("STATUS=%d", status));
+    try
+    {
+        // If failure in hwp execution
+        if (!status)
+        {
+            FFDCData ffdc = libekb_get_ffdc();
+            ffdc.insert(ffdc.end(), traceLog.begin(), traceLog.end());
+            openpower::pel::createBootErrorPEL(ffdc);
+        }
+    }
+    catch (std::exception& ex)
+    {
+        reset();
+        throw ex;
+    }
+    reset();
+}
+
+void reset()
+{
+    // reset the trace log and counter
+    traceLog.clear();
+    counter = 0;
+}
+} // namespace detail
+
+void addBootErrorCallbacks()
+{
+    // set log level to info
+    ipl_set_loglevel(IPL_INFO);
+
+    // add callback for debug traces
+    ipl_set_logfunc(detail::processLogTraceCallback, NULL);
+
+    // add callback for ipl failures
+    ipl_set_app_callback_func(detail::processBootErrorCallback);
+}
+} // namespace pel
+} // namespace openpower

edtanous/openbmc

openpower-proc-control/phalerror/phal_error.cpp
@@ -1,0 +1,101 @@
+#include "phal_error.hpp"
+
+#include "create_pel.hpp"
+
+#include <libekb.H>
+#include <libipl.H>
+
+#include <iomanip>
+#include <phosphor-logging/elog.hpp>
+#include <sstream>
+
+namespace openpower
+{
+namespace pel
+{
+using namespace phosphor::logging;
+
+namespace detail
+{
+
+// keys need to be unique so using counter value to generate unique key
+static int counter = 0;
+
+// list of debug traces
+static std::vector<std::pair<std::string, std::string>> traceLog;
+
+void processLogTraceCallback(void* private_data, const char* fmt, va_list ap)
+{
+    va_list vap;
+    va_copy(vap, ap);
+    std::vector<char> log(1 + std::vsnprintf(nullptr, 0, fmt, ap));
+    std::vsnprintf(log.data(), log.size(), fmt, vap);
+    va_end(vap);
+    std::string logstr(log.begin(), log.end());
+
+    // ignore stray characters in log traces coming from ekb
+    // example: "\n", which are good for print to screen
+    if (logstr.length() < 5)
+    {
+        return;
+    }
+
+    char timeBuf[80];
+    time_t t = time(0);
+    tm myTm{};
+    gmtime_r(&t, &myTm);
+    strftime(timeBuf, 80, "%Y-%m-%d %H:%M:%S", &myTm);
+
+    // key values need to be unique for PEL
+    // TODO #openbmc/dev/issues/1563
+    // If written to Json no need to worry about unique KEY
+    std::stringstream str;
+    str << std::setfill('0');
+    str << "LOG" << std::setw(3) << counter;
+    str << " " << timeBuf;
+    traceLog.emplace_back(std::make_pair(str.str(), std::move(logstr)));
+    counter++;
+}
+
+void processBootErrorCallback(bool status)
+{
+    log<level::INFO>("processBootCallback ", entry("STATUS=%d", status));
+    try
+    {
+        // If failure in hwp execution
+        if (!status)
+        {
+            FFDCData ffdc = libekb_get_ffdc();
+            ffdc.insert(ffdc.end(), traceLog.begin(), traceLog.end());
+            openpower::pel::createBootErrorPEL(ffdc);
+        }
+    }
+    catch (std::exception& ex)
+    {
+        reset();
+        throw ex;
+    }
+    reset();
+}
+
+void reset()
+{
+    // reset the trace log and counter
+    traceLog.clear();
+    counter = 0;
+}
+} // namespace detail
+
+void addBootErrorCallbacks()
+{
+    // set log level to info
+    ipl_set_loglevel(IPL_INFO);
+
+    // add callback for debug traces
+    ipl_set_logfunc(detail::processLogTraceCallback, NULL);
+
+    // add callback for ipl failures
+    ipl_set_app_callback_func(detail::processBootErrorCallback);
+}
+} // namespace pel
+} // namespace openpower