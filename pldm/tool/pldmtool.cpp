openbmc/pldm

pldmtool: oem: Implement GetAlertStatus command
Tested:
 pldmtool oem-ibm GetAlertStatus -i 0
 Encode request successfully
 Request Message:
 08 01 80 3f f0 00
 Success in creating the socket : RC = 4
 Success in connecting to socket : RC = 0
 Success in sending message type as pldm to mctp : RC = 0
 Write to socket successful : RC = 6
 Total length: 14
 Shutdown Socket successful :  RC = 0
 Response Message:
 08 01 00 3f f0 00 30 00 00 ff 30 80 00 00
 GetAlertStatus Success:
 rack entry: 0xff000030
 pri cec node: 0x8030

Signed-off-by: George Liu <liuxiwei@inspur.com>
Change-Id: I5650591f547abb4ea5961e51ff11af8d1d20b4f2

tool/pldmtool.cpp
@@ -3,6 +3,7 @@
 #include "pldm_cmd_helper.hpp"
 #include "pldm_fru_cmd.hpp"
 #include "pldm_platform_cmd.hpp"
+#include "tool/oem/ibm/pldm_power_host_cmd.hpp"
 
 #include <CLI/CLI.hpp>
 
@@ -72,6 +73,7 @@ int main(int argc, char** argv)
     pldmtool::bios::registerCommand(app);
     pldmtool::platform::registerCommand(app);
     pldmtool::fru::registerCommand(app);
+    pldmtool::oem_ibm::power_host::registerCommand(app);
 
     CLI11_PARSE(app, argc, argv);
     return 0;

edtanous/openbmc

 pri cec node: 0x00008030

pldm/tool/pldmtool.cpp
@@ -3,6 +3,7 @@
 #include "pldm_cmd_helper.hpp"
 #include "pldm_fru_cmd.hpp"
 #include "pldm_platform_cmd.hpp"
+#include "tool/oem/ibm/pldm_host_cmd.hpp"
 
 #include <CLI/CLI.hpp>
 
@@ -72,6 +73,7 @@ int main(int argc, char** argv)
     pldmtool::bios::registerCommand(app);
     pldmtool::platform::registerCommand(app);
     pldmtool::fru::registerCommand(app);
+    pldmtool::oem_ibm::power_host::registerCommand(app);
 
     CLI11_PARSE(app, argc, argv);
     return 0;