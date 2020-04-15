openbmc/phosphor-power

regs: Subscribe to InterfacesAdded signals
Subscribe to InterfacesAdded signals to handle not being able to read
the JSON configuration data filename from dbus due to the service
hosting that property is not available yet. The InterfacesAdded signal
is used to get notified when that property becomes available on dbus and
is read upon receiving the signal.

Tested:
    InterfacesAdded signals on an object path is received
    A string is used as the filename when the path, interface, and
property match from the InterfacesAdded signals received

Signed-off-by: Matthew Barth <msbarth@us.ibm.com>
Change-Id: I0256145b9d2772efaae560513c73dc883ecd0a5b

phosphor-regulators/src/manager.cpp
@@ -35,8 +35,13 @@ Manager::Manager(sdbusplus::bus::bus& bus, const sdeventplus::Event& event) :
     // Attempt to get the filename property from dbus
     setFileName(getFileNameDbus());
 
-    // TODO Subscribe to interfacesAdded signal for filename property
-    // Callback should set fileName and call parse json function
+    // Subscribe to interfacesAdded signal for filename property
+    std::unique_ptr<sdbusplus::server::match::match> matchPtr =
+        std::make_unique<sdbusplus::server::match::match>(bus,
+            sdbusplus::bus::match::rules::interfacesAdded(sysDbusObj).c_str(),
+            std::bind(std::mem_fn(&Manager::signalHandler), this,
+                      std::placeholders::_1));
+    signals.emplace_back(std::move(matchPtr));
 
     if (!fileName.empty())
     {
@@ -86,6 +91,41 @@ void Manager::sighupHandler(sdeventplus::source::Signal& sigSrc,
     (void)sigInfo;
 
     // TODO Reload and process the configuration data
+}
+
+void Manager::signalHandler(sdbusplus::message::message& msg)
+{
+    using namespace sdbusplus::message;
+
+    if (msg)
+    {
+        object_path op;
+        msg.read(op);
+        if (static_cast<const std::string&>(op) != sysDbusPath)
+        {
+            // Object path does not match the path
+            return;
+        }
+
+        std::map<std::string, std::map<std::string,
+                variant<std::string>>> intfProp;
+        msg.read(intfProp);
+        auto itIntf = intfProp.find(sysDbusIntf);
+        if (itIntf == intfProp.cend())
+        {
+            // Interface not found on the path
+            return;
+        }
+        auto itProp = itIntf->second.find(sysDbusProp);
+        if (itProp == itIntf->second.cend())
+        {
+            // Property not found on the interface
+            return;
+        }
+        // Set fileName and call parse json function
+        setFileName(variant_ns::get<std::string>(itProp->second));
+        // TODO Load & parse JSON configuration data file
+    }
 }
 
 const std::string Manager::getFileNameDbus()

edtanous/openbmc

phosphor-power/phosphor-regulators/src/manager.cpp
@@ -21,6 +21,7 @@
 #include <sdbusplus/bus.hpp>
 
 #include <chrono>
+#include <variant>
 
 namespace phosphor
 {
@@ -32,11 +33,17 @@ namespace regulators
 Manager::Manager(sdbusplus::bus::bus& bus, const sdeventplus::Event& event) :
     ManagerObject(bus, objPath, true), bus(bus), eventLoop(event), fileName("")
 {
+    // Subscribe to interfacesAdded signal for filename property
+    std::unique_ptr<sdbusplus::server::match::match> matchPtr =
+        std::make_unique<sdbusplus::server::match::match>(
+            bus,
+            sdbusplus::bus::match::rules::interfacesAdded(sysDbusObj).c_str(),
+            std::bind(std::mem_fn(&Manager::signalHandler), this,
+                      std::placeholders::_1));
+    signals.emplace_back(std::move(matchPtr));
+
     // Attempt to get the filename property from dbus
     setFileName(getFileNameDbus());
-
-    // TODO Subscribe to interfacesAdded signal for filename property
-    // Callback should set fileName and call parse json function
 
     if (!fileName.empty())
     {
@@ -80,6 +87,42 @@ void Manager::sighupHandler(sdeventplus::source::Signal& /*sigSrc*/,
                             const struct signalfd_siginfo* /*sigInfo*/)
 {
     // TODO Reload and process the configuration data
+}
+
+void Manager::signalHandler(sdbusplus::message::message& msg)
+{
+    if (msg)
+    {
+        sdbusplus::message::object_path op;
+        msg.read(op);
+        if (static_cast<const std::string&>(op) != sysDbusPath)
+        {
+            // Object path does not match the path
+            return;
+        }
+
+        // An interfacesAdded signal returns a dictionary of interface
+        // names to a dictionary of properties and their values
+        // https://dbus.freedesktop.org/doc/dbus-specification.html
+        std::map<std::string, std::map<std::string, std::variant<std::string>>>
+            intfProp;
+        msg.read(intfProp);
+        auto itIntf = intfProp.find(sysDbusIntf);
+        if (itIntf == intfProp.cend())
+        {
+            // Interface not found on the path
+            return;
+        }
+        auto itProp = itIntf->second.find(sysDbusProp);
+        if (itProp == itIntf->second.cend())
+        {
+            // Property not found on the interface
+            return;
+        }
+        // Set fileName and call parse json function
+        setFileName(std::get<std::string>(itProp->second));
+        // TODO Load & parse JSON configuration data file
+    }
 }
 
 const std::string Manager::getFileNameDbus()