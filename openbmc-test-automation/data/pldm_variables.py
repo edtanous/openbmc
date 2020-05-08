openbmc/openbmc-test-automation

PLDM: Automate below test scenarios for pldmtool oem specific commands
    Verify GetPLDMTypes For OEM
    Verify GetPLDMVersion For OEM
    Verify GetPLDMCommands For OEM
    Verify GetAlertStatus

Change-Id: I4802d8f94366e9e48644d7cbaf02daf0f9d35b93
Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

data/pldm_variables.py
@@ -4,19 +4,20 @@ r"""
 Contains PLDM-related constants.
 """
 
-PLDM_SUPPORTED_TYPES = ['base', 'platform', 'bios']
+PLDM_SUPPORTED_TYPES = ['base', 'platform', 'bios', 'fru', 'oem-ibm']
 
 # PLDM types.
 PLDM_TYPE_BASE = {'VALUE': '00', 'STRING': 'base'}
 PLDM_TYPE_PLATFORM = {'VALUE': '02', 'STRING': 'platform'}
 PLDM_TYPE_BIOS = {'VALUE': '03', 'STRING': 'bios'}
 PLDM_TYPE_FRU = {'VALUE': '04', 'STRING': 'fru'}
-PLDM_TYPE_OEM = {'VALUE': '3F', 'STRING': 'oem'}
+PLDM_TYPE_OEMIBM = {'VALUE': '63', 'STRING': 'oem-ibm'}
 
 VERSION_BASE = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
 VERSION_PLATFORM = {'VALUE': ['f1', 'f2', 'f0', '00'], 'STRING': '1.2.0'}
 VERSION_BIOS = {'VALUE': ['f1', 'f1', 'f1', '00'], 'STRING': '1.0.0'}
 VERSION_FRU = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
+VERSION_OEMIBM = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
 
 
 PLDM_BASE_CMDS = ['2(GetTID)', '3(GetPLDMVersion)', '4(GetPLDMTypes)', '5(GetPLDMCommands)']
@@ -25,6 +26,11 @@ PLDM_BIOS_CMDS = ['1(GetBIOSTable)', '7(SetBIOSAttributeCurrentValue)',
                   '8(GetBIOSAttributeCurrentValueByHandle)', '12(GetDateTime)',
                   '13(SetDateTime)']
 PLDM_FRU_CMDS = ['1(GetFRURecordTableMetadata)', '2(GetFRURecordTable)']
+PLDM_OEMIBM_CMDS = ['1(GetFileTable)', '4(ReadFile)', '5(WriteFile)', '6(ReadFileInToMemory)',
+                    '7(WriteFileFromMemory)', '8(ReadFileByTypeIntoMemory)',
+                    '9(WriteFileByTypeFromMemory)', '10(NewFileAvailable)',
+                    '11(ReadFileByType)', '12(WriteFileByType)', '13(FileAck)',
+                    '240(GetAlertStatus)']
 
 # PLDM command format.
 

edtanous/openbmc

openbmc-test-automation/data/pldm_variables.py
@@ -4,19 +4,20 @@ r"""
 Contains PLDM-related constants.
 """
 
-PLDM_SUPPORTED_TYPES = ['base', 'platform', 'bios']
+PLDM_SUPPORTED_TYPES = ['base', 'platform', 'bios', 'fru', 'oem-ibm']
 
 # PLDM types.
 PLDM_TYPE_BASE = {'VALUE': '00', 'STRING': 'base'}
 PLDM_TYPE_PLATFORM = {'VALUE': '02', 'STRING': 'platform'}
 PLDM_TYPE_BIOS = {'VALUE': '03', 'STRING': 'bios'}
 PLDM_TYPE_FRU = {'VALUE': '04', 'STRING': 'fru'}
-PLDM_TYPE_OEM = {'VALUE': '3F', 'STRING': 'oem'}
+PLDM_TYPE_OEM = {'VALUE': '63', 'STRING': 'oem-ibm'}
 
 VERSION_BASE = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
 VERSION_PLATFORM = {'VALUE': ['f1', 'f2', 'f0', '00'], 'STRING': '1.2.0'}
 VERSION_BIOS = {'VALUE': ['f1', 'f1', 'f1', '00'], 'STRING': '1.0.0'}
 VERSION_FRU = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
+VERSION_OEM = {'VALUE': ['f1', 'f0', 'f0', '00'], 'STRING': '1.0.0'}
 
 
 PLDM_BASE_CMDS = ['2(GetTID)', '3(GetPLDMVersion)', '4(GetPLDMTypes)', '5(GetPLDMCommands)']
@@ -25,6 +26,11 @@ PLDM_BIOS_CMDS = ['1(GetBIOSTable)', '7(SetBIOSAttributeCurrentValue)',
                   '8(GetBIOSAttributeCurrentValueByHandle)', '12(GetDateTime)',
                   '13(SetDateTime)']
 PLDM_FRU_CMDS = ['1(GetFRURecordTableMetadata)', '2(GetFRURecordTable)']
+PLDM_OEM_CMDS = ['1(GetFileTable)', '4(ReadFile)', '5(WriteFile)', '6(ReadFileInToMemory)',
+                 '7(WriteFileFromMemory)', '8(ReadFileByTypeIntoMemory)',
+                 '9(WriteFileByTypeFromMemory)', '10(NewFileAvailable)',
+                 '11(ReadFileByType)', '12(WriteFileByType)', '13(FileAck)',
+                 '240(GetAlertStatus)']
 
 # PLDM command format.
 