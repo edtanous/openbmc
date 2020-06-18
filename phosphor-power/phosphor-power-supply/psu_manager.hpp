openbmc/phosphor-power

psu-ng: Add code to set ON_OFF_CONFIG
Add code that sends the appropriate ON_OFF_CONFIG mask for the power
supply power on operation.

Tested:
    Ran gtest using x86sdk
    Used simulator to change ON_OFF_CONFIG value, restarted service,
    verified ON_OFF_CONFIG changed.
    Used i2cget on hardware to change ON_OFF_CONFIG, restarted service,
    verified ON_OFF_CONFIG changed.

Signed-off-by: Brandon Wyman <bjwyman@gmail.com>
Change-Id: Ifd9d63fcd2e86a62b7358e08958b5e2dbd21db9f

phosphor-power-supply/psu_manager.hpp
@@ -87,6 +87,7 @@ class PSUManager
             powerOn = false;
         }
 
+        onOffConfig(phosphor::pmbus::ON_OFF_CONFIG_DATA);
         clearFaults();
         updateInventory();
     }
@@ -97,6 +98,20 @@ class PSUManager
     int run()
     {
         return timer->get_event().loop();
+    }
+
+    /**
+     * Write PMBus ON_OFF_CONFIG
+     *
+     * This function will be called to cause the PMBus device driver to send the
+     * ON_OFF_CONFIG command. Takes one byte of data.
+     */
+    void onOffConfig(const uint8_t data)
+    {
+        for (auto& psu : psus)
+        {
+            psu->onOffConfig(data);
+        }
     }
 
     /**

pmbus.cpp
@@ -303,6 +303,40 @@ void PMBus::write(const std::string& name, int value, Type type)
     }
 }
 
+void PMBus::writeBinary(const std::string& name, std::vector<uint8_t> data,
+                        Type type)
+{
+    std::ofstream file;
+    fs::path path = getPath(type);
+
+    path /= name;
+
+    file.exceptions(std::ofstream::failbit | std::ofstream::badbit |
+                    std::ofstream::eofbit);
+
+    try
+    {
+        // I need to specify binary mode when I construct the ofstream
+        file.open(path, std::ios::out | std::ios_base::binary);
+        log<level::INFO>("Write data to sysfs file",
+                         entry("FILENAME=%s", path.c_str()));
+        file.write(reinterpret_cast<const char*>(&data[0]), data.size());
+    }
+    catch (const std::exception& e)
+    {
+        auto rc = errno;
+
+        log<level::ERR>("Failed to write binary data to sysfs file",
+                        entry("FILENAME=%s", path.c_str()));
+
+        using metadata = xyz::openbmc_project::Common::Device::WriteFailure;
+
+        elog<WriteFailure>(
+            metadata::CALLOUT_ERRNO(rc),
+            metadata::CALLOUT_DEVICE_PATH(fs::canonical(basePath).c_str()));
+    }
+}
+
 void PMBus::findHwmonDir()
 {
     fs::path path{basePath};

pmbus.hpp
@@ -101,6 +101,24 @@ namespace status_temperature
 constexpr auto OT_FAULT = 0x80;
 } // namespace status_temperature
 
+constexpr auto ON_OFF_CONFIG = "on_off_config";
+
+// From PMBus Specification Part II Revsion 1.2:
+// The ON_OFF_CONFIG command configures the combination of CONTROL pin input
+// and serial bus commands needed to turn the unit on and off. This includes how
+// the unit responds when power is applied.
+// Bits [7:5] - 000 - Reserved
+// Bit 4 - 1 - Unit does not power up until commanded by the CONTROL pin and
+// OPERATION command (as programmed in bits [3:0]).
+// Bit 3 - 0 - Unit ignores the on/off portion of the OPERATION command from
+// serial bus.
+// Bit 2 - 1 - Unit requires the CONTROL pin to be asserted to start the unit.
+// Bit 1 - 0 - Polarity of the CONTROL pin. Active low (Pull pin low to start
+// the unit).
+// Bit 0 - 1 - Turn off the output and stop transferring energy to the output as
+// fast as possible.
+constexpr auto ON_OFF_CONFIG_DATA = 0x15;
+
 /**
  * Where the access should be done
  */
@@ -124,6 +142,8 @@ class PMBusBase
     virtual ~PMBusBase() = default;
 
     virtual uint64_t read(const std::string& name, Type type) = 0;
+    virtual void writeBinary(const std::string& name, std::vector<uint8_t> data,
+                             Type type) = 0;
 };
 
 /**
@@ -273,6 +293,16 @@ class PMBus : public PMBusBase
      * @param[in] type - Path type
      */
     void write(const std::string& name, int value, Type type);
+
+    /**
+     * Writes binary data to a file in sysfs.
+     *
+     * @param[in] name - path concatenated to basePath to write
+     * @param[in] data - The data to write to the file
+     * @param[in] type - Path type
+     */
+    void writeBinary(const std::string& name, std::vector<uint8_t> data,
+                     Type type) override;
 
     /**
      * Returns the sysfs base path of this device

edtanous/openbmc

phosphor-power/phosphor-power-supply/psu_manager.hpp
@@ -87,6 +87,7 @@ class PSUManager
             powerOn = false;
         }
 
+        onOffConfig(phosphor::pmbus::ON_OFF_CONFIG_CONTROL_PIN_ONLY);
         clearFaults();
         updateInventory();
     }
@@ -97,6 +98,20 @@ class PSUManager
     int run()
     {
         return timer->get_event().loop();
+    }
+
+    /**
+     * Write PMBus ON_OFF_CONFIG
+     *
+     * This function will be called to cause the PMBus device driver to send the
+     * ON_OFF_CONFIG command. Takes one byte of data.
+     */
+    void onOffConfig(const uint8_t data)
+    {
+        for (auto& psu : psus)
+        {
+            psu->onOffConfig(data);
+        }
     }
 
     /**

phosphor-power/pmbus.cpp
@@ -303,6 +303,40 @@ void PMBus::write(const std::string& name, int value, Type type)
     }
 }
 
+void PMBus::writeBinary(const std::string& name, std::vector<uint8_t> data,
+                        Type type)
+{
+    std::ofstream file;
+    fs::path path = getPath(type);
+
+    path /= name;
+
+    file.exceptions(std::ofstream::failbit | std::ofstream::badbit |
+                    std::ofstream::eofbit);
+
+    try
+    {
+        // I need to specify binary mode when I construct the ofstream
+        file.open(path, std::ios::out | std::ios_base::binary);
+        log<level::DEBUG>("Write data to sysfs file",
+                          entry("FILENAME=%s", path.c_str()));
+        file.write(reinterpret_cast<const char*>(&data[0]), data.size());
+    }
+    catch (const std::exception& e)
+    {
+        auto rc = errno;
+
+        log<level::ERR>("Failed to write binary data to sysfs file",
+                        entry("FILENAME=%s", path.c_str()));
+
+        using metadata = xyz::openbmc_project::Common::Device::WriteFailure;
+
+        elog<WriteFailure>(
+            metadata::CALLOUT_ERRNO(rc),
+            metadata::CALLOUT_DEVICE_PATH(fs::canonical(basePath).c_str()));
+    }
+}
+
 void PMBus::findHwmonDir()
 {
     fs::path path{basePath};

phosphor-power/pmbus.hpp
@@ -101,6 +101,24 @@ namespace status_temperature
 constexpr auto OT_FAULT = 0x80;
 } // namespace status_temperature
 
+constexpr auto ON_OFF_CONFIG = "on_off_config";
+
+// From PMBus Specification Part II Revsion 1.2:
+// The ON_OFF_CONFIG command configures the combination of CONTROL pin input
+// and serial bus commands needed to turn the unit on and off. This includes how
+// the unit responds when power is applied.
+// Bits [7:5] - 000 - Reserved
+// Bit 4 - 1 - Unit does not power up until commanded by the CONTROL pin and
+// OPERATION command (as programmed in bits [3:0]).
+// Bit 3 - 0 - Unit ignores the on/off portion of the OPERATION command from
+// serial bus.
+// Bit 2 - 1 - Unit requires the CONTROL pin to be asserted to start the unit.
+// Bit 1 - 0 - Polarity of the CONTROL pin. Active low (Pull pin low to start
+// the unit).
+// Bit 0 - 1 - Turn off the output and stop transferring energy to the output as
+// fast as possible.
+constexpr auto ON_OFF_CONFIG_CONTROL_PIN_ONLY = 0x15;
+
 /**
  * Where the access should be done
  */
@@ -124,6 +142,8 @@ class PMBusBase
     virtual ~PMBusBase() = default;
 
     virtual uint64_t read(const std::string& name, Type type) = 0;
+    virtual void writeBinary(const std::string& name, std::vector<uint8_t> data,
+                             Type type) = 0;
 };
 
 /**
@@ -273,6 +293,16 @@ class PMBus : public PMBusBase
      * @param[in] type - Path type
      */
     void write(const std::string& name, int value, Type type);
+
+    /**
+     * Writes binary data to a file in sysfs.
+     *
+     * @param[in] name - path concatenated to basePath to write
+     * @param[in] data - The data to write to the file
+     * @param[in] type - Path type
+     */
+    void writeBinary(const std::string& name, std::vector<uint8_t> data,
+                     Type type) override;
 
     /**
      * Returns the sysfs base path of this device