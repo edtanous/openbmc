openbmc/openpower-vpd-parser

VPD-Manager app skeleton
This commit implements the outline of VPD Manager
application.
Manager app provides multiple functionality over DBUS
to manage VPD data in the system.

This commit does not  implements the functionality, it
just implement the prototype of functionality that will
be provided by the app.

To build the app:
meson build
ninja -C build

Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
Change-Id: Iefc86468d0b7179c10764d35399189150773eada

vpd-manager/error.cpp
@@ -1,0 +1,31 @@
+#include <com/ibm/vpd/error.hpp>
+
+namespace sdbusplus
+{
+namespace com
+{
+namespace ibm
+{
+namespace vpd
+{
+namespace Error
+{
+const char* NotFound::name() const noexcept
+{
+    return errName;
+}
+const char* NotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* NotFound::what() const noexcept
+{
+    return errWhat;
+}
+
+} // namespace Error
+} // namespace vpd
+} // namespace ibm
+} // namespace com
+} // namespace sdbusplus
+

edtanous/openbmc

openpower-vpd-parser/vpd-manager/error.cpp
@@ -1,0 +1,78 @@
+#include <com/ibm/VPD/error.hpp>
+
+namespace sdbusplus
+{
+namespace com
+{
+namespace ibm
+{
+namespace VPD
+{
+namespace Error
+{
+const char* LocationNotFound::name() const noexcept
+{
+    return errName;
+}
+const char* LocationNotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* LocationNotFound::what() const noexcept
+{
+    return errWhat;
+}
+const char* NodeNotFound::name() const noexcept
+{
+    return errName;
+}
+const char* NodeNotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* NodeNotFound::what() const noexcept
+{
+    return errWhat;
+}
+const char* PathNotFound::name() const noexcept
+{
+    return errName;
+}
+const char* PathNotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* PathNotFound::what() const noexcept
+{
+    return errWhat;
+}
+const char* RecordNotFound::name() const noexcept
+{
+    return errName;
+}
+const char* RecordNotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* RecordNotFound::what() const noexcept
+{
+    return errWhat;
+}
+const char* KeywordNotFound::name() const noexcept
+{
+    return errName;
+}
+const char* KeywordNotFound::description() const noexcept
+{
+    return errDesc;
+}
+const char* KeywordNotFound::what() const noexcept
+{
+    return errWhat;
+}
+
+} // namespace Error
+} // namespace VPD
+} // namespace ibm
+} // namespace com
+} // namespace sdbusplus