openbmc/pldm

sensor_event: Correct the D-Bus object path
The hardcoded D-Bus object path for hypervisor network config is
corrected to match the settings object path.

Signed-off-by: Tom Joseph <tomjoseph@in.ibm.com>
Change-Id: Iba7ca207376a3d42a88a7715e64dcae28735b592

libpldmresponder/platform.cpp
@@ -27,28 +27,28 @@ const EventEntryMap eventEntryMap = {
         0x01010007, // SensorID for VMI Port 0 ipv4 = 7, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 1
                     // (Valid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf0/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/intf0/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", true},
     },
     {
         0x02010007, // SensorID for VMI Port 0 ipv4 = 7, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 2
                     // (Invalid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf0/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/intf0/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", false},
     },
     {
         0x01010008, // SensorID for VMI Port 1 ipv4 = 8, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 1
                     // (Valid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf1/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/intf1/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", true},
     },
     {
         0x02010008, // SensorID for VMI Port 1 ipv4 = 8, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 2
                     // (Invalid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf1/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/intf1/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", false},
     }};
 

edtanous/openbmc

pldm/libpldmresponder/platform.cpp
@@ -27,28 +27,28 @@ const EventEntryMap eventEntryMap = {
         0x01010007, // SensorID for VMI Port 0 ipv4 = 7, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 1
                     // (Valid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf0/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", true},
     },
     {
         0x02010007, // SensorID for VMI Port 0 ipv4 = 7, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 2
                     // (Invalid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf0/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/eth0/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", false},
     },
     {
         0x01010008, // SensorID for VMI Port 1 ipv4 = 8, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 1
                     // (Valid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf1/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", true},
     },
     {
         0x02010008, // SensorID for VMI Port 1 ipv4 = 8, SensorOffset for the
                     // State Set ID 15 = 1 & PLDM State Set Enumeration List = 2
                     // (Invalid Configuration)
-        {"/xyz/openbmc_project/network/vmi/intf1/ipv4/addr0",
+        {"/xyz/openbmc_project/network/hypervisor/eth1/ipv4/addr0",
          "xyz.openbmc_project.Object.Enable", "Enabled", "bool", false},
     }};
 