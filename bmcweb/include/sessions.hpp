openbmc/bmcweb

Modernize;  Move some apis to range based for loop
There were a couple places in code where we still use index based for
loops.  Move these to the more modern range based for loops.

Tested:  Needs testing.  Changes made by clang-tidy.

Signed-off-by: Ed Tanous <ed.tanous@intel.com>
Change-Id: I30bf6fae6b2540434d5c98900a8f6bd0c8f2be93

include/sessions.hpp
@@ -363,23 +363,23 @@ class SessionStore
         std::string sessionToken;
         sessionToken.resize(20, '0');
         std::uniform_int_distribution<size_t> dist(0, alphanum.size() - 1);
-        for (size_t i = 0; i < sessionToken.size(); ++i)
+        for (char & i : sessionToken)
         {
-            sessionToken[i] = alphanum[dist(rd)];
+            i = alphanum[dist(rd)];
         }
         // Only need csrf tokens for cookie based auth, token doesn't matter
         std::string csrfToken;
         csrfToken.resize(20, '0');
-        for (size_t i = 0; i < csrfToken.size(); ++i)
+        for (char & i : csrfToken)
         {
-            csrfToken[i] = alphanum[dist(rd)];
+            i = alphanum[dist(rd)];
         }
 
         std::string uniqueId;
         uniqueId.resize(10, '0');
-        for (size_t i = 0; i < uniqueId.size(); ++i)
+        for (char & i : uniqueId)
         {
-            uniqueId[i] = alphanum[dist(rd)];
+            i = alphanum[dist(rd)];
         }
 
         auto session = std::make_shared<UserSession>(UserSession{

edtanous/openbmc

bmcweb/include/sessions.hpp
@@ -233,9 +233,9 @@ class SessionStore
 
         OpenSSLGenerator gen;
 
-        for (size_t i = 0; i < sessionToken.size(); ++i)
+        for (char& sessionChar : sessionToken)
         {
-            sessionToken[i] = alphanum[dist(gen)];
+            sessionChar = alphanum[dist(gen)];
             if (gen.error())
             {
                 return nullptr;
@@ -244,9 +244,9 @@ class SessionStore
         // Only need csrf tokens for cookie based auth, token doesn't matter
         std::string csrfToken;
         csrfToken.resize(sessionTokenSize, '0');
-        for (size_t i = 0; i < csrfToken.size(); ++i)
+        for (char& csrfChar : csrfToken)
         {
-            csrfToken[i] = alphanum[dist(gen)];
+            csrfChar = alphanum[dist(gen)];
             if (gen.error())
             {
                 return nullptr;
@@ -255,9 +255,9 @@ class SessionStore
 
         std::string uniqueId;
         uniqueId.resize(10, '0');
-        for (size_t i = 0; i < uniqueId.size(); ++i)
+        for (char& uidChar : uniqueId)
         {
-            uniqueId[i] = alphanum[dist(gen)];
+            uidChar = alphanum[dist(gen)];
             if (gen.error())
             {
                 return nullptr;