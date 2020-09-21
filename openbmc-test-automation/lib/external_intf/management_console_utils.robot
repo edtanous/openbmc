openbmc/openbmc-test-automation

Re-write lock management test suit
Changes:
    - Re-structure lock management test suite.
    - Add new test case "Acquire And Release Lock In Loop".

Change-Id: I1d7021440e564bff5b4d7046e5b52a030d4adee3
Signed-off-by: Sushil Singh <susilsi7@in.ibm.com>

lib/external_intf/management_console_utils.robot
@@ -38,3 +38,15 @@ Verify AvahiDaemon Service Status
     ${service_message}=  Get From Dictionary  ${daemon_message}  ${message}
     ${resp}  ${stderr}  ${rc}=  BMC Execute Command  ${service_command}  print_out=1
     Should Contain  ${resp}  ${service_message}
+
+
+Get Lock Resource Information
+    [Documentation]  Get lock resource information.
+
+
+    ${code_base_dir_path}=  Get Code Base Dir Path
+    ${lock_resource_info}=  Evaluate
+    ...  json.load(open('${code_base_dir_path}data/lockresource_table.json'))  modules=json
+    Rprint Vars  lock_resource_info
+
+    [Return]  ${lock_resource_info}

edtanous/openbmc

openbmc-test-automation/lib/external_intf/management_console_utils.robot
@@ -38,3 +38,14 @@ Verify AvahiDaemon Service Status
     ${service_message}=  Get From Dictionary  ${daemon_message}  ${message}
     ${resp}  ${stderr}  ${rc}=  BMC Execute Command  ${service_command}  print_out=1
     Should Contain  ${resp}  ${service_message}
+
+
+Get Lock Resource Information
+    [Documentation]  Get lock resource information.
+
+    ${code_base_dir_path}=  Get Code Base Dir Path
+    ${resource_lock_json}=  Evaluate
+    ...  json.load(open('${code_base_dir_path}data/resource_lock_table.json'))  modules=json
+    Rprint Vars  resource_lock_json
+
+    [Return]  ${resource_lock_json}