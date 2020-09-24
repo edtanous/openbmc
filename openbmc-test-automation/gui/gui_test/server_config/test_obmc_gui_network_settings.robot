openbmc/openbmc-test-automation

GUI test to verify system and interface section
Added test cases:
    - Verify Network Interface Details
    - Verify Static IPv4 Section In Network Setting Page

Change-Id: I2b22a67e37050f8612ef446080da72e03306c6d0
Signed-off-by: Anves Kumar rayankula <anvesr77@in.ibm.com>

gui/gui_test/server_config/test_obmc_gui_network_settings.robot
@@ -4,6 +4,7 @@ Documentation   Test OpenBMC GUI "Network settings" sub-menu of
 ...             "Server configuration".
 
 Resource        ../../lib/resource.robot
+Resource        ../../../lib/bmc_network_utils.robot
 
 Suite Setup     Suite Setup Execution
 Suite Teardown  Close Browser
@@ -23,6 +24,8 @@ ${xpath_static_input_ip0}         //*[@data-test-id="networkSettings-input-stati
 ${xpath_add_static_ip}            //button[contains(text(),"Add static IP")]
 ${xpath_setting_success}          //*[contains(text(),"Successfully saved network settings.")]
 ${xpath_add_dns_server}           //button[contains(text(),"Add DNS server")]
+${xpath_network_interface}        //*[@data-test-id="networkSettings-select-interface"]
+${xpath_input_netmask_addr0}      //*[@data-test-id="networkSettings-input-subnetMask-0"]
 
 *** Test Cases ***
 
@@ -115,6 +118,27 @@ Verify System Section In Network Setting page
 
     ${default_gateway}=  Get BMC Default Gateway
     Textfield Value Should Be  ${xpath_default_gateway_input}  ${default_gateway}
+
+
+Verify Network Interface Details
+    [Documentation]  Verify network interface name network setting page.
+    [Tags]  Verify_Network_Interface_Details
+
+    ${active_channel_config}=  Get Active Channel Config
+    ${ethernet_interface}=  Set Variable  ${active_channel_config['${CHANNEL_NUMBER}']['name']}
+    ${interfaces}=  Get Text  ${xpath_network_interface}
+    Should Contain  ${interfaces}  ${ethernet_interface}
+
+
+Verify Network Static IPv4 Details
+    [Documentation]  Verify network static IPv4 details.
+    [Tags]  Verify_Network_static_IPv4_Details
+
+    @{network_configurations}=  Get Network Configuration
+    FOR  ${network_configuration}  IN  @{network_configurations}
+      Textfield Value Should Be  ${xpath_static_input_ip0}  ${network_configuration["Address"]}
+      Textfield Value Should Be  ${xpath_input_netmask_addr0}  ${network_configuration['SubnetMask']}
+    END
 
 
 *** Keywords ***

edtanous/openbmc

    - Verify Network Static IPv4 Details

openbmc-test-automation/gui/gui_test/server_config/test_obmc_gui_network_settings.robot
@@ -4,6 +4,7 @@ Documentation   Test OpenBMC GUI "Network settings" sub-menu of
 ...             "Server configuration".
 
 Resource        ../../lib/resource.robot
+Resource        ../../../lib/bmc_network_utils.robot
 
 Suite Setup     Suite Setup Execution
 Suite Teardown  Close Browser
@@ -23,6 +24,8 @@ ${xpath_static_input_ip0}         //*[@data-test-id="networkSettings-input-stati
 ${xpath_add_static_ip}            //button[contains(text(),"Add static IP")]
 ${xpath_setting_success}          //*[contains(text(),"Successfully saved network settings.")]
 ${xpath_add_dns_server}           //button[contains(text(),"Add DNS server")]
+${xpath_network_interface}        //*[@data-test-id="networkSettings-select-interface"]
+${xpath_input_netmask_addr0}      //*[@data-test-id="networkSettings-input-subnetMask-0"]
 
 *** Test Cases ***
 
@@ -115,6 +118,27 @@ Verify System Section In Network Setting page
 
     ${default_gateway}=  Get BMC Default Gateway
     Textfield Value Should Be  ${xpath_default_gateway_input}  ${default_gateway}
+
+
+Verify Network Interface Details
+    [Documentation]  Verify network interface name in network setting page.
+    [Tags]  Verify_Network_Interface_Details
+
+    ${active_channel_config}=  Get Active Channel Config
+    ${ethernet_interface_redfish}=  Set Variable  ${active_channel_config['${CHANNEL_NUMBER}']['name']}
+    ${ethernet_interface_gui}=  Get Text  ${xpath_network_interface}
+    Should Contain  ${ethernet_interface_gui}  ${ethernet_interface_redfish}
+
+
+Verify Network Static IPv4 Details
+    [Documentation]  Verify network static IPv4 details.
+    [Tags]  Verify_Network_static_IPv4_Details
+
+    @{network_configurations}=  Get Network Configuration
+    FOR  ${network_configuration}  IN  @{network_configurations}
+      Textfield Value Should Be  ${xpath_static_input_ip0}  ${network_configuration["Address"]}
+      Textfield Value Should Be  ${xpath_input_netmask_addr0}  ${network_configuration['SubnetMask']}
+    END
 
 
 *** Keywords ***