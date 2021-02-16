openbmc/phosphor-debug-collector

Add support for resource dump delete.
Add support for deleting resource dumps in host memory.
Change requestDelete to accept dump type.
Update system dump delete to pass dump type.

Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
Change-Id: I8dcf85a2b38da15ff8451003c11232e340c4ca89

host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.hpp
@@ -20,10 +20,11 @@ void requestOffload(uint32_t id);
  * @brief Request to delete dump
  *
  * @param[in] id - The Dump Source ID.
+ * @param[in] dumpType - Type of the dump.
  * @return NULL
  *
  */
-void requestDelete(uint32_t id);
+void requestDelete(uint32_t id, pldm_fileio_file_type dumpType);
 } // namespace host
 
 namespace pldm
@@ -56,10 +57,11 @@ mctp_eid_t readEID();
  * @brief Request to delete dump
  *
  * @param[in] id - The Dump Source ID.
+ * @param[in] dumpType - Type of the dump.
  * @return NULL
  *
  */
-void requestDelete(uint32_t id);
+void requestDelete(uint32_t id, pldm_fileio_file_type dumpType);
 } // namespace pldm
 } // namespace dump
 } // namespace phosphor

edtanous/openbmc

phosphor-debug-collector/host-transport-extensions/pldm/oem/ibm/pldm_oem_cmds.hpp
@@ -20,10 +20,11 @@ void requestOffload(uint32_t id);
  * @brief Request to delete dump
  *
  * @param[in] id - The Dump Source ID.
+ * @param[in] dumpType - Type of the dump.
  * @return NULL
  *
  */
-void requestDelete(uint32_t id);
+void requestDelete(uint32_t id, uint32_t dumpType);
 } // namespace host
 
 namespace pldm
@@ -56,10 +57,11 @@ mctp_eid_t readEID();
  * @brief Request to delete dump
  *
  * @param[in] id - The Dump Source ID.
+ * @param[in] dumpType - Type of the dump.
  * @return NULL
  *
  */
-void requestDelete(uint32_t id);
+void requestDelete(uint32_t id, uint32_t dumpType);
 } // namespace pldm
 } // namespace dump
 } // namespace phosphor