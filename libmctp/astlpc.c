openbmc/libmctp

libmctp: Split padding in pktbuf into header and trailer
The astlpc binding will shortly make use of a medium-specific trailer to
implement a CRC-32 integrity check over packet data. Allow for
medium-specific trailer data in the pktbuf in order to remove the need
for a bounce-buffer. The binding will trim the pktbuf to the payload
length before passing the instance up the stack.

Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Change-Id: Ib215a552ff218b007f7f119c5235fbc75d9ca6cb

astlpc.c
@@ -1045,7 +1045,8 @@ static struct mctp_binding_astlpc *__mctp_astlpc_init(uint8_t mode,
 	astlpc->binding.version = 1;
 	astlpc->binding.pkt_size =
 		MCTP_PACKET_SIZE(mtu > MCTP_BTU ? mtu : MCTP_BTU);
-	astlpc->binding.pkt_pad = 0;
+	astlpc->binding.pkt_hdr = 0;
+	astlpc->binding.pkt_tlr = 0;
 	astlpc->binding.tx = mctp_binding_astlpc_tx;
 	if (mode == MCTP_BINDING_ASTLPC_MODE_BMC)
 		astlpc->binding.start = mctp_binding_astlpc_start_bmc;

core.c
@@ -90,13 +90,13 @@ struct mctp_pktbuf *mctp_pktbuf_alloc(struct mctp_binding *binding, size_t len)
 	struct mctp_pktbuf *buf;
 	size_t size;
 
-	size = binding->pkt_size + binding->pkt_pad;
+	size = binding->pkt_size + binding->pkt_hdr + binding->pkt_tlr;
 
 	/* todo: pools */
 	buf = __mctp_alloc(sizeof(*buf) + size);
 
 	buf->size = size;
-	buf->start = binding->pkt_pad;
+	buf->start = binding->pkt_hdr;
 	buf->end = buf->start + len;
 	buf->mctp_hdr_off = buf->start;
 	buf->next = NULL;

libmctp.h
@@ -103,7 +103,8 @@ struct mctp_binding {
 	struct mctp_bus *bus;
 	struct mctp *mctp;
 	size_t pkt_size;
-	size_t pkt_pad;
+	size_t pkt_hdr;
+	size_t pkt_tlr;
 	int (*start)(struct mctp_binding *binding);
 	int (*tx)(struct mctp_binding *binding, struct mctp_pktbuf *pkt);
 	mctp_rx_fn control_rx;

tests/test-utils.c
@@ -30,7 +30,8 @@ struct mctp_binding_test *mctp_binding_test_init(void)
 	test->binding.version = 1;
 	test->binding.tx = mctp_binding_test_tx;
 	test->binding.pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	test->binding.pkt_pad = 0;
+	test->binding.pkt_hdr = 0;
+	test->binding.pkt_tlr = 0;
 	return test;
 }
 

tests/test_bridge.c
@@ -70,7 +70,8 @@ static struct mctp_binding_bridge *mctp_binding_bridge_init(void)
 	binding->binding.version = 1;
 	binding->binding.tx = mctp_binding_bridge_tx;
 	binding->binding.pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	binding->binding.pkt_pad = 0;
+	binding->binding.pkt_hdr = 0;
+	binding->binding.pkt_tlr = 0;
 	return binding;
 }
 

tests/test_cmds.c
@@ -63,7 +63,8 @@ static void setup_test_binding(struct mctp_binding *test_binding,
 	test_binding->version = 1;
 	test_binding->tx = NULL;
 	test_binding->pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	test_binding->pkt_pad = 0;
+	test_binding->pkt_hdr = 0;
+	test_binding->pkt_tlr = 0;
 	test_binding->control_rx = control_message_transport_callback;
 	test_binding->control_rx_data = callback_ctx;
 

edtanous/openbmc

libmctp/astlpc.c
@@ -1049,7 +1049,8 @@ static struct mctp_binding_astlpc *__mctp_astlpc_init(uint8_t mode,
 	astlpc->binding.version = 1;
 	astlpc->binding.pkt_size =
 		MCTP_PACKET_SIZE(mtu > MCTP_BTU ? mtu : MCTP_BTU);
-	astlpc->binding.pkt_pad = 0;
+	astlpc->binding.pkt_header = 0;
+	astlpc->binding.pkt_trailer = 0;
 	astlpc->binding.tx = mctp_binding_astlpc_tx;
 	if (mode == MCTP_BINDING_ASTLPC_MODE_BMC)
 		astlpc->binding.start = mctp_binding_astlpc_start_bmc;

libmctp/core.c
@@ -90,13 +90,13 @@ struct mctp_pktbuf *mctp_pktbuf_alloc(struct mctp_binding *binding, size_t len)
 	struct mctp_pktbuf *buf;
 	size_t size;
 
-	size = binding->pkt_size + binding->pkt_pad;
+	size = binding->pkt_size + binding->pkt_header + binding->pkt_trailer;
 
 	/* todo: pools */
 	buf = __mctp_alloc(sizeof(*buf) + size);
 
 	buf->size = size;
-	buf->start = binding->pkt_pad;
+	buf->start = binding->pkt_header;
 	buf->end = buf->start + len;
 	buf->mctp_hdr_off = buf->start;
 	buf->next = NULL;

libmctp/libmctp.h
@@ -103,7 +103,8 @@ struct mctp_binding {
 	struct mctp_bus *bus;
 	struct mctp *mctp;
 	size_t pkt_size;
-	size_t pkt_pad;
+	size_t pkt_header;
+	size_t pkt_trailer;
 	int (*start)(struct mctp_binding *binding);
 	int (*tx)(struct mctp_binding *binding, struct mctp_pktbuf *pkt);
 	mctp_rx_fn control_rx;

libmctp/tests/test-utils.c
@@ -30,7 +30,8 @@ struct mctp_binding_test *mctp_binding_test_init(void)
 	test->binding.version = 1;
 	test->binding.tx = mctp_binding_test_tx;
 	test->binding.pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	test->binding.pkt_pad = 0;
+	test->binding.pkt_header = 0;
+	test->binding.pkt_trailer = 0;
 	return test;
 }
 

libmctp/tests/test_bridge.c
@@ -70,7 +70,8 @@ static struct mctp_binding_bridge *mctp_binding_bridge_init(void)
 	binding->binding.version = 1;
 	binding->binding.tx = mctp_binding_bridge_tx;
 	binding->binding.pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	binding->binding.pkt_pad = 0;
+	binding->binding.pkt_header = 0;
+	binding->binding.pkt_trailer = 0;
 	return binding;
 }
 

libmctp/tests/test_cmds.c
@@ -63,7 +63,8 @@ static void setup_test_binding(struct mctp_binding *test_binding,
 	test_binding->version = 1;
 	test_binding->tx = NULL;
 	test_binding->pkt_size = MCTP_PACKET_SIZE(MCTP_BTU);
-	test_binding->pkt_pad = 0;
+	test_binding->pkt_header = 0;
+	test_binding->pkt_trailer = 0;
 	test_binding->control_rx = control_message_transport_callback;
 	test_binding->control_rx_data = callback_ctx;
 