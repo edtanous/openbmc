openbmc/openpower-occ-control

Verify the value of the tempX_label file
- The intent behind this commit is to verify the value of
  the tempX_label file.

- In the future, the value of this file will be changed to
  `D0000002` (OCC sensor ID).
  - OCC Sensor Ids(D0000002)
  - First byte is type                 - D0
  - Second byte is reserved            - 00
  - Third+Fourth bytes are instance id - 0002

Tested: built openpower-occ-control successfully and unit test worked

Signed-off-by: George Liu <liuxiwei@inspur.com>
Change-Id: I5e89ce9592898f79b9fe926dd7bbe5691bf535a5

test/utest.cpp
@@ -1,4 +1,5 @@
 #include "powercap.hpp"
+#include "utils.hpp"
 
 #include <experimental/filesystem>
 #include <occ_events.hpp>
@@ -67,4 +68,18 @@ TEST(VerifyPathParsing, DirectoryPath)
     std::string parsed = Device::getPathBack(path);
 
     EXPECT_STREQ(parsed.c_str(), "bar");
+}
+
+TEST(VerifyLabelValue, checkLabelValue)
+{
+    const std::string value = "D0000002";
+
+    auto labelValue = checkLabelValue(value);
+    EXPECT_NE(labelValue, std::nullopt);
+
+    std::string reType = "D0";
+    uint32_t reID = 2;
+    auto& [type, instanceID] = *labelValue;
+    EXPECT_EQ(type, reType);
+    EXPECT_EQ(instanceID, reID);
 }

utils.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <optional>
 #include <sdbusplus/bus.hpp>
 #include <string>
+#include <tuple>
+
 namespace open_power
 {
 namespace occ
 {
+
+using LABLEVALUE = std::tuple<std::string, uint32_t>;
+
 /**
  * @brief Gets the D-Bus Service name for the input D-Bus path
  *
@@ -18,5 +24,15 @@ namespace occ
  */
 std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
                        const std::string& intf);
+
+/**
+ * @brief Check the value of the `tempX_label` file
+ *
+ * @param[in] value  -  the value of the `tempX_label` file
+ *
+ * @return              Sensors type and Sensors ID
+ */
+std::optional<LABLEVALUE> checkLabelValue(const std::string& value);
+
 } // namespace occ
 } // namespace open_power

edtanous/openbmc

openpower-occ-control/test/utest.cpp
@@ -1,4 +1,5 @@
 #include "powercap.hpp"
+#include "utils.hpp"
 
 #include <experimental/filesystem>
 #include <occ_events.hpp>
@@ -7,6 +8,7 @@
 #include <gtest/gtest.h>
 
 using namespace open_power::occ;
+using namespace open_power::occ::utils;
 
 class VerifyOccInput : public ::testing::Test
 {
@@ -65,4 +67,18 @@ TEST(VerifyPathParsing, DirectoryPath)
     std::string parsed = Device::getPathBack(path);
 
     EXPECT_STREQ(parsed.c_str(), "bar");
+}
+
+TEST(VerifyLabelValue, checkLabelValue)
+{
+    const std::string value = "D0000002";
+
+    auto labelValue = checkLabelValue(value);
+    EXPECT_NE(labelValue, std::nullopt);
+
+    std::string reType = "D0";
+    uint16_t reID = 2;
+    auto& [type, instanceID] = *labelValue;
+    EXPECT_EQ(type, reType);
+    EXPECT_EQ(instanceID, reID);
 }

openpower-occ-control/utils.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <optional>
 #include <sdbusplus/bus.hpp>
 #include <string>
+#include <tuple>
+
 namespace open_power
 {
 namespace occ
 {
 namespace utils
 {
+
+using LABELVALUE = std::tuple<std::string, uint16_t>;
+
 constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
 constexpr auto MAPPER_OBJ_PATH = "/xyz/openbmc_project/object_mapper";
 constexpr auto MAPPER_IFACE = "xyz.openbmc_project.ObjectMapper";
@@ -48,6 +54,15 @@ const std::string getService(const std::string& path,
 const PropertyValue getProperty(const std::string& objectPath,
                                 const std::string& interface,
                                 const std::string& propertyName);
+
+/**
+ * @brief Check the value of the `tempX_label` file
+ *
+ * @param[in] value  -  the value of the `tempX_label` file
+ *
+ * @return              Sensors type and Sensors ID
+ */
+std::optional<LABELVALUE> checkLabelValue(const std::string& value);
 
 } // namespace utils
 } // namespace occ