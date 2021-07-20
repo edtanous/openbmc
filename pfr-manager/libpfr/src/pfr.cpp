openbmc/pfr-manager

pfr-manager: update pfr cpld error codes
Add support to update the cpld error codes in new platform.

Remove the IBB and OBB watchdog expired events as they are no longer
reported from CPLD.

Tested:
Update BMC firmware with mismatched SVN.
POST: https://<BMC_IP>/redfish/v1/UpdateService/
      with <BMC_Update_Capsule> binary file
Update failed and event log received.
Command: GET: https://<BMC_IP>/redfish/v1/Systems/system/LogServices/
              EventLog/Entries
Response:
 {
            "@odata.id": "/redfish/v1/Systems/system/LogServices/
             EventLog/Entries/1621435142_1",
            "@odata.type": "#LogEntry.v1_4_0.LogEntry",
            "Created": "2021-05-19T14:39:02+00:00",
            "EntryType": "Event",
            "Id": "1621435142_1",
            "Message": "Firmware resiliency error. Error reason:
             Firmware update failed(MinorCode:0x02).",
            "MessageArgs": [
                "Firmware update failed(MinorCode:0x02)"
            ],
            "MessageId": "OpenBMC.0.1.FirmwareResiliencyError",
            "Name": "System Event Log Entry",
            "Severity": "Critical"
        },

Signed-off-by: Chalapathi Venkataramashetty <chalapathix.venkataramashetty@intel.com>
Change-Id: I406cb50695bd77b1beddb0788677af3657dca334

libpfr/src/pfr.cpp
@@ -400,6 +400,9 @@ int readCpldReg(const ActionType& action, uint8_t& value)
 
     switch (action)
     {
+        case (ActionType::readRoT):
+            cpldReg = cpldROTVersion;
+            break;
         case (ActionType::recoveryCount):
             cpldReg = recoveryCount;
             break;

edtanous/openbmc

pfr-manager/libpfr/src/pfr.cpp
@@ -491,6 +491,9 @@ int readCpldReg(const ActionType& action, uint8_t& value)
 
     switch (action)
     {
+        case (ActionType::readRoTRev):
+            cpldReg = cpldROTVersion;
+            break;
         case (ActionType::recoveryCount):
             cpldReg = recoveryCount;
             break;