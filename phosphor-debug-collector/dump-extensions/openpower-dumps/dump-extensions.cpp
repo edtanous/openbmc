openbmc/phosphor-debug-collector

Move OpenPOWER specific dump configurations to extension
Move the configuration options for OpenPOWER specific dumps
to the respective extension directory.

Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
Change-Id: I8610de2d0982541110bebfbfda24bebdbdf70748

dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -1,4 +1,5 @@
 #include "config.h"
+#include "dump-extensions/openpower-dumps/opconfig.h"
 
 #include "dump-extensions.hpp"
 

edtanous/openbmc

phosphor-debug-collector/dump-extensions/openpower-dumps/dump-extensions.cpp
@@ -2,6 +2,8 @@
 
 #include "dump-extensions.hpp"
 
+#include "dump-extensions/openpower-dumps/openpower_dumps_config.h"
+
 #include "dump_manager_resource.hpp"
 #include "dump_manager_system.hpp"
 