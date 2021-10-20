openbmc/phosphor-power

psu-ng: Implement gpio write function
Implement a gpio write function to allow writing to gpios via the
libgpiod bindings. The bindings allow to set the desired gpio value by
passing it to the line request call.

Add the ability to pass flags as defined in gpiod.hpp so that the caller
can specify options such as FLAG_OPEN_DRAIN.

Change-Id: Ia7329a17c46f4f1fda0b66f795f123c8524a1be5
Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

phosphor-power-supply/util.cpp
@@ -81,6 +81,32 @@ int GPIOReader::read()
     return value;
 }
 
+void GPIOReader::write(int value, std::bitset<32> flags)
+{
+    using namespace phosphor::logging;
+
+    if (!line)
+    {
+        log<level::ERR>("Failed line");
+        throw std::runtime_error{std::string{"Failed to find line"}};
+    }
+
+    try
+    {
+        line.request(
+            {__FUNCTION__, gpiod::line_request::DIRECTION_OUTPUT, flags},
+            value);
+
+        line.release();
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>("Failed to set GPIO line", entry("MSG=%s", e.what()),
+                        entry("VALUE=%d", value));
+        throw;
+    }
+}
+
 std::unique_ptr<GPIOInterface> createGPIO(const std::string& namedGpio)
 {
     return GPIOReader::createGPIO(namedGpio);

edtanous/openbmc

phosphor-power/phosphor-power-supply/util.cpp
@@ -81,6 +81,32 @@ int GPIOInterface::read()
     return value;
 }
 
+void GPIOInterface::write(int value, std::bitset<32> flags)
+{
+    using namespace phosphor::logging;
+
+    if (!line)
+    {
+        log<level::ERR>("Failed line");
+        throw std::runtime_error{std::string{"Failed to find line"}};
+    }
+
+    try
+    {
+        line.request(
+            {__FUNCTION__, gpiod::line_request::DIRECTION_OUTPUT, flags},
+            value);
+
+        line.release();
+    }
+    catch (std::exception& e)
+    {
+        log<level::ERR>("Failed to set GPIO line", entry("MSG=%s", e.what()),
+                        entry("VALUE=%d", value));
+        throw;
+    }
+}
+
 std::unique_ptr<GPIOInterfaceBase> createGPIO(const std::string& namedGpio)
 {
     return GPIOInterface::createGPIO(namedGpio);