openbmc/bmcweb

reduce error traces in connection and auth code
These logs are reported in good paths (at least web interfaces are
working fine when they are logged). Convert them to warnings so they do
not show up as false errors when debugging bmcweb.

Here's the log we were getting:
Oct 08 19:20:48 p10bmc bmcweb[15348]: (2021-10-08 19:20:48) [ERROR "http_connection.hpp":537] 0x14bd360 Error while reading: end of stream
Oct 08 19:20:48 p10bmc bmcweb[15348]: (2021-10-08 19:20:48) [ERROR "http_connection.hpp":531] 0x14910b0 async_read_header 308 Bytes
Oct 08 19:20:48 p10bmc bmcweb[15348]: (2021-10-08 19:20:48) [ERROR "authorization.hpp":292] authHeader=

These code paths, and why they are not real errors, is not totally clear
to me. Hoping for some discussion on it in this review.

Tested:
- Verified these no longer show up when doing basic system management
  with just error traces enabled in bmcweb

Change-Id: If357aeb93ff28ef02e135a888524e057cb188871
Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

include/authorization.hpp
@@ -285,7 +285,7 @@ static std::shared_ptr<persistent_data::UserSession> authenticate(
     }
 #endif
     std::string_view authHeader = reqHeader["Authorization"];
-    BMCWEB_LOG_ERROR << "authHeader=" << authHeader;
+    BMCWEB_LOG_WARNING << "authHeader=" << authHeader;
 
     if (sessionOut == nullptr && authMethodsConfig.sessionToken)
     {

edtanous/openbmc

bmcweb/include/authorization.hpp
@@ -285,7 +285,7 @@ static std::shared_ptr<persistent_data::UserSession> authenticate(
     }
 #endif
     std::string_view authHeader = reqHeader["Authorization"];
-    BMCWEB_LOG_ERROR << "authHeader=" << authHeader;
+    BMCWEB_LOG_DEBUG << "authHeader=" << authHeader;
 
     if (sessionOut == nullptr && authMethodsConfig.sessionToken)
     {