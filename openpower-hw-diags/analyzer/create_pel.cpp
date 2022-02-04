openbmc/openpower-hw-diags

Update primary SRC field based on callout list
Signed-off-by: Zane Shelley <zshelle@us.ibm.com>
Change-Id: I41f3eda5c27b8ec74d6329b8f145181f400547f8

analyzer/create_pel.cpp
@@ -330,6 +330,9 @@ uint32_t createPel(const ServiceData& i_servData)
     //          until the PEL is submitted.
     std::vector<util::FFDCFile> userDataFiles;
 
+    // Set the subsystem in the primary SRC.
+    i_servData.getSrcSubsystem(logData);
+
     // Set words 6-9 of the SRC.
     __setSrc(i_servData.getRootCause(), logData);
 

edtanous/openbmc

openpower-hw-diags/analyzer/create_pel.cpp
@@ -330,6 +330,9 @@ uint32_t createPel(const ServiceData& i_servData)
     //          until the PEL is submitted.
     std::vector<util::FFDCFile> userDataFiles;
 
+    // Set the subsystem in the primary SRC.
+    i_servData.addSrcSubsystem(logData);
+
     // Set words 6-9 of the SRC.
     __setSrc(i_servData.getRootCause(), logData);
 