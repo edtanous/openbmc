openbmc/openbmc-test-automation

Added fix for vmi run on eth1 interface
           -Defined default_interface variable in library to use in vmi Suite

Change-Id: I9162269d187d9a5718df28e9b69fba093e72cc57
Signed-off-by: shrsuman123 <shrsuman@in.ibm.com>

lib/external_intf/vmi_utils.robot
@@ -12,13 +12,14 @@ Library          ../../lib/bmc_network_utils.py
 *** Variables ***
 
 ${wait_time}              10s
+${default_interface}      eth0
 
 *** Keywords ***
 
 Set Static IPv4 Address To VMI And Verify
     [Documentation]  Set static IPv4 address to VMI.
     [Arguments]  ${ip}  ${gateway}  ${netmask}  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${default_interface}
 
     # Description of argument(s):
     # ip                 VMI IPv4 address.
@@ -43,7 +44,7 @@ Set Static IPv4 Address To VMI And Verify
 Verify VMI Network Interface Details
     [Documentation]  Verify VMI network interface details.
     [Arguments]  ${ip}  ${origin}  ${gateway}  ${netmask}
-    ...  ${interface}=eth0  ${valid_status_code}=${HTTP_OK}
+    ...  ${interface}=${default_interface}  ${valid_status_code}=${HTTP_OK}
 
     # Description of argument(s):
     # ip                 VMI IPv4 address.
@@ -62,7 +63,7 @@ Verify VMI Network Interface Details
 Delete VMI IPv4 Address
     [Documentation]  Delete VMI IPv4 address.
     [Arguments]  ${delete_param}=IPv4StaticAddresses  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${default_interface}
 
     # Description of argument(s):
     # delete_param       Parameter to be deleted eg. IPv4StaticAddresses or IPv4Addresses.
@@ -82,7 +83,7 @@ Delete VMI IPv4 Address
 Set VMI IPv4 Origin
     [Documentation]  Set VMI IPv4 origin.
     [Arguments]  ${dhcp_enabled}=${False}  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${default_interface}
 
     # Description of argument(s):
     # dhcp_enabled       True if user wants to enable DHCP. Default is Static, hence value is set to False.
@@ -102,7 +103,7 @@ Set VMI IPv4 Origin
 
 Get VMI Network Interface Details
     [Documentation]  Get VMI network interface details.
-    [Arguments]  ${interface}=eth0  ${valid_status_code}=${HTTP_OK}
+    [Arguments]  ${interface}=${default_interface}  ${valid_status_code}=${HTTP_OK}
 
     # Description of argument(s):
     # interface          VMI interface (eg. eth0 or eth1).

edtanous/openbmc

           -Defined ethernet_interface variable in library to pick user passed interface

openbmc-test-automation/lib/external_intf/vmi_utils.robot
@@ -18,13 +18,14 @@ Library          ../../lib/bmc_network_utils.py
 &{DISABLE_DHCP}           DHCPv4=&{DHCP_DISABLED}
 
 ${wait_time}              10s
+${ethernet_interface}     eth0
 
 *** Keywords ***
 
 Set Static IPv4 Address To VMI And Verify
     [Documentation]  Set static IPv4 address to VMI.
     [Arguments]  ${ip}  ${gateway}  ${netmask}  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${ethernet_interface}
 
     # Description of argument(s):
     # ip                 VMI IPv4 address.
@@ -49,7 +50,7 @@ Set Static IPv4 Address To VMI And Verify
 Verify VMI Network Interface Details
     [Documentation]  Verify VMI network interface details.
     [Arguments]  ${ip}  ${origin}  ${gateway}  ${netmask}
-    ...  ${interface}=eth0  ${valid_status_code}=${HTTP_OK}
+    ...  ${interface}=${ethernet_interface}  ${valid_status_code}=${HTTP_OK}
 
     # Description of argument(s):
     # ip                 VMI IPv4 address.
@@ -68,7 +69,7 @@ Verify VMI Network Interface Details
 Delete VMI IPv4 Address
     [Documentation]  Delete VMI IPv4 address.
     [Arguments]  ${delete_param}=IPv4StaticAddresses  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${ethernet_interface}
 
     # Description of argument(s):
     # delete_param       Parameter to be deleted eg. IPv4StaticAddresses or IPv4Addresses.
@@ -91,7 +92,7 @@ Delete VMI IPv4 Address
 Set VMI IPv4 Origin
     [Documentation]  Set VMI IPv4 origin.
     [Arguments]  ${dhcp_enabled}=${False}  ${valid_status_code}=${HTTP_ACCEPTED}
-    ...  ${interface}=eth0
+    ...  ${interface}=${ethernet_interface}
 
     # Description of argument(s):
     # dhcp_enabled       True if user wants to enable DHCP. Default is Static, hence value is set to False.
@@ -111,7 +112,7 @@ Set VMI IPv4 Origin
 
 Get VMI Network Interface Details
     [Documentation]  Get VMI network interface details.
-    [Arguments]  ${interface}=eth0  ${valid_status_code}=${HTTP_OK}
+    [Arguments]  ${interface}=${ethernet_interface}  ${valid_status_code}=${HTTP_OK}
 
     # Description of argument(s):
     # interface          VMI interface (eg. eth0 or eth1).