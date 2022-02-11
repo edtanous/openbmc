openbmc/openbmc-test-automation

Rebased for IPMI Get BIOS POST Code
New script for IPMI Get BIOS POST Code command

Add new script for retrieving BIOS POST codes via IPMI interface.
The script tests the command while Host is powered on, after a
Host power cycle, and after a Host power off.

Note: The power off test case fails since in the test image
for this feature, chassis power off is not implemented.  Also, lanplus
is not working with the test image.

Test results uploaded in the MR.

Signed-off-by: Nagarjun <nagarjunb@ami.com>
Change-Id: I7c5c58e9822d3c39b427767b325a8a673a15b6ec

data/ipmi_raw_cmd_table.py
@@ -144,5 +144,17 @@ IPMI_RAW_CMD = {
             "When the payload is activated, the first four bytes are the session ID,"
             "otherwise it should be 00."
         ]
+    },
+    'BIOS_POST_Code':
+    {
+        'Get':
+        [
+            # raw command, expected output, comment
+            "0x30 0xe9",
+            "",
+            "Response bytes will vary in length depending on state of system",
+            "0x89",
+            "error responde byte when host is powered off"
+        ]
     }
 }

edtanous/openbmc

openbmc-test-automation/data/ipmi_raw_cmd_table.py
@@ -144,5 +144,17 @@ IPMI_RAW_CMD = {
             "When the payload is activated, the first four bytes are the session ID,"
             "otherwise it should be 00."
         ]
+    },
+    'BIOS_POST_Code':
+    {
+        'Get':
+        [
+            # raw command, expected output, comment
+            "0x30 0xe9",
+            "",
+            "Response bytes will vary in length depending on state of system",
+            "0x89",
+            "error response byte when host is powered off"
+        ]
     }
 }